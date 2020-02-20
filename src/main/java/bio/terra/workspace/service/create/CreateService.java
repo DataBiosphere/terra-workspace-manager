3f0cae7139353c4fd3c699b253468536bb5b15e2
zloery <zloery@gmail.com>
2020-02-20 16:57:48 -0500

    WIP implementation of Create endpoint for Workspace Manager. Still missing significant work

--- src/main/java/bio/terra/workspace/service/create/CreateService.java
+++ src/main/java/bio/terra/workspace/service/create/CreateService.java
@@ -1,23 +1,33 @@
 package bio.terra.workspace.service.create;
 
 // import bio.terra.workspace.service.ping.exception.BadPingException;
+import bio.terra.workspace.app.configuration.ApplicationConfiguration;
 import bio.terra.workspace.generated.model.CreatedWorkspace;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 @Component
 public class CreateService {
+  private ApplicationConfiguration configuration;
 
-  // public String computePing(String message) {
-  //   if (StringUtils.isEmpty(message)) {
-  //     throw new BadPingException("No message to ping");
-  //   }
-  //   return "pong: " + message + "\n";
-  // }
+  @Autowired
+  public CreateService(ApplicationConfiguration configuration) {
+    this.configuration = configuration;
+  }
 
   public CreatedWorkspace createWorkspace() {
     CreatedWorkspace workspace = new CreatedWorkspace();
-    workspace.setId("0");
+    NamedParameterJdbcTemplate jdbc = configuration.getNamedParameterJdbcTemplate();
+    String id = UUID.randomUUID().toString();
+    Map<String,Object> paramMap = new HashMap<>();
+    paramMap.put("id", id);
+    jdbc.update("insert into workspace (workspace_id, spend_profile, profile_settable, properties) values (:id, 100, true, '{\"key\":\"value\"}')", paramMap);
+    workspace.setId(id);
     return workspace;
   }
 
