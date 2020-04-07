d6c43b25cb031735b1fc7d65aefbea6e17e460cc
zloery <zloery@gmail.com>
2020-04-07 11:17:54 -0400

    Add Transactional annotation to WorkspaceDao, delete CreateDAO (#11)
    * Fix transaction management annotations, remove unused DAO
    
    * Remove EnableTransactionManagement from AppConfig, remove unused imports
    
    * Move Transactional annotation from class level to individual methods

--- src/main/java/bio/terra/workspace/app/configuration/ApplicationConfiguration.java
+++ src/main/java/bio/terra/workspace/app/configuration/ApplicationConfiguration.java
@@ -15,11 +15,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableConfigurationProperties
-@EnableTransactionManagement
 @ConfigurationProperties(prefix = "workspace")
 public class ApplicationConfiguration {
 
