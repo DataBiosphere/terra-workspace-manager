1891f99a7ba15376760f34a46f2677de6f8ff4ad
zloery <zloery@gmail.com>
2020-04-17 14:07:45 -0400

    Revert CreateWorkspace to synch instead of async. Still uses stairway

--- src/main/java/bio/terra/workspace/app/controller/WorkspaceApiController.java
+++ src/main/java/bio/terra/workspace/app/controller/WorkspaceApiController.java
@@ -43,12 +43,15 @@ public class WorkspaceApiController implements WorkspaceApi {
   }
 
   @Override
-  public ResponseEntity<JobModel> createWorkspace(@RequestBody CreateWorkspaceRequestBody body) {
+  public ResponseEntity<CreatedWorkspace> createWorkspace(
+      @RequestBody CreateWorkspaceRequestBody body) {
     AuthenticatedUserRequest userReq = getAuthenticatedInfo();
-    createService.createWorkspace(body, userReq);
+    return new ResponseEntity<>(createService.createWorkspace(body, userReq), HttpStatus.OK);
+    // createService.createWorkspace(body, userReq);
     // Look up the newly-created job
-    JobModel createJob = jobService.retrieveJob(body.getJobControl().getJobid(), userReq);
-    return new ResponseEntity<JobModel>(createJob, HttpStatus.valueOf(createJob.getStatusCode()));
+    // JobModel createJob = jobService.retrieveJob(body.getJobControl().getJobid(), userReq);
+    // return new ResponseEntity<JobModel>(createJob,
+    // HttpStatus.valueOf(createJob.getStatusCode()));
   }
 
   @Override
