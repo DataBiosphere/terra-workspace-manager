ba1aad48ca2198fd2c66a0ddf7d260dfa8cd00a3
zloery <zloery@gmail.com>
2020-04-29 16:53:02 -0400

    Small cleanups before demo
    -Adds bearer token auth to swagger API
    -Fixes missing swagger resources
    -Adds dev backend to the SwaggerUI selection box
    -Provides address of dev Sam. I don't know exactly how we plan to wire these together in the future and it'll probably change when Sam moves to K8s.

--- src/main/java/bio/terra/workspace/app/configuration/ApiResourceConfig.java
+++ src/main/java/bio/terra/workspace/app/configuration/ApiResourceConfig.java
@@ -10,7 +10,7 @@ public class ApiResourceConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry
-        .addResourceHandler("/swagger-webjar/**")
+        .addResourceHandler("/api/swagger-webjar/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.24.0/");
     registry.addResourceHandler("/api/**").addResourceLocations("classpath:/api/");
   }
