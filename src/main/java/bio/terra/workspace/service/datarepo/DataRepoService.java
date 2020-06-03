bfd1fccd17ab5cea779ce9b17138978ef2e7a8a6
Isaac Zarsky <zarsky@broadinstitute.org>
2020-06-03 12:42:26 -0400

    Enforce whitelist for data repo instance

--- src/main/java/bio/terra/workspace/service/datarepo/DataRepoService.java
+++ src/main/java/bio/terra/workspace/service/datarepo/DataRepoService.java
@@ -3,11 +3,22 @@ package bio.terra.workspace.service.datarepo;
 import bio.terra.datarepo.api.RepositoryApi;
 import bio.terra.datarepo.client.ApiClient;
 import bio.terra.datarepo.client.ApiException;
+import bio.terra.workspace.common.exception.ValidationException;
 import bio.terra.workspace.service.iam.AuthenticatedUserRequest;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataRepoService {
+
+  private static final Set<String> INSTANCE_WHITELIST =
+      Stream.of(
+              "https://jade.datarepo-dev.broadinstitute.org",
+              "https://jade-terra.datarepo-prod.broadinstitute.org",
+              "https://data.terra.bio")
+          .collect(Collectors.toSet());
 
   private ApiClient getApiClient(String accessToken) {
     ApiClient client = new ApiClient();
@@ -16,7 +27,11 @@ public class DataRepoService {
   }
 
   private RepositoryApi repositoryApi(String instance, AuthenticatedUserRequest userReq) {
-    return new RepositoryApi(getApiClient(userReq.getRequiredToken()).setBasePath(instance));
+    if (INSTANCE_WHITELIST.contains(instance)) {
+      return new RepositoryApi(getApiClient(userReq.getRequiredToken()).setBasePath(instance));
+    } else {
+      throw new ValidationException("Data repository instance " + instance + " is not allowed.");
+    }
   }
 
   public boolean snapshotExists(
