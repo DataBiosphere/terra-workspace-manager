7d1e2dbef6ad6f61de241a7c8be99a6f5757ab35
Matt Bemis <mbemis@broadinstitute.org>
2020-06-19 09:56:37 -0400

    cleanup

--- src/main/java/bio/terra/workspace/mdc/MdcLogEnhancerFilter.java
+++ src/main/java/bio/terra/workspace/mdc/MdcLogEnhancerFilter.java
@@ -12,12 +12,6 @@ import org.springframework.stereotype.Component;
 public class MdcLogEnhancerFilter implements Filter {
 
   @Override
-  public void destroy() {}
-
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {}
-
-  @Override
   public void doFilter(
       ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
       throws IOException, ServletException {
@@ -33,10 +27,9 @@ public class MdcLogEnhancerFilter implements Filter {
   }
 
   private String generateRequestId() {
-    Hashids hashids = new Hashids("requestIdSalt");
-    long generatedLong = ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE);
-    String id = hashids.encode(generatedLong);
+    Hashids hashids = new Hashids("requestIdSalt", 8); // min length will be 8
+    long generatedLong = ThreadLocalRandom.current().nextLong(0, Integer.MAX_VALUE);
 
-    return id;
+    return hashids.encode(generatedLong);
   }
 }
