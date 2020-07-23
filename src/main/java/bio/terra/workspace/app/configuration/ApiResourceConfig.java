4fa52bd934f6882dcf7a3f7812343427c2f9acd9
ansingh7115 <singh.an@husky.neu.edu>
2020-07-23 10:45:05 -0400

    MDC.put()

--- src/main/java/bio/terra/workspace/app/configuration/ApiResourceConfig.java
+++ src/main/java/bio/terra/workspace/app/configuration/ApiResourceConfig.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.hashids.Hashids;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -33,8 +34,10 @@ public class ApiResourceConfig implements WebMvcConfigurer {
               HttpServletRequest httpRequest, HttpServletResponse httpResponse, Object handler)
               throws Exception {
 
-            // get an mdc id from the request (if not found, create one), and pass it along in the response
+            // get an mdc id from the request (if not found, create one), and pass it along in the
+            // response
             String requestId = getMDCRequestId(httpRequest);
+            MDC.put("requestId", requestId);
             httpResponse.addHeader(mdcRequestIdKey, requestId);
 
             // add tags to Stackdriver traces
