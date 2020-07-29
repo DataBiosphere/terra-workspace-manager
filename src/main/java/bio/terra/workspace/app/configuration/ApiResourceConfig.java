eeccd7ecb7a7150aa8ba0ae4ed57d534efdd0880
zloery <zloery@gmail.com>
2020-07-29 12:20:38 -0400

    Remove webjar config that was accidentally re-added (#71)
    * Remove webjar config that was accidentally re-added
    
    * Remove unused import

--- src/main/java/bio/terra/workspace/app/configuration/ApiResourceConfig.java
+++ src/main/java/bio/terra/workspace/app/configuration/ApiResourceConfig.java
@@ -10,16 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApiResourceConfig implements WebMvcConfigurer {
-
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/**").addResourceLocations("classpath:/api/");
-  }
 
   @Autowired private SpanCustomizer spanCustomizer;
   private String mdcRequestIdKey = "X-Request-ID";
