28f1965f4ddbf1d5cf63903cb479a8328cd4af7d
Ivan <ichang@broadinstitute.org>
2020-09-16 08:48:23 -0400

    Based on David's comments, I decided to remove the header and simply implement just the correlation Id generation and let the user have the flexibility to choose whatever header that works for their team. So all of us have a common function of generating a correlation Id regardless of the underlying implementation.

--- jmeter-plugins/src/main/java/plugins/jmeter/functions/XCorrelationID.java
+++ jmeter-plugins/src/main/java/plugins/jmeter/functions/XCorrelationID.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.ThreadLocalRandom;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.jmeter.engine.util.CompoundVariable;
 import org.apache.jmeter.functions.AbstractFunction;
 import org.apache.jmeter.functions.InvalidVariableException;
@@ -16,13 +15,12 @@ import org.hashids.Hashids;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class XCorrelationIDHeader extends AbstractFunction {
-    private Hashids hashids = new Hashids("XCorrelatiomnIDSalt", 8);
-    private static final String MDC_CORRELATION_ID_HEADER = "X-Correlation-ID";
-    private static final Logger log = LoggerFactory.getLogger(XCorrelationIDHeader.class);
+public class XCorrelationID extends AbstractFunction {
+    private Hashids hashids = new Hashids("XCorrelationIDSalt", 8);
+    private static final Logger log = LoggerFactory.getLogger(XCorrelationID.class);
     private static final List<String> desc = new LinkedList<>();
 
-    private static final String KEY = "__XCorrelationIDHeader"; //$NON-NLS-1$
+    private static final String KEY = "__XCorrelationID"; //$NON-NLS-1$
 
     // Number of parameters expected - used to reject invalid calls
     private static final int MIN_PARAMETER_COUNT = 1;
@@ -35,7 +33,7 @@ public class XCorrelationIDHeader extends AbstractFunction {
 
     private Object[] values;
 
-    public XCorrelationIDHeader() {
+    public XCorrelationID() {
 
     }
 
@@ -46,16 +44,13 @@ public class XCorrelationIDHeader extends AbstractFunction {
 
     @Override
     public String execute(SampleResult previousResult, Sampler currentSampler) throws InvalidVariableException {
-        StringBuilder header = new StringBuilder();
-        header.append(MDC_CORRELATION_ID_HEADER);
-        header.append(": ");
+        StringBuilder uniqueId = new StringBuilder();
         String correlationId = generateCorrelationId();
         String prefix = ((CompoundVariable) values[0]).execute().trim();
         if (prefix.length() > 0) {
-            header.append(prefix);
-            header.append("-");
+            uniqueId.append(prefix).append("-");
         }
-        header.append(correlationId);
+        uniqueId.append(correlationId);
 
         JMeterVariables vars = getVariables();
 
@@ -65,10 +60,10 @@ public class XCorrelationIDHeader extends AbstractFunction {
             varName = ((CompoundVariable) values[1]).execute().trim();
 
         if (vars != null && varName != null) {
-            vars.put(varName, header.toString());
+            vars.put(varName, uniqueId.toString());
         }
 
-        return header.toString();
+        return uniqueId.toString();
     }
 
     @Override
