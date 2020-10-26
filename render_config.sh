19267cd838d94f565ec45a0820ea5f2797415166
wchamber <1499870+wchamber@users.noreply.github.com>
2020-10-26 15:59:16 -0400

    PF-152 Add CRL dependency and configuration (#123)
    Include CRL build dependencies for initial libraries needed for project creation within a workspace.
    Create configuration to create CRL Cloud Object Wrapper (COW) beans.
    Add janitor client credential grabbing for local test runs.

--- render_config.sh
+++ render_config.sh
@@ -5,6 +5,8 @@ WM_APP_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/dev/dev/workspace/app
 WM_APP_SERVICE_ACCOUNT_OUTPUT_PATH=$(dirname "$0")/rendered/service-account.json
 USER_DELEGATED_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/firecloud/dev/common/firecloud-account.json
 USER_DELEGATED_SERVICE_ACCOUNT_OUTPUT_PATH=$(dirname "$0")/rendered/user-delegated-service-account.json
+JANITOR_CLIENT_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/integration/tools/crl_janitor/client-sa
+JANITOR_CLIENT_SERVICE_ACCOUNT_OUTPUT_PATH="$(dirname $0)"/rendered/janitor-client-sa-account.json
 
 DSDE_TOOLBOX_DOCKER_IMAGE=broadinstitute/dsde-toolbox:consul-0.20.0
 
@@ -15,3 +17,6 @@ docker run --rm -e VAULT_TOKEN="$VAULT_TOKEN" $DSDE_TOOLBOX_DOCKER_IMAGE \
     vault read -format=json ${USER_DELEGATED_SERVICE_ACCOUNT_VAULT_PATH} | \
     # Not base64 encoded or stored under 'key'
     jq -r .data > ${USER_DELEGATED_SERVICE_ACCOUNT_OUTPUT_PATH}
+docker run --rm -e VAULT_TOKEN="$VAULT_TOKEN" $DSDE_TOOLBOX_DOCKER_IMAGE \
+    vault read -format=json ${JANITOR_CLIENT_SERVICE_ACCOUNT_VAULT_PATH} | \
+    jq -r .data.key | base64 -d > ${JANITOR_CLIENT_SERVICE_ACCOUNT_OUTPUT_PATH}
---
+++ src/main/java/bio/terra/workspace/app/configuration/external/CrlConfiguration.java
@@ -0,0 +1,115 @@
+package bio.terra.workspace.app.configuration.external;
+
+import bio.terra.cloudres.common.ClientConfig;
+import bio.terra.cloudres.common.cleanup.CleanupConfig;
+import bio.terra.cloudres.google.billing.CloudBillingClientCow;
+import bio.terra.cloudres.google.cloudresourcemanager.CloudResourceManagerCow;
+import bio.terra.cloudres.google.serviceusage.ServiceUsageCow;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.time.Duration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/** Configuration to use Terra Cloud Resource Library. */
+@Component
+@EnableConfigurationProperties
+@EnableTransactionManagement
+@ConfigurationProperties(prefix = "workspace.crl")
+public class CrlConfiguration {
+  /** The client name required by CRL. */
+  public static final String CLIENT_NAME = "workspace";
+  /** How long to keep the resource before Janitor do the cleanup. */
+  public static final Duration TEST_RESOURCE_TIME_TO_LIVE = Duration.ofHours(1);
+
+  /**
+   * Whether we're running RBS in test mode with Cloud Resource Library. If so, we enable to the
+   * Janitor to auto-delete all created cloud resources.
+   */
+  private boolean testingMode = false;
+
+  /** Credential file path to be able to publish message to Janitor */
+  private String janitorClientCredentialFilePath;
+
+  /** pubsub project id to publish track resource to Janitor */
+  private String janitorTrackResourceProjectId;
+
+  /** pubsub topic id to publish track resource to Janitor */
+  private String janitorTrackResourceTopicId;
+
+  public void setTestingMode(boolean testingMode) {
+    this.testingMode = testingMode;
+  }
+
+  public void setJanitorClientCredentialFilePath(String janitorClientCredentialFilePath) {
+    this.janitorClientCredentialFilePath = janitorClientCredentialFilePath;
+  }
+
+  public void setJanitorTrackResourceProjectId(String janitorTrackResourceProjectId) {
+    this.janitorTrackResourceProjectId = janitorTrackResourceProjectId;
+  }
+
+  public void setJanitorTrackResourceTopicId(String janitorTrackResourceTopicId) {
+    this.janitorTrackResourceTopicId = janitorTrackResourceTopicId;
+  }
+
+  /**
+   * The {@link ClientConfig} in CRL's COW object. If in test, it will also include {@link
+   * CleanupConfig}.
+   */
+  @Bean
+  @Lazy
+  public ClientConfig clientConfig() {
+    ClientConfig.Builder builder = ClientConfig.Builder.newBuilder().setClient(CLIENT_NAME);
+    if (testingMode) {
+      builder.setCleanupConfig(
+          CleanupConfig.builder()
+              .setCleanupId(CLIENT_NAME + "-test")
+              .setJanitorProjectId(janitorTrackResourceProjectId)
+              .setTimeToLive(TEST_RESOURCE_TIME_TO_LIVE)
+              .setJanitorTopicName(janitorTrackResourceTopicId)
+              .setCredentials(getGoogleCredentialsOrDie(janitorClientCredentialFilePath))
+              .build());
+    }
+    return builder.build();
+  }
+
+  /** The CRL {@link CloudResourceManagerCow} which wrappers Google Cloud Resource Manager API. */
+  @Bean
+  @Lazy
+  public CloudResourceManagerCow cloudResourceManagerCow()
+      throws IOException, GeneralSecurityException {
+    return CloudResourceManagerCow.create(
+        clientConfig(), GoogleCredentials.getApplicationDefault());
+  }
+
+  /** The CRL {@link CloudBillingClientCow} which wrappers Google Billing API. */
+  @Bean
+  @Lazy
+  public CloudBillingClientCow cloudBillingClientCow() throws IOException {
+    return new CloudBillingClientCow(clientConfig(), GoogleCredentials.getApplicationDefault());
+  }
+
+  /** The CRL {@link ServiceUsageCow} which wrappers Google Cloud ServiceUsage API. */
+  @Bean
+  @Lazy
+  public ServiceUsageCow serviceUsageCow() throws GeneralSecurityException, IOException {
+    return ServiceUsageCow.create(clientConfig(), GoogleCredentials.getApplicationDefault());
+  }
+
+  private static ServiceAccountCredentials getGoogleCredentialsOrDie(String serviceAccountPath) {
+    try {
+      return ServiceAccountCredentials.fromStream(new FileInputStream(serviceAccountPath));
+    } catch (Exception e) {
+      throw new RuntimeException(
+          "Unable to load GoogleCredentials from configuration: " + serviceAccountPath, e);
+    }
+  }
+}
