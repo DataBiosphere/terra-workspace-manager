9fca50dba4dc8acca07efd54d092e892145cbeba
wchambers <1499870+wchamber@users.noreply.github.com>
2020-11-03 10:04:54 -0500

    Move @AutoConfigureMockMvc to WorkspaceServiceTest.

--- src/test/java/bio/terra/workspace/service/workspace/WorkspaceServiceTest.java
+++ src/test/java/bio/terra/workspace/service/workspace/WorkspaceServiceTest.java
@@ -39,12 +39,14 @@ import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+@AutoConfigureMockMvc
 public class WorkspaceServiceTest extends BaseConnectedTest {
   @Autowired private WorkspaceService workspaceService;
   @Autowired private JobService jobService;
