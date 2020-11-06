d2f2b8997345ef8e22beebdf03ccbf50c7e51cb5
wchambers <1499870+wchamber@users.noreply.github.com>
2020-11-06 10:04:30 -0500

    revert json serialization

--- src/main/java/bio/terra/workspace/service/spendprofile/SpendProfileId.java
+++ src/main/java/bio/terra/workspace/service/spendprofile/SpendProfileId.java
@@ -9,14 +9,9 @@ import java.util.UUID;
  * <p>Implemented as a class wrapping a {@link UUID} so that this is strongly typed.
  */
 @AutoValue
-@JsonSerialize(as = SpendProfileId.class)
-@JsonDeserialize(as = AutoValue_SpendProfileId.class)
-@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
 public abstract class SpendProfileId {
-  @JsonProperty("uuid")
   public abstract UUID uuid();
 
-  @JsonCreator
   public static SpendProfileId create(UUID id) {
     return new AutoValue_SpendProfileId(id);
   }
