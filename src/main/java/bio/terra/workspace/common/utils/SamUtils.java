4dd25b79a40622e1b8b71f738bdfc848c70d39c9
wchamber <1499870+wchamber@users.noreply.github.com>
2020-11-09 13:49:23 -0500

    PF-186 Add a spend service based on config with Sam authz check (#141)
    Add a SpendProfileService. Base the "SpendProfiles" being handed out on configuration for spend profile ids & billing account id. Do a Sam spend-profile link authz check. This will serve as our short term Spend Profile Manager stub until the component is built and we integrate with it.
    I will replace the existing UUID spendProfileId usage in the rest of the code/api with the String based strongly typed SpendProfileId in another PR.

--- src/main/java/bio/terra/workspace/common/utils/SamUtils.java
+++ src/main/java/bio/terra/workspace/common/utils/SamUtils.java
@@ -6,4 +6,7 @@ public class SamUtils {
   public static String SAM_WORKSPACE_READ_ACTION = "read";
   public static String SAM_WORKSPACE_WRITE_ACTION = "write";
   public static String SAM_WORKSPACE_DELETE_ACTION = "delete";
+
+  public static String SPEND_PROFILE_RESOURCE = "spend-profile";
+  public static String SPEND_PROFILE_LINK_ACTION = "link";
 }
