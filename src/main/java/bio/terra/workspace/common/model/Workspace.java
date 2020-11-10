445075c758184fda457423e2e454be8177d96f9b
zloery <6668057+zloery@users.noreply.github.com>
2020-11-10 15:58:58 -0500

    Add javadoc to Workspace

--- src/main/java/bio/terra/workspace/common/model/Workspace.java
+++ src/main/java/bio/terra/workspace/common/model/Workspace.java
@@ -5,14 +5,29 @@ import com.google.auto.value.AutoValue;
 import java.util.Optional;
 import java.util.UUID;
 
-/** Internal representation of a Workspace. */
+/**
+ * Internal representation of a Workspace.
+ *
+ * A workspace is a collection of resources, data references, and applications with some shared
+ * context. In general, a workspace is the fundamental unit of analysis in Terra. Workspaces may
+ * have an associated billing account and may have zero or one associated GCP projects.
+ */
 @AutoValue
 public abstract class Workspace {
 
+  /** The globally unique identifier of this workspace */
   public abstract UUID workspaceId();
 
+  /**
+   * The spend profile ID associated with this project, if one exists.
+   *
+   * In the future, this will correlate to a spend profile in the Spend Profile Manager. For now,
+   * it's just a unique identifier. Accounts with an associated GCP project must have a spend
+   * profile, while Rawls workspaces do not need one.
+   */
   public abstract Optional<SpendProfileId> spendProfileId();
 
+  /** Temporary feature flag indicating whether this workspace uses MC Terra features. */
   public abstract WorkspaceStage workspaceStage();
 
   public static Builder builder() {
