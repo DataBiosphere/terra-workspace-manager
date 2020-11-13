b577c0ae2c5c7d3bb19dd200f264d6abdeba618d
zloery <6668057+zloery@users.noreply.github.com>
2020-11-13 13:33:54 -0500

    Break dataReference changes into separate PR

--- src/main/java/bio/terra/workspace/service/datareference/flight/CreateDataReferenceStep.java
+++ src/main/java/bio/terra/workspace/service/datareference/flight/CreateDataReferenceStep.java
@@ -13,12 +13,10 @@ import bio.terra.workspace.service.job.JobMapKeys;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 
+/** Stairway step to persist a data reference in WM's database. */
 public class CreateDataReferenceStep implements Step {
 
   private DataReferenceDao dataReferenceDao;
-
-  private static final String CREATE_DATA_REFERENCE_COMPLETED_KEY =
-      "createDataReferenceStepCompleted";
 
   public CreateDataReferenceStep(DataReferenceDao dataReferenceDao) {
     this.dataReferenceDao = dataReferenceDao;
@@ -28,8 +26,7 @@ public class CreateDataReferenceStep implements Step {
   public StepResult doStep(FlightContext flightContext) throws RetryException {
     FlightMap inputMap = flightContext.getInputParameters();
     FlightMap workingMap = flightContext.getWorkingMap();
-    workingMap.put(CREATE_DATA_REFERENCE_COMPLETED_KEY, false);
-    UUID referenceId = inputMap.get(DataReferenceFlightMapKeys.REFERENCE_ID, UUID.class);
+    UUID referenceId = workingMap.get(DataReferenceFlightMapKeys.REFERENCE_ID, UUID.class);
     UUID workspaceId = inputMap.get(DataReferenceFlightMapKeys.WORKSPACE_ID, UUID.class);
     DataRepoSnapshot reference =
         inputMap.get(DataReferenceFlightMapKeys.REFERENCE, DataRepoSnapshot.class);
@@ -45,9 +42,8 @@ public class CreateDataReferenceStep implements Step {
         body.getCloningInstructions(),
         body.getReferenceType(),
         reference);
-    workingMap.put(CREATE_DATA_REFERENCE_COMPLETED_KEY, true);
 
-    FlightUtils.setResponse(flightContext, referenceId.toString(), HttpStatus.OK);
+    FlightUtils.setResponse(flightContext, referenceId, HttpStatus.OK);
 
     return StepResult.getStepResultSuccess();
   }
@@ -57,11 +53,12 @@ public class CreateDataReferenceStep implements Step {
     FlightMap inputMap = flightContext.getInputParameters();
     FlightMap workingMap = flightContext.getWorkingMap();
 
-    if (workingMap.get(CREATE_DATA_REFERENCE_COMPLETED_KEY, Boolean.class)) {
-      UUID referenceId = inputMap.get(DataReferenceFlightMapKeys.REFERENCE_ID, UUID.class);
-      UUID workspaceId = inputMap.get(DataReferenceFlightMapKeys.WORKSPACE_ID, UUID.class);
-      dataReferenceDao.deleteDataReference(workspaceId, referenceId);
-    }
+    UUID referenceId = workingMap.get(DataReferenceFlightMapKeys.REFERENCE_ID, UUID.class);
+    UUID workspaceId = inputMap.get(DataReferenceFlightMapKeys.WORKSPACE_ID, UUID.class);
+
+    // Ignore return value, as we don't care whether a reference was deleted or just not found.
+    dataReferenceDao.deleteDataReference(workspaceId, referenceId);
+
     return StepResult.getStepResultSuccess();
   }
 }
