ab7253503f9611c804d19f6c2212e19b182c020a
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2021-01-08 17:43:19 -0500

    progress

--- workspace-manager-clienttests/src/main/java/scripts/testscripts/GetRoles.java
+++ workspace-manager-clienttests/src/main/java/scripts/testscripts/GetRoles.java
@@ -8,6 +8,8 @@ import bio.terra.workspace.api.WorkspaceApi;
 import bio.terra.workspace.client.ApiException;
 import bio.terra.workspace.model.CreateWorkspaceRequestBody;
 import bio.terra.workspace.model.CreatedWorkspace;
+import bio.terra.workspace.model.IamRole;
+import bio.terra.workspace.model.RoleBindingList;
 import java.util.List;
 import java.util.UUID;
 import scripts.utils.WorkspaceTestScriptBase;
@@ -15,35 +17,47 @@ import scripts.utils.WorkspaceTestScriptBase;
 public class GetRoles extends WorkspaceTestScriptBase {
 private static final IamRole IAM_ROLE = IamRole.WRITER;
   private UUID workspaceId;
-  private CreatedWorkspace workspace;
 
   // Grant a role for all test users on a new workspace
   @Override
   public void doSetup(List<TestUserSpecification> testUsers, WorkspaceApi workspaceApi)
       throws ApiException {
+    workspaceId = UUID.randomUUID();
     final CreateWorkspaceRequestBody requestBody = new CreateWorkspaceRequestBody();
     requestBody.setId(workspaceId);
-    workspace = workspaceApi.createWorkspace(requestBody);
-    for (TestUserSpecification testUser : testUsers) {
-      workspaceApi.grantRole(workspaceId, IAM_ROLE, testUser.userEmail);
-    }
+    final CreatedWorkspace workspace = workspaceApi.createWorkspace(requestBody);
+    assertThat(workspace.getId(), equalTo(workspaceId));
   }
 
   @Override
   public void doUserJourney(TestUserSpecification testUser, WorkspaceApi workspaceApi)
       throws ApiException {
-   final bio.terra.workspace.model.RoleBindingList roles = workspaceApi.getRoles(workspaceId);
+    // grant the role
+    workspaceApi.grantRole(workspaceId, IAM_ROLE, testUser.userEmail);
+
+    // check granted roles
+    final RoleBindingList roles = workspaceApi.getRoles(workspaceId);
    // our user should be in this list, with the correct role
-    final boolean isInList = roles.stream()
+    boolean isInList = roles.stream()
         .anyMatch(rb ->
             rb.getRole() == IAM_ROLE &&
             rb.getMembers().contains(testUser.userEmail));
     assertThat(isInList, equalTo(true));
+
+    // remove the role
+    workspaceApi.removeRole(workspaceId, IAM_ROLE, testUser.userEmail);
+    final RoleBindingList updatedRoles = workspaceApi.getRoles(workspaceId);
+    isInList = roles.stream()
+        .anyMatch(rb ->
+            rb.getRole() == IAM_ROLE &&
+                rb.getMembers().contains(testUser.userEmail));
+    assertThat(isInList, equalTo(false));
   }
 
   @Override
   public void doCleanup(List<TestUserSpecification> testUsers, WorkspaceApi workspaceApi)
       throws ApiException {
-    // remove role
+    // assert that no users have the iam role
+    workspaceApi.deleteWorkspace(workspaceId);
   }
 }
