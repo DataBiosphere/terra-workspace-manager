35c29982e07ba3dd2475a020dbdfa280cfbe3884
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2021-01-08 15:44:39 -0500

    first user of base class

--- workspace-manager-clienttests/src/main/java/scripts/testscripts/GetWorkspace.java
+++ workspace-manager-clienttests/src/main/java/scripts/testscripts/GetWorkspace.java
@@ -1,8 +1,6 @@
 package scripts.testscripts;
 
-import bio.terra.testrunner.runner.TestScript;
 import bio.terra.testrunner.runner.config.TestUserSpecification;
-import bio.terra.workspace.client.ApiClient;
 import bio.terra.workspace.api.WorkspaceApi;
 import bio.terra.workspace.client.ApiException;
 import bio.terra.workspace.model.CreateWorkspaceRequestBody;
@@ -10,69 +8,40 @@ import bio.terra.workspace.model.CreatedWorkspace;
 import bio.terra.workspace.model.WorkspaceDescription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scripts.utils.WorkspaceManagerServiceUtils;
 
 import java.util.List;
 import java.util.UUID;
+import scripts.utils.WorkspaceTestScriptBase;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-public class GetWorkspace extends TestScript {
+public class GetWorkspace extends WorkspaceTestScriptBase {
     private static final Logger logger = LoggerFactory.getLogger(GetWorkspace.class);
-    private UUID id;
     private CreatedWorkspace workspace;
 
     @Override
-    public void setup(List<TestUserSpecification> testUsers) throws Exception {
-        assertThat("There must be at least one test user in configs/testusers directory.", testUsers!=null && testUsers.size()>0);
-        id = UUID.randomUUID();
-        ApiClient apiClient = WorkspaceManagerServiceUtils.getClientForTestUser(testUsers.get(0), server);
-        WorkspaceApi workspaceApi = new WorkspaceApi(apiClient);
-        try {
-            CreateWorkspaceRequestBody requestBody = new CreateWorkspaceRequestBody();
-            requestBody.setId(id);
-            workspace = workspaceApi.createWorkspace(requestBody);
-        } catch (ApiException apiEx) {
-            logger.debug("Caught exception creating workspace ", apiEx);
-        }
-
-        int httpCode = workspaceApi.getApiClient().getStatusCode();
-        logger.info("CREATE workspace HTTP code: {}", httpCode);
-        assertThat(httpCode, equalTo(200));
+    public void doSetup(List<TestUserSpecification> testUsers, WorkspaceApi workspaceApi)
+        throws ApiException {
+        final UUID workspaceId = UUID.randomUUID();
+        final CreateWorkspaceRequestBody requestBody = new CreateWorkspaceRequestBody();
+        requestBody.setId(workspaceId);
+        workspace = workspaceApi.createWorkspace(requestBody);
     }
 
     @Override
-    public void userJourney(TestUserSpecification testUser) throws Exception {
-        ApiClient apiClient = WorkspaceManagerServiceUtils.getClientForTestUser(testUser, server);
-        WorkspaceApi workspaceApi = new WorkspaceApi(apiClient);
-
+    public void doUserJourney(TestUserSpecification testUser, WorkspaceApi workspaceApi)
+        throws ApiException {
         /**
          * This GetWorkspace test expects a valid workspace id
          * created by the setup step.
          *
          * Throw exception if anything goes wrong
          * **/
-        WorkspaceDescription workspaceDescription = workspaceApi.getWorkspace(workspace.getId());
-        int httpCode = workspaceApi.getApiClient().getStatusCode();
-        logger.info("GET workspace HTTP code: {}", httpCode);
-        assertThat(httpCode, equalTo(200));
+        final WorkspaceDescription workspaceDescription = workspaceApi.getWorkspace(workspace.getId());
     }
 
     @Override
-    public void cleanup(List<TestUserSpecification> testUsers) throws Exception {
-        assertThat("There must be at least one test user in configs/testusers directory.", testUsers!=null && testUsers.size()>0);
-        ApiClient apiClient = WorkspaceManagerServiceUtils.getClientForTestUser(testUsers.get(0), server);
-        WorkspaceApi workspaceApi = new WorkspaceApi(apiClient);
-
-        try {
-            workspaceApi.deleteWorkspace(workspace.getId());
-        } catch (ApiException apiEx) {
-            logger.debug("Caught exception deleting workspace ", apiEx);
-        }
-
-        int httpCode = workspaceApi.getApiClient().getStatusCode();
-        logger.info("DELETE workspace HTTP code: {}", httpCode);
-        assertThat(httpCode, equalTo(204));
+    public void doCleanup(List<TestUserSpecification> testUsers, WorkspaceApi workspaceApi)
+        throws ApiException {
+        workspaceApi.deleteWorkspace(workspace.getId());
     }
+
 }
