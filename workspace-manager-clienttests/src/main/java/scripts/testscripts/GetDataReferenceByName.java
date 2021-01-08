aa7bdb679b133e16269893113b35ee8e701f122e
zloery <6668057+zloery@users.noreply.github.com>
2021-01-08 15:04:14 -0500

    messy, but all tests translated

--- workspace-manager-clienttests/src/main/java/scripts/testscripts/GetDataReferenceByName.java
+++ workspace-manager-clienttests/src/main/java/scripts/testscripts/GetDataReferenceByName.java
@@ -1,49 +1,53 @@
 package scripts.testscripts;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 import bio.terra.testrunner.runner.TestScript;
 import bio.terra.testrunner.runner.config.TestUserSpecification;
-import bio.terra.workspace.client.ApiClient;
 import bio.terra.workspace.api.WorkspaceApi;
+import bio.terra.workspace.client.ApiClient;
 import bio.terra.workspace.client.ApiException;
-import bio.terra.workspace.model.CloningInstructionsEnum;
 import bio.terra.workspace.model.CreateDataReferenceRequestBody;
 import bio.terra.workspace.model.CreateWorkspaceRequestBody;
 import bio.terra.workspace.model.CreatedWorkspace;
 import bio.terra.workspace.model.DataReferenceDescription;
-import bio.terra.workspace.model.DataRepoSnapshot;
 import bio.terra.workspace.model.ReferenceTypeEnum;
 import bio.terra.workspace.model.WorkspaceDescription;
+import java.util.List;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scripts.utils.DataReferenceUtils;
 import scripts.utils.WorkspaceManagerServiceUtils;
 
-import java.util.List;
-import java.util.UUID;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-public class CreateGetDeleteDataReference extends TestScript {
-  private static final Logger logger = LoggerFactory.getLogger(CreateGetDeleteDataReference.class);
-  private UUID id;
-  private CreatedWorkspace workspace;
+public class GetDataReferenceByName extends TestScript {
+  private static final Logger logger = LoggerFactory.getLogger(GetDataReferenceByName.class);
+  private UUID workspaceId;
+  private DataReferenceDescription dataReference;
 
   @Override
   public void setup(List<TestUserSpecification> testUsers) throws Exception {
     assertThat("There must be at least one test user in configs/testusers directory.", testUsers!=null && testUsers.size()>0);
-    id = UUID.randomUUID();
+    workspaceId = UUID.randomUUID();
     ApiClient apiClient = WorkspaceManagerServiceUtils.getClientForTestUser(testUsers.get(0), server);
     WorkspaceApi workspaceApi = new WorkspaceApi(apiClient);
+    // First, create a workspace.
     try {
-      CreateWorkspaceRequestBody requestBody = new CreateWorkspaceRequestBody().id(id);
-      workspace = workspaceApi.createWorkspace(requestBody);
+      CreateWorkspaceRequestBody requestBody = new CreateWorkspaceRequestBody().id(workspaceId);
+      workspaceApi.createWorkspace(requestBody);
     } catch (ApiException apiEx) {
       logger.debug("Caught exception creating workspace ", apiEx);
     }
-
     int httpCode = workspaceApi.getApiClient().getStatusCode();
     logger.info("CREATE workspace HTTP code: {}", httpCode);
+    assertThat(httpCode, equalTo(200));
+    // Second, create a data reference in that workspace.
+    CreateDataReferenceRequestBody referenceRequest = DataReferenceUtils
+        .defaultDataReferenceRequest();
+    dataReference = workspaceApi.createDataReference(referenceRequest, workspaceId);
+    httpCode = workspaceApi.getApiClient().getStatusCode();
+    logger.info("CREATE data reference HTTP code: {}", httpCode);
     assertThat(httpCode, equalTo(200));
   }
 
@@ -51,27 +55,27 @@ public class CreateGetDeleteDataReference extends TestScript {
   public void userJourney(TestUserSpecification testUser) throws Exception {
     ApiClient apiClient = WorkspaceManagerServiceUtils.getClientForTestUser(testUser, server);
     WorkspaceApi workspaceApi = new WorkspaceApi(apiClient);
-    CreateDataReferenceRequestBody referenceRequest = DataReferenceUtils
-        .defaultDataReferenceRequest();
-    DataReferenceDescription createdReferenceDescription = workspaceApi.createDataReference(referenceRequest, id);
+    DataReferenceDescription receivedReference = workspaceApi.getDataReferenceByName(workspaceId, dataReference.getReferenceType(), dataReference.getName());
     int httpCode = workspaceApi.getApiClient().getStatusCode();
-    logger.info("CREATE data reference HTTP code: {}", httpCode);
+    logger.info("GET workspace HTTP code: {}", httpCode);
     assertThat(httpCode, equalTo(200));
-
-    DataReferenceDescription getResult = workspaceApi.getDataReference(id, createdReferenceDescription.getReferenceId());
-    httpCode = workspaceApi.getApiClient().getStatusCode();
-    logger.info("GET data reference HTTP code: {}", httpCode);
-    assertThat(httpCode, equalTo(200));
-    assertThat(getResult, equalTo(createdReferenceDescription));
-
-    workspaceApi.deleteDataReference(id, getResult.getReferenceId());
-    httpCode = workspaceApi.getApiClient().getStatusCode();
-    logger.info("DELETE data reference HTTP code: {}", httpCode);
-    assertThat(httpCode, equalTo(204));
+    assertThat(receivedReference, equalTo(dataReference));
   }
 
   @Override
   public void cleanup(List<TestUserSpecification> testUsers) throws Exception {
     assertThat("There must be at least one test user in configs/testusers directory.", testUsers!=null && testUsers.size()>0);
+    ApiClient apiClient = WorkspaceManagerServiceUtils.getClientForTestUser(testUsers.get(0), server);
+    WorkspaceApi workspaceApi = new WorkspaceApi(apiClient);
+
+    try {
+      workspaceApi.deleteWorkspace(workspaceId);
+    } catch (ApiException apiEx) {
+      logger.debug("Caught exception deleting workspace ", apiEx);
+    }
+
+    int httpCode = workspaceApi.getApiClient().getStatusCode();
+    logger.info("DELETE workspace HTTP code: {}", httpCode);
+    assertThat(httpCode, equalTo(204));
   }
 }
---
+++ workspace-manager-clienttests/src/main/java/scripts/utils/DataReferenceUtils.java
@@ -0,0 +1,34 @@
+package scripts.utils;
+
+import bio.terra.workspace.model.CloningInstructionsEnum;
+import bio.terra.workspace.model.CreateDataReferenceRequestBody;
+import bio.terra.workspace.model.DataRepoSnapshot;
+import bio.terra.workspace.model.ReferenceTypeEnum;
+
+public class DataReferenceUtils {
+
+  public static CreateDataReferenceRequestBody defaultDataReferenceRequest() {
+    // This method relies on a persistent snapshot existing in Data Repo, currently in dev.
+    // This snapshot was created using our dev service account, which is a steward in dev Data Repo.
+    // First, I created a dataset "wm_integration_test_dataset" using TDR's
+    // snapshot-test-dataset.json. Then, I created the snapshot
+    // "workspace_integration_test_snapshot" using the "byFullView" mode. Finally, I added the
+    // integration test user as a reader of this snapshot.
+    // These steps should only need to be repeated if the dev DataRepo data is deleted, or to
+    // support this test in other DataRepo environments.
+    // Data Repo makes a reasonable effort to maintain their dev environment, so this should be a
+    // very rare occurrence.
+    DataRepoSnapshot snapshotReference =
+        new DataRepoSnapshot()
+            .snapshot("97b5559a-2f8f-4df3-89ae-5a249173ee0c")
+            .instanceName("terra");
+    String dataReferenceName = "workspace_integration_test_snapshot";
+    CreateDataReferenceRequestBody referenceRequest =
+        new CreateDataReferenceRequestBody()
+            .name(dataReferenceName)
+            .referenceType(ReferenceTypeEnum.DATA_REPO_SNAPSHOT)
+            .reference(snapshotReference)
+            .cloningInstructions(CloningInstructionsEnum.NOTHING);
+    return referenceRequest;
+  }
+}
