06aebd6387c43d4e38f62d092ff5400592cf00fd
ansingh7115 <singh.an@husky.neu.edu>
2021-01-19 19:59:51 -0500

    user-emails

--- src/test/java/bio/terra/workspace/integration/common/configuration/IntegrationTestConfiguration.java
+++ src/test/java/bio/terra/workspace/integration/common/configuration/IntegrationTestConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "workspace.integration-test")
 public class IntegrationTestConfiguration {
 
-  @Value("${TEST_ENV:local}")
+  @Value("${TEST_ENV:dev}")
   private String testEnv;
 
   private HashMap<String, String> wsmUrls;
