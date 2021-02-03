a2f3c491ec15e9efc9f714cda0291cd1e26de577
zloery <6668057+zloery@users.noreply.github.com>
2021-02-03 16:01:04 -0500

    remove typeMap

--- src/main/java/bio/terra/workspace/service/datareference/model/DataReferenceType.java
+++ src/main/java/bio/terra/workspace/service/datareference/model/DataReferenceType.java
@@ -2,7 +2,6 @@ package bio.terra.workspace.service.datareference.model;
 
 import bio.terra.workspace.generated.model.ReferenceTypeEnum;
 import com.google.common.collect.BiMap;
-import com.google.common.collect.EnumBiMap;
 import com.google.common.collect.EnumHashBiMap;
 
 /** Enum describing the type of object a data reference is pointing to. */
@@ -14,28 +13,21 @@ public enum DataReferenceType {
   /** A Google BigQuery dataset. Corresponds to a {@Code BigQueryReference} object. */
   BIG_QUERY_DATASET;
 
-  private static final BiMap<DataReferenceType, ReferenceTypeEnum> typeMap =
-      EnumBiMap.create(DataReferenceType.class, ReferenceTypeEnum.class);
-
   private static final BiMap<DataReferenceType, String> sqlMap =
       EnumHashBiMap.create(DataReferenceType.class);
 
   static {
-    typeMap.put(DataReferenceType.DATA_REPO_SNAPSHOT, ReferenceTypeEnum.DATA_REPO_SNAPSHOT);
-    typeMap.put(DataReferenceType.GOOGLE_BUCKET, ReferenceTypeEnum.GOOGLE_BUCKET);
-    typeMap.put(DataReferenceType.BIG_QUERY_DATASET, ReferenceTypeEnum.BIG_QUERY_DATASET);
-
     sqlMap.put(DATA_REPO_SNAPSHOT, "DATA_REPO_SNAPSHOT");
     sqlMap.put(GOOGLE_BUCKET, "GOOGLE_BUCKET");
     sqlMap.put(BIG_QUERY_DATASET, "BIG_QUERY_DATASET");
   }
 
   public static DataReferenceType fromApiModel(ReferenceTypeEnum modelEnum) {
-    return typeMap.inverse().get(modelEnum);
+    return DataReferenceType.valueOf(modelEnum.name());
   }
 
   public ReferenceTypeEnum toApiModel() {
-    return typeMap.get(this);
+    return ReferenceTypeEnum.valueOf(this.name());
   }
 
   /** Convert this to a String to be serialized to the DB. */
