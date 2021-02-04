c8d523a22f56540f145920336cbd25f3fe2f0298
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2021-02-04 10:12:14 -0500

    merge error

--- src/main/java/bio/terra/workspace/service/iam/model/SamConstants.java
+++ src/main/java/bio/terra/workspace/service/iam/model/SamConstants.java
@@ -6,7 +6,6 @@ public class SamConstants {
   public static final String SAM_WORKSPACE_READ_ACTION = "read";
   public static final String SAM_WORKSPACE_WRITE_ACTION = "write";
   public static final String SAM_WORKSPACE_DELETE_ACTION = "delete";
-  public static String SAM_WORKSPACE_READ_IAM_ACTION = "read_policies";
   public static final String SAM_WORKSPACE_READ_IAM_ACTION = "read_policies";
   public static final String SPEND_PROFILE_RESOURCE = "spend-profile";
   public static final String SPEND_PROFILE_LINK_ACTION = "link";
