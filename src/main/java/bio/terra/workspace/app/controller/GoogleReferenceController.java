eaae8beb4c0a317b841e7e279efc8fab99a442f7
zloery <6668057+zloery@users.noreply.github.com>
2021-02-08 14:49:00 -0500

    add controller annotations

--- src/main/java/bio/terra/workspace/app/controller/GoogleReferenceController.java
+++ src/main/java/bio/terra/workspace/app/controller/GoogleReferenceController.java
@@ -24,7 +24,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class GoogleReferenceController implements GoogleReferenceApi {
 
   private final DataReferenceValidationUtils dataReferenceValidation;
