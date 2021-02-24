32cc53dd21f65b10af4a856d8f90f7c5cc0ad221
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2021-02-24 14:44:29 -0500

    simplify

--- src/test/java/bio/terra/workspace/service/resource/controlled/flight/CreateGcsBucketStepTest.java
+++ src/test/java/bio/terra/workspace/service/resource/controlled/flight/CreateGcsBucketStepTest.java
@@ -8,17 +8,12 @@ import static org.mockito.Mockito.verify;
 
 import bio.terra.cloudres.google.storage.StorageCow;
 import bio.terra.stairway.FlightContext;
-import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.StepResult;
 import bio.terra.stairway.exception.RetryException;
 import bio.terra.workspace.common.BaseUnitTest;
 import bio.terra.workspace.common.fixtures.ControlledResourceFixtures;
 import bio.terra.workspace.service.crl.CrlService;
-import bio.terra.workspace.service.datareference.flight.DataReferenceFlightMapKeys;
 import bio.terra.workspace.service.iam.AuthenticatedUserRequest;
-import bio.terra.workspace.service.job.JobMapKeys;
-import bio.terra.workspace.service.workspace.flight.WorkspaceFlightMapKeys;
-import bio.terra.workspace.service.workspace.flight.WorkspaceFlightMapKeys.ControlledResourceKeys;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.BucketInfo.LifecycleRule;
 import com.google.cloud.storage.BucketInfo.LifecycleRule.LifecycleAction;
@@ -34,13 +29,13 @@ import org.mockito.Mock;
 public class CreateGcsBucketStepTest extends BaseUnitTest {
 
   @Mock private FlightContext mockFlightContext;
-
   @Mock private CrlService mockCrlService;
   @Mock private StorageCow mockStorageCow;
   @Mock private AuthenticatedUserRequest mockUserRequest;
-  private CreateGcsBucketStep createGcsBucketStep;
 
   @Captor private ArgumentCaptor<BucketInfo> bucketInfoCaptor;
+
+  private CreateGcsBucketStep createGcsBucketStep;
 
   @BeforeEach
   public void setup() {
@@ -49,19 +44,6 @@ public class CreateGcsBucketStepTest extends BaseUnitTest {
             mockCrlService, ControlledResourceFixtures.BUCKET_RESOURCE, mockUserRequest);
 
     doReturn(mockStorageCow).when(mockCrlService).createStorageCow(mockUserRequest);
-
-    // Stub the flight map as of this step
-    final FlightMap inputFlightMap = new FlightMap();
-    inputFlightMap.put(JobMapKeys.REQUEST.getKeyName(), ControlledResourceFixtures.BUCKET_RESOURCE);
-    inputFlightMap.put(
-        WorkspaceFlightMapKeys.WORKSPACE_ID, ControlledResourceFixtures.WORKSPACE_ID);
-    inputFlightMap.put(ControlledResourceKeys.RESOURCE_ID, ControlledResourceFixtures.RESOURCE_ID);
-    inputFlightMap.put(
-        DataReferenceFlightMapKeys.REFERENCE_ID, ControlledResourceFixtures.DATA_REFERENCE_ID);
-    inputFlightMap.put(ControlledResourceKeys.OWNER_EMAIL, ControlledResourceFixtures.OWNER_EMAIL);
-    inputFlightMap.makeImmutable();
-
-    doReturn(inputFlightMap).when(mockFlightContext).getInputParameters();
   }
 
   @Test
