a2cdde2b2f9ea892bcb853a275424c3bfef216b2
ansingh7115 <singh.an@husky.neu.edu>
2021-03-17 20:34:54 -0400

    rebase

--- src/test/java/bio/terra/workspace/integration/common/response/WorkspaceResponse.java
+++
@@ -1,5 +0,0 @@
-package bio.terra.workspace.integration.common.response;
-
-import bio.terra.workspace.model.ErrorReport;
-
-public class WorkspaceResponse<T> extends ErrorOrObjectResponse<ErrorReport, T> {}
