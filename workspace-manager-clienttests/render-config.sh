cf1f4c1c2ec845644a097a379152316e61d93389
Ivan <ichang@broadinstitute.org>
2021-03-19 12:48:10 -0400

    Cleaned up comments. Moved test runner GCP SA account out of vault paths referencing namespaces and placed them in common path. Removed IAM reference from config files.

--- workspace-manager-clienttests/render-config.sh
+++ workspace-manager-clienttests/render-config.sh
@@ -5,8 +5,8 @@ WM_APP_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/integration/wsmtest/w
 WM_APP_SERVICE_ACCOUNT_OUTPUT_PATH=$(dirname "$0")/rendered/workspace-manager-app-service-account.json
 USER_DELEGATED_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/firecloud/dev/common/firecloud-account.json
 USER_DELEGATED_SERVICE_ACCOUNT_OUTPUT_PATH=$(dirname "$0")/rendered/user-delegated-service-account.json
-TESTRUNNER_IAM_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/integration/ichang/testrunner-sa
-TESTRUNNER_IAM_SERVICE_ACCOUNT_OUTPUT_PATH=$(dirname "$0")/rendered/testrunner-iam-service-account.json
+TESTRUNNER_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/integration/common/testrunner/testrunner-sa
+TESTRUNNER_SERVICE_ACCOUNT_OUTPUT_PATH=$(dirname "$0")/rendered/testrunner-service-account.json
 
 DSDE_TOOLBOX_DOCKER_IMAGE=broadinstitute/dsde-toolbox:consul-0.20.0
 
@@ -18,5 +18,5 @@ docker run --rm -e VAULT_TOKEN="$VAULT_TOKEN" $DSDE_TOOLBOX_DOCKER_IMAGE \
     # Not base64 encoded or stored under 'key'
     jq -r .data > ${USER_DELEGATED_SERVICE_ACCOUNT_OUTPUT_PATH}
 docker run --rm -e VAULT_TOKEN="$VAULT_TOKEN" $DSDE_TOOLBOX_DOCKER_IMAGE \
-    vault read -format=json ${TESTRUNNER_IAM_SERVICE_ACCOUNT_VAULT_PATH} | \
-    jq -r .data.key | base64 --decode > ${TESTRUNNER_IAM_SERVICE_ACCOUNT_OUTPUT_PATH}
+    vault read -format=json ${TESTRUNNER_SERVICE_ACCOUNT_VAULT_PATH} | \
+    jq -r .data.key | base64 --decode > ${TESTRUNNER_SERVICE_ACCOUNT_OUTPUT_PATH}
