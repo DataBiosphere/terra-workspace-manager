16097b3ce4724a86526add58f0db15a0e62268ed
Ivan <ichang@broadinstitute.org>
2021-03-23 17:01:28 -0400

    Ran spotless check.

--- workspace-manager-clienttests/src/main/java/scripts/disruptivescripts/DeleteInitialPods.java
+++ workspace-manager-clienttests/src/main/java/scripts/disruptivescripts/DeleteInitialPods.java
@@ -4,58 +4,67 @@ import bio.terra.testrunner.common.utils.KubernetesClientUtils;
 import bio.terra.testrunner.runner.DisruptiveScript;
 import bio.terra.testrunner.runner.config.TestUserSpecification;
 import io.kubernetes.client.openapi.models.V1Deployment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DeleteInitialPods extends DisruptiveScript {
-    private static final Logger logger = LoggerFactory.getLogger(DeleteInitialPods.class);
+  private static final Logger logger = LoggerFactory.getLogger(DeleteInitialPods.class);
 
-    public DeleteInitialPods() {
-        super();
-        manipulatesKubernetes = true;
+  public DeleteInitialPods() {
+    super();
+    manipulatesKubernetes = true;
+  }
+
+  protected static final int SECONDS_TO_WAIT_BEFORE_STARTING_DISRUPT = 15;
+
+  @Override
+  public void disrupt(List<TestUserSpecification> testUsers) throws Exception {
+    // give user journey threads time to get started before disruption
+    TimeUnit.SECONDS.sleep(SECONDS_TO_WAIT_BEFORE_STARTING_DISRUPT);
+
+    logger.info(
+        "Starting disruption - all initially created api pods will be deleted, one by one.");
+
+    String componentLabelKey =
+        KubernetesClientUtils.getComponentLabel(); // e.g. "app.kubernetes.io/component";
+    String componentLabelVal =
+        KubernetesClientUtils.getApiComponentLabel(); // e.g. "workspacemanager";
+    // V1Deployment workspacemanagerDeployment =
+    // KubernetesClientUtils.getApiDeployment(componentLabelKey, componentLabelVal);
+    V1Deployment workspacemanagerDeployment = KubernetesClientUtils.getApiDeployment();
+    if (workspacemanagerDeployment == null) {
+      throw new RuntimeException("WorkspaceManager deployment not found.");
     }
 
-    protected static final int SECONDS_TO_WAIT_BEFORE_STARTING_DISRUPT = 15;
+    List<String> podsToDelete =
+        KubernetesClientUtils.listPods().stream()
+            .filter(
+                pod ->
+                    pod.getMetadata().getLabels().containsKey(componentLabelKey)
+                        && pod.getMetadata()
+                            .getLabels()
+                            .get(componentLabelKey)
+                            .equals(componentLabelVal))
+            .map(pod -> pod.getMetadata().getName())
+            .collect(Collectors.toList());
 
-    @Override
-    public void disrupt(List<TestUserSpecification> testUsers) throws Exception {
-        // give user journey threads time to get started before disruption
-        TimeUnit.SECONDS.sleep(SECONDS_TO_WAIT_BEFORE_STARTING_DISRUPT);
-
-        logger.info("Starting disruption - all initially created api pods will be deleted, one by one.");
-
-        String componentLabelKey = KubernetesClientUtils.getComponentLabel(); // e.g. "app.kubernetes.io/component";
-        String componentLabelVal = KubernetesClientUtils.getApiComponentLabel(); // e.g. "workspacemanager";
-        //V1Deployment workspacemanagerDeployment = KubernetesClientUtils.getApiDeployment(componentLabelKey, componentLabelVal);
-        V1Deployment workspacemanagerDeployment = KubernetesClientUtils.getApiDeployment();
-        if (workspacemanagerDeployment == null) {
-            throw new RuntimeException("WorkspaceManager deployment not found.");
-        }
-
-        List<String> podsToDelete = KubernetesClientUtils.listPods().stream()
-                .filter(
-                        pod ->
-                                pod.getMetadata().getLabels().containsKey(componentLabelKey)
-                                        && pod.getMetadata().getLabels().get(componentLabelKey).equals(componentLabelVal))
-                .map(pod -> pod.getMetadata().getName())
-                .collect(Collectors.toList());
-
-        // delete original pods, and give them a chance to recover
-        for (String podName : podsToDelete) {
-            logger.debug("delete pod: {}", podName);
-            workspacemanagerDeployment = KubernetesClientUtils.getApiDeployment();
-            if (workspacemanagerDeployment != null) {
-                KubernetesClientUtils.printApiPods(workspacemanagerDeployment);
-                KubernetesClientUtils.deletePod(podName);
-                KubernetesClientUtils.waitForReplicaSetSizeChange(workspacemanagerDeployment, podsToDelete.size());
-            }
-        }
-
-        logger.debug("original pods:");
-        podsToDelete.forEach(p -> logger.debug(p));
+    // delete original pods, and give them a chance to recover
+    for (String podName : podsToDelete) {
+      logger.debug("delete pod: {}", podName);
+      workspacemanagerDeployment = KubernetesClientUtils.getApiDeployment();
+      if (workspacemanagerDeployment != null) {
         KubernetesClientUtils.printApiPods(workspacemanagerDeployment);
+        KubernetesClientUtils.deletePod(podName);
+        KubernetesClientUtils.waitForReplicaSetSizeChange(
+            workspacemanagerDeployment, podsToDelete.size());
+      }
     }
+
+    logger.debug("original pods:");
+    podsToDelete.forEach(p -> logger.debug(p));
+    KubernetesClientUtils.printApiPods(workspacemanagerDeployment);
+  }
 }
--- workspace-manager-clienttests/src/main/java/scripts/testscripts/ServiceStatus.java
+++ workspace-manager-clienttests/src/main/java/scripts/testscripts/ServiceStatus.java
@@ -5,37 +5,36 @@ import bio.terra.testrunner.runner.config.TestUserSpecification;
 import bio.terra.workspace.api.UnauthenticatedApi;
 import bio.terra.workspace.client.ApiClient;
 import bio.terra.workspace.model.SystemStatus;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scripts.utils.ClientTestUtils;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 public class ServiceStatus extends TestScript {
-    private static final Logger logger = LoggerFactory.getLogger(ServiceStatus.class);
-    private Duration delay = Duration.ZERO;
+  private static final Logger logger = LoggerFactory.getLogger(ServiceStatus.class);
+  private Duration delay = Duration.ZERO;
 
-    @Override
-    public void setParameters(List<String> parameters) {
+  @Override
+  public void setParameters(List<String> parameters) {
 
-        if (parameters == null || parameters.size() == 0) {
-            return;
-        }
-        delay = Duration.ofSeconds(Long.parseLong(parameters.get(0)));
+    if (parameters == null || parameters.size() == 0) {
+      return;
     }
+    delay = Duration.ofSeconds(Long.parseLong(parameters.get(0)));
+  }
 
-    @Override
-    public void userJourney(TestUserSpecification testUser) throws Exception {
-        if (delay.getSeconds() > 0) TimeUnit.SECONDS.sleep(delay.getSeconds());
+  @Override
+  public void userJourney(TestUserSpecification testUser) throws Exception {
+    if (delay.getSeconds() > 0) TimeUnit.SECONDS.sleep(delay.getSeconds());
 
-        logger.info("Checking service status endpoint now.");
-        ApiClient apiClient = ClientTestUtils.getClientWithoutAccessToken(server);
-        UnauthenticatedApi unauthenticatedApi = new UnauthenticatedApi(apiClient);
-        SystemStatus systemStatus = unauthenticatedApi.serviceStatus();
+    logger.info("Checking service status endpoint now.");
+    ApiClient apiClient = ClientTestUtils.getClientWithoutAccessToken(server);
+    UnauthenticatedApi unauthenticatedApi = new UnauthenticatedApi(apiClient);
+    SystemStatus systemStatus = unauthenticatedApi.serviceStatus();
 
-        int httpCode = unauthenticatedApi.getApiClient().getStatusCode();
-        logger.info("Service status with code {}: {}", httpCode, systemStatus);
-    }
+    int httpCode = unauthenticatedApi.getApiClient().getStatusCode();
+    logger.info("Service status with code {}: {}", httpCode, systemStatus);
+  }
 }
