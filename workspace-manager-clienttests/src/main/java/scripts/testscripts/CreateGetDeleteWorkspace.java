b1b36709662f00465cc191efe4d06d913900cedd
Ivan <ichang@broadinstitute.org>
2021-03-24 17:29:01 -0400

    Reformat this file.

--- workspace-manager-clienttests/src/main/java/scripts/testscripts/CreateGetDeleteWorkspace.java
+++ workspace-manager-clienttests/src/main/java/scripts/testscripts/CreateGetDeleteWorkspace.java
@@ -1,26 +1,26 @@
 package scripts.testscripts;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
 import bio.terra.testrunner.runner.config.TestUserSpecification;
 import bio.terra.workspace.api.WorkspaceApi;
 import bio.terra.workspace.client.ApiException;
 import bio.terra.workspace.model.CreateWorkspaceRequestBody;
 import bio.terra.workspace.model.WorkspaceDescription;
 import bio.terra.workspace.model.WorkspaceStageModel;
-import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scripts.utils.ClientTestUtils;
+
+import java.util.UUID;
 import scripts.utils.WorkspaceTestScriptBase;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class CreateGetDeleteWorkspace extends WorkspaceTestScriptBase {
   private static final Logger logger = LoggerFactory.getLogger(CreateGetDeleteWorkspace.class);
 
   @Override
-  public void doUserJourney(TestUserSpecification testUser, WorkspaceApi workspaceApi)
-      throws ApiException {
+  public void doUserJourney(TestUserSpecification testUser, WorkspaceApi workspaceApi) throws ApiException {
     UUID workspaceId = UUID.randomUUID();
     CreateWorkspaceRequestBody requestBody = new CreateWorkspaceRequestBody().id(workspaceId).stage(WorkspaceStageModel.MC_WORKSPACE);
     workspaceApi.createWorkspace(requestBody);
