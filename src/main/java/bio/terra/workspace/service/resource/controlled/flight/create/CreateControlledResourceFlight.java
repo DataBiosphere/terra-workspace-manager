25a69662125310005ee699e2b808124c2b03056b
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2021-04-14 10:45:09 -0400

    spotless

--- src/main/java/bio/terra/workspace/service/resource/controlled/flight/create/CreateControlledResourceFlight.java
+++ src/main/java/bio/terra/workspace/service/resource/controlled/flight/create/CreateControlledResourceFlight.java
@@ -57,7 +57,7 @@ public class CreateControlledResourceFlight extends Flight {
                 flightBeanBag.getCrlService(),
                 resource.castToGcsBucketResource(),
                 flightBeanBag.getWorkspaceService()));
-addStep(
+        addStep(
             new CreateGcsBucketStep(
                 flightBeanBag.getCrlService(),
                 resource.castToGcsBucketResource(),
