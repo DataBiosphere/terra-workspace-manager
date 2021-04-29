3251dc05ba17f057b2753f58865b7782155ff032
zloery <6668057+zloery@users.noreply.github.com>
2021-04-29 10:54:25 -0400

    [PF-419] Add endpoint for deleting controlled BQ dataset (#312)
    * initial resource definitions, cloud sync is wip
    
    * cleaner iam management, still needs tests
    
    * small fixes
    
    * tests
    
    * fix retryrule usage
    
    * comment fixes
    
    * add delete endpoint, test
    
    * more sane sync retry rule values
    
    * pr comments
    
    * add test for 'undoing' dataset deletion

---
+++ src/main/java/bio/terra/workspace/service/resource/controlled/flight/delete/DeleteBigQueryDatasetStep.java
@@ -0,0 +1,75 @@
+package bio.terra.workspace.service.resource.controlled.flight.delete;
+
+import bio.terra.cloudres.google.bigquery.BigQueryCow;
+import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.Step;
+import bio.terra.stairway.StepResult;
+import bio.terra.stairway.StepStatus;
+import bio.terra.stairway.exception.RetryException;
+import bio.terra.workspace.service.crl.CrlService;
+import bio.terra.workspace.service.resource.controlled.ControlledBigQueryDatasetResource;
+import bio.terra.workspace.service.workspace.WorkspaceService;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import java.io.IOException;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A step for deleting a controlled BigQuery dataset resource. This will delete a dataset even if it
+ * has tables or data present.
+ */
+public class DeleteBigQueryDatasetStep implements Step {
+
+  private final ControlledBigQueryDatasetResource resource;
+  private final CrlService crlService;
+  private final WorkspaceService workspaceService;
+
+  private final Logger logger = LoggerFactory.getLogger(DeleteBigQueryDatasetStep.class);
+
+  public DeleteBigQueryDatasetStep(
+      ControlledBigQueryDatasetResource resource,
+      CrlService crlService,
+      WorkspaceService workspaceService) {
+    this.resource = resource;
+    this.crlService = crlService;
+    this.workspaceService = workspaceService;
+  }
+
+  @Override
+  public StepResult doStep(FlightContext flightContext)
+      throws InterruptedException, RetryException {
+    String projectId = workspaceService.getRequiredGcpProject(resource.getWorkspaceId());
+    BigQueryCow bqCow = crlService.createWsmSaBigQueryCow();
+    try {
+      // With deleteContents set to true, this will delete the dataset even if it still has tables.
+      bqCow
+          .datasets()
+          .delete(projectId, resource.getDatasetName())
+          .setDeleteContents(true)
+          .execute();
+    } catch (GoogleJsonResponseException e) {
+      if (e.getStatusCode() == HttpStatus.SC_NOT_FOUND) {
+        logger.info(
+            "BQ dataset {} in project {} already deleted", resource.getDatasetName(), projectId);
+        return StepResult.getStepResultSuccess();
+      }
+      return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY, e);
+    } catch (IOException e) {
+      return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY, e);
+    }
+
+    return StepResult.getStepResultSuccess();
+  }
+
+  @Override
+  public StepResult undoStep(FlightContext flightContext) throws InterruptedException {
+    // Deletes cannot be undone, so we log a warning and continue the flight.
+    String projectId = workspaceService.getRequiredGcpProject(resource.getWorkspaceId());
+
+    logger.error(
+        "Cannot undo delete of BQ dataset {} in project {}.", resource.getDatasetName(), projectId);
+    // Surface whatever error caused Stairway to begin undoing.
+    return flightContext.getResult();
+  }
+}
--- src/test/java/bio/terra/workspace/service/resource/controlled/ControlledResourceServiceTest.java
+++ src/test/java/bio/terra/workspace/service/resource/controlled/ControlledResourceServiceTest.java
@@ -28,6 +28,9 @@ import bio.terra.workspace.service.resource.controlled.flight.create.CreateBigQu
 import bio.terra.workspace.service.resource.controlled.flight.create.notebook.CreateAiNotebookInstanceStep;
 import bio.terra.workspace.service.resource.controlled.flight.create.notebook.CreateServiceAccountStep;
 import bio.terra.workspace.service.resource.controlled.flight.create.notebook.RetrieveNetworkNameStep;
+import bio.terra.workspace.service.resource.controlled.flight.delete.DeleteBigQueryDatasetStep;
+import bio.terra.workspace.service.resource.controlled.flight.delete.DeleteMetadataStep;
+import bio.terra.workspace.service.resource.controlled.flight.delete.DeleteSamResourceStep;
 import bio.terra.workspace.service.resource.exception.DuplicateResourceException;
 import bio.terra.workspace.service.resource.exception.ResourceNotFoundException;
 import bio.terra.workspace.service.spendprofile.SpendConnectedTestUtils;
@@ -375,4 +378,120 @@ public class ControlledResourceServiceTest extends BaseConnectedTest {
                 resource.getResourceId(),
                 userAccessUtils.defaultUserAuthRequest()));
   }
+
+  @Test
+  @DisabledIfEnvironmentVariable(named = "TEST_ENV", matches = bufferServiceDisabledEnvsRegEx)
+  public void deleteBqDatasetDo() throws Exception {
+    Workspace workspace = reusableWorkspace();
+    String projectId = workspace.getGcpCloudContext().get().getGcpProjectId();
+
+    String datasetId = "my_test_dataset";
+    String location = "us-central1";
+
+    ApiGcpBigQueryDatasetCreationParameters creationParameters =
+        new ApiGcpBigQueryDatasetCreationParameters().datasetId(datasetId).location(location);
+    ControlledBigQueryDatasetResource resource =
+        ControlledResourceFixtures.makeDefaultControlledBigQueryDatasetResource()
+            .workspaceId(workspace.getWorkspaceId())
+            .accessScope(AccessScopeType.ACCESS_SCOPE_SHARED)
+            .managedBy(ManagedByType.MANAGED_BY_USER)
+            .datasetName(datasetId)
+            .build();
+
+    ControlledBigQueryDatasetResource createdDataset =
+        controlledResourceService.createBqDataset(
+            resource,
+            creationParameters,
+            Collections.emptyList(),
+            userAccessUtils.defaultUserAuthRequest());
+    assertEquals(resource, createdDataset);
+
+    // Test idempotency of delete by retrying steps once.
+    Map<String, StepStatus> retrySteps = new HashMap<>();
+    retrySteps.put(DeleteSamResourceStep.class.getName(), StepStatus.STEP_RESULT_FAILURE_RETRY);
+    retrySteps.put(DeleteMetadataStep.class.getName(), StepStatus.STEP_RESULT_FAILURE_RETRY);
+    retrySteps.put(DeleteBigQueryDatasetStep.class.getName(), StepStatus.STEP_RESULT_FAILURE_RETRY);
+    // Do not test lastStepFailure, as this flight has no undo steps, only dismal failure.
+    jobService.setFlightDebugInfoForTest(
+        FlightDebugInfo.newBuilder().doStepFailures(retrySteps).build());
+
+    controlledResourceService.deleteControlledResourceSync(
+        resource.getWorkspaceId(),
+        resource.getResourceId(),
+        userAccessUtils.defaultUserAuthRequest());
+
+    BigQueryCow bqCow = crlService.createWsmSaBigQueryCow();
+    GoogleJsonResponseException getException =
+        assertThrows(
+            GoogleJsonResponseException.class,
+            () -> bqCow.datasets().get(projectId, resource.getDatasetName()).execute());
+    assertEquals(HttpStatus.NOT_FOUND.value(), getException.getStatusCode());
+
+    assertThrows(
+        ResourceNotFoundException.class,
+        () ->
+            controlledResourceService.getControlledResource(
+                workspace.getWorkspaceId(),
+                resource.getResourceId(),
+                userAccessUtils.defaultUserAuthRequest()));
+  }
+
+  @Test
+  @DisabledIfEnvironmentVariable(named = "TEST_ENV", matches = bufferServiceDisabledEnvsRegEx)
+  public void deleteBqDatasetUndo() throws Exception {
+    Workspace workspace = reusableWorkspace();
+    String projectId = workspace.getGcpCloudContext().get().getGcpProjectId();
+
+    String datasetId = "my_test_dataset";
+    String location = "us-central1";
+
+    ApiGcpBigQueryDatasetCreationParameters creationParameters =
+        new ApiGcpBigQueryDatasetCreationParameters().datasetId(datasetId).location(location);
+    ControlledBigQueryDatasetResource resource =
+        ControlledResourceFixtures.makeDefaultControlledBigQueryDatasetResource()
+            .workspaceId(workspace.getWorkspaceId())
+            .accessScope(AccessScopeType.ACCESS_SCOPE_SHARED)
+            .managedBy(ManagedByType.MANAGED_BY_USER)
+            .datasetName(datasetId)
+            .build();
+
+    ControlledBigQueryDatasetResource createdDataset =
+        controlledResourceService.createBqDataset(
+            resource,
+            creationParameters,
+            Collections.emptyList(),
+            userAccessUtils.defaultUserAuthRequest());
+    assertEquals(resource, createdDataset);
+
+    // None of the steps on this flight are undoable, so even with lastStepFailure set to true we
+    // should expect the resource to really be deleted.
+    jobService.setFlightDebugInfoForTest(
+        FlightDebugInfo.newBuilder().lastStepFailure(true).build());
+
+    assertThrows(
+        InvalidResultStateException.class,
+        () ->
+            controlledResourceService.deleteControlledResourceSync(
+                resource.getWorkspaceId(),
+                resource.getResourceId(),
+                userAccessUtils.defaultUserAuthRequest()));
+
+    BigQueryCow bqCow = crlService.createWsmSaBigQueryCow();
+    GoogleJsonResponseException getException =
+        assertThrows(
+            GoogleJsonResponseException.class,
+            () -> bqCow.datasets().get(projectId, resource.getDatasetName()).execute());
+    assertEquals(HttpStatus.NOT_FOUND.value(), getException.getStatusCode());
+
+    assertThrows(
+        ResourceNotFoundException.class,
+        () ->
+            controlledResourceService.getControlledResource(
+                workspace.getWorkspaceId(),
+                resource.getResourceId(),
+                userAccessUtils.defaultUserAuthRequest()));
+  }
+
+  // TODO(PF-469): Add a test verifying that a controlled resource can't be created for an instance
+  // that already exists once we're ensuring cloud URI uniqueness for controlled resources.
 }
