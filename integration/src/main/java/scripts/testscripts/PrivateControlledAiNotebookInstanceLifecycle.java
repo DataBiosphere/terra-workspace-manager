d3abf263ef16e0e1045ae05965d54b65605bd0c2
wchambers <1499870+wchamber@users.noreply.github.com>
2021-05-12 17:59:54 -0400

    PF-716 private controlled AI Notebook instance client test.

--- integration/src/main/java/scripts/testscripts/PrivateControlledAiNotebookInstanceLifecycle.java
+++ integration/src/main/java/scripts/testscripts/PrivateControlledAiNotebookInstanceLifecycle.java
@@ -3,6 +3,8 @@ package scripts.testscripts;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import bio.terra.testrunner.runner.config.TestUserSpecification;
 import bio.terra.workspace.api.ControlledGcpResourceApi;
@@ -14,6 +16,8 @@ import bio.terra.workspace.model.ControlledResourceCommonFields;
 import bio.terra.workspace.model.ControlledResourceIamRole;
 import bio.terra.workspace.model.CreateControlledGcpAiNotebookInstanceRequestBody;
 import bio.terra.workspace.model.CreatedControlledGcpAiNotebookInstanceResult;
+import bio.terra.workspace.model.DeleteControlledGcpAiNotebookInstanceRequest;
+import bio.terra.workspace.model.DeleteControlledGcpAiNotebookInstanceResult;
 import bio.terra.workspace.model.GcpAiNotebookInstanceCreationParameters;
 import bio.terra.workspace.model.GcpAiNotebookInstanceResource;
 import bio.terra.workspace.model.GcpAiNotebookInstanceVmImage;
@@ -24,12 +28,24 @@ import bio.terra.workspace.model.JobReport;
 import bio.terra.workspace.model.ManagedBy;
 import bio.terra.workspace.model.PrivateResourceIamRoles;
 import bio.terra.workspace.model.PrivateResourceUser;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpStatusCodes;
+import com.google.api.services.iam.v1.model.TestIamPermissionsRequest;
+import com.google.api.services.notebooks.v1.AIPlatformNotebooks;
+import com.google.api.services.notebooks.v1.model.Instance;
+import com.google.api.services.notebooks.v1.model.StopInstanceRequest;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scripts.utils.ClientTestUtils;
@@ -37,6 +53,7 @@ import scripts.utils.CloudContextMaker;
 import scripts.utils.WorkspaceAllocateTestScriptBase;
 
 public class PrivateControlledAiNotebookInstanceLifecycle extends WorkspaceAllocateTestScriptBase {
+
   private static final Logger logger =
       LoggerFactory.getLogger(PrivateControlledAiNotebookInstanceLifecycle.class);
 
@@ -62,28 +79,29 @@ public class PrivateControlledAiNotebookInstanceLifecycle extends WorkspaceAlloc
   @Override
   protected void doUserJourney(TestUserSpecification testUser, WorkspaceApi workspaceApi)
       throws Exception {
-    logger.info("test user {}", testUser.userEmail);
+    String projectId = CloudContextMaker.createGcpCloudContext(getWorkspaceId(), workspaceApi);
+
     workspaceApi.grantRole(
         new GrantRoleRequestBody().memberEmail(resourceUser.userEmail),
         getWorkspaceId(),
         IamRole.WRITER);
-    logger.info("Added {} as a writer to workspace {}", resourceUser.userEmail, getWorkspaceId());
     workspaceApi.grantRole(
         new GrantRoleRequestBody().memberEmail(otherWorkspaceUser.userEmail),
         getWorkspaceId(),
         IamRole.WRITER);
-    logger.info(
-        "Added {} as a writer to workspace {}", otherWorkspaceUser.userEmail, getWorkspaceId());
-
-    String projectId = CloudContextMaker.createGcpCloudContext(getWorkspaceId(), workspaceApi);
-    logger.info("Created project {}", projectId);
 
     ControlledGcpResourceApi resourceUserApi =
         ClientTestUtils.getControlledGcpResourceClient(resourceUser, server);
     CreatedControlledGcpAiNotebookInstanceResult creationResult =
         createPrivateNotebook(resourceUser, resourceUserApi);
-    logger.info("Initiated notebook instance creation. Polling...");
-    creationResult = pollUntilComplete(creationResult, resourceUserApi);
+    logger.info("Instance creation started for instanceId {}",
+        creationResult.getAiNotebookInstance().getAttributes().getInstanceId());
+    String creationJobId = creationResult.getJobReport().getId();
+    creationResult = ClientTestUtils.pollWhileRunning(
+        creationResult,
+        () -> resourceUserApi.getCreateAiNotebookInstanceResult(getWorkspaceId(), creationJobId),
+        CreatedControlledGcpAiNotebookInstanceResult::getJobReport,
+        Duration.ofSeconds(10));
     assertEquals(JobReport.StatusEnum.SUCCEEDED, creationResult.getJobReport().getStatus());
     logger.info(
         "Creation succeeded for instanceId {}",
@@ -94,12 +112,55 @@ public class PrivateControlledAiNotebookInstanceLifecycle extends WorkspaceAlloc
     GcpAiNotebookInstanceResource resource =
         resourceUserApi.getAiNotebookInstance(getWorkspaceId(), resourceId);
     assertEquals(instanceId, resource.getAttributes().getInstanceId());
-    assertEquals(
-        resource.getAttributes().getInstanceId(),
+    assertEquals(instanceId,
         creationResult.getAiNotebookInstance().getAttributes().getInstanceId());
 
-    // TODO(PF-626): Test cloud permissions for the users.
-    // TODO(PF-712): Test notebook instance deletion.
+    String instanceName = String
+        .format("projects/%s/locations/%s/instances/%s", resource.getAttributes().getProjectId(),
+            resource.getAttributes().getLocation(), resource.getAttributes().getInstanceId());
+    AIPlatformNotebooks userNotebooks = ClientTestUtils.getAIPlatformNotebooksClient(resourceUser);
+
+    Instance instance = userNotebooks.projects().locations().instances().get(instanceName)
+        .execute();
+
+    // TODO(PF-765): Assert that the other user does not have proxy access once we don't have
+    // project level service account permissions.
+    assertUserHasProxyAccess(resourceUser, instance, resource.getAttributes().getProjectId());
+    // The user should be able to stop their notebook.
+    userNotebooks.projects().locations().instances().stop(instanceName, new StopInstanceRequest());
+
+    // The user should not be able to directly delete their notebook.
+    GoogleJsonResponseException directDeleteForbidden = assertThrows(
+        GoogleJsonResponseException.class, () ->
+            userNotebooks.projects().locations().instances().delete(instanceName).execute());
+    assertEquals(HttpStatus.SC_FORBIDDEN, directDeleteForbidden.getStatusCode());
+
+    // Delete the AI Notebook through WSM.
+    DeleteControlledGcpAiNotebookInstanceResult deleteResult = resourceUserApi
+        .deleteAiNotebookInstance(new DeleteControlledGcpAiNotebookInstanceRequest()
+                .jobControl(new JobControl().id(UUID.randomUUID().toString())), getWorkspaceId(),
+            resourceId);
+    String deleteJobId = deleteResult.getJobReport().getId();
+    deleteResult = ClientTestUtils.pollWhileRunning(deleteResult,
+        () -> resourceUserApi.getDeleteAiNotebookInstanceResult(getWorkspaceId(), deleteJobId),
+        DeleteControlledGcpAiNotebookInstanceResult::getJobReport,
+        Duration.ofSeconds(10));
+
+    assertNull(deleteResult.getErrorReport());
+    assertEquals(JobReport.StatusEnum.SUCCEEDED, deleteResult.getJobReport().getStatus());
+
+    // Verify the notebook was deleted from WSM metadata.
+    ApiException notebookIsMissing =
+        assertThrows(
+            ApiException.class,
+            () -> resourceUserApi.getAiNotebookInstance(getWorkspaceId(), resourceId));
+    assertEquals(HttpStatus.SC_NOT_FOUND, notebookIsMissing.getCode());
+
+    // Verify the notebook was deleted form GCP.
+    GoogleJsonResponseException notebookNotFound =
+        assertThrows(GoogleJsonResponseException.class, () ->
+            userNotebooks.projects().locations().instances().get(instanceName).execute());
+    assertEquals(HttpStatus.SC_NOT_FOUND, notebookNotFound.getStatusCode());
   }
 
   private CreatedControlledGcpAiNotebookInstanceResult createPrivateNotebook(
@@ -139,20 +200,29 @@ public class PrivateControlledAiNotebookInstanceLifecycle extends WorkspaceAlloc
     return resourceApi.createAiNotebookInstance(body, getWorkspaceId());
   }
 
-  /** Polls job for the notebook instance creation until it finishes or we give up. */
-  private CreatedControlledGcpAiNotebookInstanceResult pollUntilComplete(
-      CreatedControlledGcpAiNotebookInstanceResult result, ControlledGcpResourceApi resourceApi)
-      throws ApiException, InterruptedException {
-    String creationJobId = result.getJobReport().getId();
-    Instant deadline = Instant.now().plus(Duration.ofMinutes(12));
-    while (ClientTestUtils.jobIsRunning(result.getJobReport())) {
-      if (Instant.now().isAfter(deadline)) {
-        throw new InterruptedException(
-            "Timed out waiting for notebok instance creation to finish.");
-      }
-      TimeUnit.SECONDS.sleep(20);
-      result = resourceApi.getCreateAiNotebookInstanceResult(getWorkspaceId(), creationJobId);
-    }
-    return result;
+  /**
+   * Assert that the user has access to the Notebook through the proxy with a service account.
+   * <p>We can't directly test that we can go through the proxy to the Jupyter notebook without a
+   * real Google user auth flow, so we check the necessary ingredients instead.
+   */
+  private static void assertUserHasProxyAccess(TestUserSpecification user, Instance instance,
+      String projectId) throws GeneralSecurityException, IOException {
+    // Test that the user has access to the notebook with a service account through proxy mode.
+    // git secrets gets a false positive if 'service_account' is double quoted.
+    assertThat(instance.getMetadata(), Matchers.hasEntry("proxy-mode", "service_" + "account"));
+
+    // The user needs to have the actAs permission on the service account.
+    String actAsPermission = "iam.serviceAccounts.actAs";
+    String serviceAccountName = String
+        .format("projects/%s/serviceAccounts/%s", projectId, instance.getServiceAccount());
+    assertThat(ClientTestUtils.getGcpIamClient(user)
+            .projects()
+            .serviceAccounts()
+            .testIamPermissions(
+                serviceAccountName,
+                new TestIamPermissionsRequest().setPermissions(List.of(actAsPermission)))
+            .execute()
+            .getPermissions(),
+        Matchers.contains(actAsPermission));
   }
 }
--- integration/src/main/java/scripts/utils/ClientTestUtils.java
+++ integration/src/main/java/scripts/utils/ClientTestUtils.java
@@ -2,6 +2,7 @@ package scripts.utils;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.in;
 
 import bio.terra.testrunner.common.utils.AuthenticationUtils;
 import bio.terra.testrunner.runner.config.ServerSpecification;
@@ -11,10 +12,16 @@ import bio.terra.workspace.api.ReferencedGcpResourceApi;
 import bio.terra.workspace.api.ResourceApi;
 import bio.terra.workspace.api.WorkspaceApi;
 import bio.terra.workspace.client.ApiClient;
+import bio.terra.workspace.client.ApiException;
 import bio.terra.workspace.model.IamRole;
 import bio.terra.workspace.model.JobReport;
 import bio.terra.workspace.model.RoleBindingList;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpStatusCodes;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.iam.v1.Iam;
+import com.google.api.services.notebooks.v1.AIPlatformNotebooks;
+import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.bigquery.BigQuery;
@@ -23,8 +30,11 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.common.base.Strings;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.time.Duration;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Function;
 import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,18 +47,12 @@ public class ClientTestUtils {
   public static final String RESOURCE_NAME_PREFIX = "terratest";
   private static final Logger logger = LoggerFactory.getLogger(ClientTestUtils.class);
 
-  // Required scopes for client tests include the usual login scopes.
-  // We need additional scopes for validating access to cloud resources,
-  // including:
-  // - cloud platform
-  // - bigquery
-  // - gcs
+  // Required scopes for client tests include the usual login scopes and GCP scope.
   private static final List<String> TEST_USER_SCOPES =
       List.of("openid", "email", "profile", "https://www.googleapis.com/auth/cloud-platform");
-  // "https://www.googleapis.com/auth/bigquery")
-  // "https://www.googleapis.com/auth/devstorage.full_control");
 
-  private ClientTestUtils() {}
+  private ClientTestUtils() {
+  }
 
   /**
    * Build the Workspace Manager Service API client object for the given server specification. It is
@@ -84,7 +88,7 @@ public class ClientTestUtils {
    * specifications. The test user's token is always refreshed
    *
    * @param testUser the test user whose credentials are supplied to the API client object
-   * @param server the server we are testing against
+   * @param server   the server we are testing against
    * @return the API client object for this user
    */
   public static ApiClient getClientForTestUser(
@@ -105,6 +109,24 @@ public class ClientTestUtils {
     }
 
     return buildClient(accessToken, server);
+  }
+
+  public static AIPlatformNotebooks getAIPlatformNotebooksClient(TestUserSpecification testUser)
+      throws GeneralSecurityException, IOException {
+    return new AIPlatformNotebooks(
+        GoogleNetHttpTransport.newTrustedTransport(),
+        JacksonFactory.getDefaultInstance(),
+        new HttpCredentialsAdapter(
+            AuthenticationUtils.getDelegatedUserCredential(testUser, TEST_USER_SCOPES)));
+  }
+
+  public static Iam getGcpIamClient(TestUserSpecification testUser)
+      throws GeneralSecurityException, IOException {
+    return new Iam(
+        GoogleNetHttpTransport.newTrustedTransport(),
+        JacksonFactory.getDefaultInstance(),
+        new HttpCredentialsAdapter(
+            AuthenticationUtils.getDelegatedUserCredential(testUser, TEST_USER_SCOPES)));
   }
 
   public static Storage getGcpStorageClient(TestUserSpecification testUser, String projectId)
@@ -188,8 +210,8 @@ public class ClientTestUtils {
    * Checks if a user email is in a role binding list
    *
    * @param roleBindings - list of role bindings, as retrieved via getRoles()
-   * @param userEmail - user to check for
-   * @param role - role to check
+   * @param userEmail    - user to check for
+   * @param role         - role to check
    * @return True if the role binding list contains a binding for a given user and Iam Role.
    */
   public static boolean containsBinding(
@@ -202,7 +224,41 @@ public class ClientTestUtils {
     return jobReport.getStatus().equals(JobReport.StatusEnum.RUNNING);
   }
 
-  /** @return a generated unique resource name consisting of letters, numbers, and underscores. */
+  /**
+   * An interface for an arbitrary workspace operation that throws an {@link ApiException}.
+   */
+  @FunctionalInterface
+  public interface WorkspaceOperation<T> {
+
+    T apply() throws ApiException;
+  }
+
+  /**
+   * Polls a workspace API operation as long as the job is running.
+   *
+   * @param <T>                the result type of the async operation.
+   * @param initialValue       the first result to use to poll
+   * @param operation          a function for the workspace async operation to execute
+   * @param jobReportExtractor a function for getting the {@link JobReport} from the result
+   * @param pollInterval       how long to sleep between polls.
+   */
+
+  public static <T> T pollWhileRunning(
+      T initialValue,
+      WorkspaceOperation<T> operation,
+      Function<T, JobReport> jobReportExtractor,
+      Duration pollInterval) throws InterruptedException, ApiException {
+    T result = initialValue;
+    while (jobIsRunning(jobReportExtractor.apply(result))) {
+      Thread.sleep(pollInterval.toMillis());
+      result = operation.apply();
+    }
+    return result;
+  }
+
+  /**
+   * @return a generated unique resource name consisting of letters, numbers, and underscores.
+   */
   public static String generateCloudResourceName() {
     String name = RESOURCE_NAME_PREFIX + UUID.randomUUID().toString();
     return name.replace("-", "_");
