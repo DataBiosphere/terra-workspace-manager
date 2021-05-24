aff1804edc3caf8c259d6103b07135ff86cd0e57
ddietterich <ddietterich@verily.com>
2021-05-24 13:47:51 -0400

    Fix9

--- .github/actions/run-sql-script/entrypoint.sh
+++ .github/actions/run-sql-script/entrypoint.sh
@@ -10,6 +10,8 @@
 #  CLOUDSQL_SA_FILE - location in the container where the invoker put the cloudsql SA JSON credentials file
 #  SCRIPT      - location in the container where the invoker put the Postgresql script to execute on the database
 
+echo "We are inside of a running entrypoing.sh"
+
 DB_USERNAME=$1
 DB_PASSWORD=$2
 DB_NAME=$3
@@ -20,15 +22,16 @@ SCRIPT=$6
 # We can make this an arg if need be, but I think the scope works fine here inside the container
 port=5434
 
-# Launch the cloud sql proxy and wait a bit for it to start
+echo "Launching the CloudSQL proxy"
 cloud_sql_proxy -instances=${DB_INSTANCE}=tcp:$port &
 proxypid=$!
 sleep 5
 
-# Run the script through psql
+echo "Running the script via psql"
 PGPASSWORD="${DB_PASSWORD}" psql "host=127.0.0.1 port=$port sslmode=disable dbname=${DB_NAME} user=${DB_USERNAME}" -f ${SCRIPT}
 
-# Stop the proxy - not sure this is needed in the docker context. It helps when testing the script.
+# Not sure we need to stop the proxy in the docker context. It helps when testing the script.
 sleep 2
+echo "Stopping the CloudSQL proxy"
 kill -9 $proxypid
 
