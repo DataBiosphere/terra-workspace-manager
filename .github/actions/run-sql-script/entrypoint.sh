3a23f0e22713c88649b6217c6d9af54ffd9df8d7
ddietterich <ddietterich@verily.com>
2021-05-25 13:31:03 -0400

    Switch run-sql-script to composite

--- .github/actions/run-sql-script/entrypoint.sh
+++ .github/actions/run-sql-script/entrypoint.sh
@@ -7,7 +7,7 @@
 #  DB_PASSWORD - password to login to CloudSQL
 #  DB_NAME     - database name to connect to
 #  DB_INSTANCE - full connection name: <project>:<region>:<cloudsql-instance>
-#  CLOUDSQL_SA_FILE - location in the container where the invoker put the cloudsql SA JSON credentials file
+#  SQLPROXY_SA_FILE - path to the SA credentials to use for the proxy
 #  SCRIPT      - location in the container where the invoker put the Postgresql script to execute on the database
 
 echo "We are inside of a running entrypoing.sh"
@@ -16,15 +16,22 @@ DB_USERNAME=$1
 DB_PASSWORD=$2
 DB_NAME=$3
 DB_INSTANCE=$4
-CLOUDSQL_SA_FILE=$5
+SQLPROXY_SA_FILE=$5
 SCRIPT=$6
+
+creddir=$(cd "${SQLPROXY_SA_FILE%/*}" && pwd)
+credfile="${SQLPROXY_SA_FILE##*/}"
 
 # We can make this an arg if need be, but I think the scope works fine here inside the container
 port=5434
 
 echo "Launching the CloudSQL proxy"
-cloud_sql_proxy -instances=${DB_INSTANCE}=tcp:$port &
-proxypid=$!
+dod=$(docker run -d \
+      -v ${creddir}:/config \
+      -p 127.0.0.1:${port}:${port} \
+      gcr.io/cloudsql-docker/gce-proxy:latest /cloud_sql_proxy \
+      -instances=${DB_INSTANCE} \
+      -credential_file=/config/${credfile})
 sleep 5
 
 echo "Running the script via psql"
@@ -33,5 +40,5 @@ PGPASSWORD="${DB_PASSWORD}" psql "host=127.0.0.1 port=$port sslmode=disable dbna
 # Not sure we need to stop the proxy in the docker context. It helps when testing the script.
 sleep 2
 echo "Stopping the CloudSQL proxy"
-kill -9 $proxypid
+docker kill $dod
 
