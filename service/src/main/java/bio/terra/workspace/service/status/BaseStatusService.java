13da600dd1998cb207dfa2d37e9357f8550772a6
ddietterich <ddietterich@verily.com>
2021-06-29 14:41:29 -0400

    bump test

--- service/src/main/java/bio/terra/workspace/service/status/BaseStatusService.java
+++ service/src/main/java/bio/terra/workspace/service/status/BaseStatusService.java
@@ -12,8 +12,6 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// USELESS CHANGE TO ALLOW A COMMIt
-
 /*
  This class is factored out with the idea that it could be moved to Terra Common Library.
 
