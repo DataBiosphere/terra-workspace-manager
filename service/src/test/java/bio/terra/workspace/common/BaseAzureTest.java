8ad2d61b426db4f37f1ebe2559ee56ee6aaa01cb
bumptagbot <robot@terra.team>
2021-09-13 12:32:17 -0400

    Merge from azure-poc

--- service/src/test/java/bio/terra/workspace/common/BaseAzureTest.java
+++ service/src/test/java/bio/terra/workspace/common/BaseAzureTest.java
@@ -3,7 +3,9 @@ package bio.terra.workspace.common;
 import org.junit.jupiter.api.Tag;
 import org.springframework.test.context.ActiveProfiles;
 
-/** Base class for azure tests. Treat these as connected tests: connected to Azure */
+/**
+ * Base class for azure tests. Treat these as connected tests: connected to Azure
+ */
 @Tag("azure")
 @ActiveProfiles("azure-test")
 public class BaseAzureTest extends BaseTest {}
