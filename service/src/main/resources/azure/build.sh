e157584b38635b523536217f9a4b2261639a17cb
bumptagbot <robot@terra.team>
2021-09-28 11:30:10 -0400

    Make codacy happy

--- service/src/main/resources/azure/build.sh
+++ service/src/main/resources/azure/build.sh
@@ -2,10 +2,9 @@
 gittop=$(git rev-parse --show-toplevel)
 target=$gittop/build/azure
 source=$gittop/service/src/main/resources/azure
-mkdir -p $target
-rm -f $target/app.zip
-cp ${source}/terralogo.png ${target}/.
-cd ${source}/marketplaceTemplate
-zip -vr ${target}/app.zip . -x "*.DS_Store"
+mkdir -p "${target}"
+rm -f "${target}/app.zip"
+cp "${source}/terralogo.png" "${target}/."
+cd "${source}/marketplaceTemplate"
+zip -vr "${target}/app.zip" . -x "*.DS_Store"
 cd ..
-
