30b52099bba5dc5769eda17b78ce54d02ebb53f9
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2022-02-16 11:57:49 -0500

    update to use env var

---
+++ service/src/main/java/bio/terra/workspace/app/configuration/external/CliConfiguration.java
@@ -0,0 +1,21 @@
+package bio.terra.workspace.app.configuration.external;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix="workspace.cli")
+public class CliConfiguration {
+
+  private String oldestSupportedVersion;
+
+  public String getOldestSupportedVersion() {
+    return oldestSupportedVersion;
+  }
+
+  public void setOldestSupportedVersion(String oldestSupportedVersion) {
+    this.oldestSupportedVersion = oldestSupportedVersion;
+  }
+}
