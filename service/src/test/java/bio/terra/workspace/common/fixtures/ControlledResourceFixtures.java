323b7e72437fea710fbab307be31085c22951a87
Sergiy Getlin <91149690+sergiygetlin@users.noreply.github.com>
2022-05-06 14:25:28 -0700

    TOAZ-128 Create VM endpoint should handle version parameter for the VM image (#641)
    Co-authored-by: Sergiy Getlin <sergiygetlin@microsoft.com>

--- service/src/test/java/bio/terra/workspace/common/fixtures/ControlledResourceFixtures.java
+++ service/src/test/java/bio/terra/workspace/common/fixtures/ControlledResourceFixtures.java
@@ -12,6 +12,7 @@ import bio.terra.workspace.generated.model.ApiAzureVmCustomScriptExtension;
 import bio.terra.workspace.generated.model.ApiAzureVmCustomScriptExtensionSetting;
 import bio.terra.workspace.generated.model.ApiAzureVmCustomScriptExtensionTag;
 import bio.terra.workspace.generated.model.ApiAzureVmImage;
+import bio.terra.workspace.generated.model.ApiAzureVmUser;
 import bio.terra.workspace.generated.model.ApiGcpAiNotebookInstanceCreationParameters;
 import bio.terra.workspace.generated.model.ApiGcpAiNotebookInstanceVmImage;
 import bio.terra.workspace.generated.model.ApiGcpBigQueryDatasetCreationParameters;
@@ -175,12 +176,13 @@ public class ControlledResourceFixtures {
         .name(uniqueAzureName(AZURE_VM_NAME_PREFIX))
         .region("westcentralus")
         .vmSize(VirtualMachineSizeTypes.STANDARD_D2S_V3.toString())
-        // TODO: it'd be nice to support standard Linux OSes in addition to custom image URIs.
-        // The below image is a Jupyter image and should be stable.
         .vmImage(
             new ApiAzureVmImage()
-                .uri(
-                    "/subscriptions/3efc5bdf-be0e-44e7-b1d7-c08931e3c16c/resourceGroups/mrg-qi-1-preview-20210517084351/providers/Microsoft.Compute/galleries/msdsvm/images/customized_ms_dsvm/versions/0.1.0"))
+                .publisher("microsoft-dsvm")
+                .offer("ubuntu-1804")
+                .sku("1804-gen2")
+                .version("latest"))
+        .vmUser(new ApiAzureVmUser().name("noname").password("StrongP@ssowrd123!!!"))
         .ipId(UUID.randomUUID())
         .diskId(UUID.randomUUID())
         .networkId(UUID.randomUUID())
