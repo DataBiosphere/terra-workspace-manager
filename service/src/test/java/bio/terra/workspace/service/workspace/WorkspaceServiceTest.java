c2f72505390c2e7826475f3934a8e1ce5c657ceb
yuhuyoyo <yuhuyoyo@google.com>
2022-07-08 18:05:16 -0400

    fix test

--- service/src/test/java/bio/terra/workspace/service/workspace/WorkspaceServiceTest.java
+++ service/src/test/java/bio/terra/workspace/service/workspace/WorkspaceServiceTest.java
@@ -405,7 +405,7 @@ class WorkspaceServiceTest extends BaseConnectedTest {
     var updatedDateAfterWorkspaceUpdate = workspaceActivityLogDao.getLastUpdatedDate(workspaceUuid);
     assertTrue(lastUpdatedDate.get().isBefore(updatedDateAfterWorkspaceUpdate.get()));
     assertEquals(
-        updatedWorkspaceAndAttributes.lastUpdatedDate().get(), updatedDateAfterWorkspaceUpdate);
+        updatedWorkspaceAndAttributes.lastUpdatedDate().get(), updatedDateAfterWorkspaceUpdate.get());
 
     assertEquals(userFacingId, updatedWorkspace.getUserFacingId());
     assertTrue(updatedWorkspace.getDisplayName().isPresent());
