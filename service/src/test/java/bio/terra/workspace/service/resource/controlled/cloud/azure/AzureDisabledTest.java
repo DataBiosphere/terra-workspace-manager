597a860ee0065dc2b5355707cee093e8f7fa5d33
Sergiy Getlin <91149690+sergiygetlin@users.noreply.github.com>
2022-07-11 20:30:46 -0700

    Revert "spotlessapply"
    This reverts commit 52d47cd183fb2d6cdd59582579add8237dd1fae6.

--- service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/AzureDisabledTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/AzureDisabledTest.java
@@ -32,16 +32,17 @@ import bio.terra.workspace.service.workspace.model.WorkspaceStage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.UUID;
 import org.apache.http.HttpStatus;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.annotation.DirtiesContext;
 
 // Test to make sure things properly do not work when Azure feature is not enabled
 @AutoConfigureMockMvc
-// We are modifying application context here. Need to clean up once tests are done.
+//We are modifying application context here. Need to clean up once tests are done.
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class AzureDisabledTest extends BaseConnectedTest {
   @Autowired private MockMvc mockMvc;
@@ -53,7 +54,7 @@ public class AzureDisabledTest extends BaseConnectedTest {
 
   @BeforeEach
   public void setUp() {
-    // explicitly disable Azure feature regardless of the configuration files
+    //explicitly disable Azure feature regardless of the configuration files
     featureConfiguration.setAzureEnabled(false);
   }
 
