0f5d3feee97b7b8f90c50b328a2999228cf6ec5a
zloery <6668057+zloery@users.noreply.github.com>
2022-08-03 15:00:50 -0400

    add comment for test

--- service/src/test/java/bio/terra/workspace/service/iam/WsmIamRoleTest.java
+++ service/src/test/java/bio/terra/workspace/service/iam/WsmIamRoleTest.java
@@ -8,6 +8,12 @@ import org.junit.jupiter.api.Test;
 
 public class WsmIamRoleTest extends BaseUnitTest {
 
+  /**
+   * This test looks trivial, but it enforces that WsmIamRole.fromSam must handle unknown values
+   * without throwing. Sam can sometimes return roles that WSM doesn't know about (e.g. if WSM lists
+   * the roles on a RAWLS_WORKSPACE stage workspace), and WSM should silently ignore them instead
+   * of throwing exceptions.
+   */
   @Test
   public void missingApiModelReturnsNull() {
     assertNull(WsmIamRole.fromSam("FAKE_VALUE"));
