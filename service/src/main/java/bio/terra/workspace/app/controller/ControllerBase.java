02cf10c24775a958d642a53d5af594c1a77b10ff
Mike Neville <mikenev@google.com>
2022-08-04 15:31:57 -0400

    PF-1218: Stop accepting a list of roles on the private resource user

--- service/src/main/java/bio/terra/workspace/app/controller/ControllerBase.java
+++ service/src/main/java/bio/terra/workspace/app/controller/ControllerBase.java
@@ -4,7 +4,6 @@ import bio.terra.common.exception.ValidationException;
 import bio.terra.workspace.common.exception.InternalLogicException;
 import bio.terra.workspace.common.utils.ControllerValidationUtils;
 import bio.terra.workspace.generated.model.ApiControlledResourceCommonFields;
-import bio.terra.workspace.generated.model.ApiControlledResourceIamRole;
 import bio.terra.workspace.generated.model.ApiJobReport;
 import bio.terra.workspace.generated.model.ApiJobReport.StatusEnum;
 import bio.terra.workspace.generated.model.ApiPrivateResourceUser;
@@ -17,10 +16,7 @@ import bio.terra.workspace.service.iam.model.SamConstants;
 import bio.terra.workspace.service.resource.controlled.model.AccessScopeType;
 import bio.terra.workspace.service.resource.controlled.model.ManagedByType;
 import bio.terra.workspace.service.resource.controlled.model.PrivateUserRole;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -147,47 +143,21 @@ public class ControllerBase {
 
           // Translate the incoming role list into our internal model form
           // This also validates that the incoming API model values are correct.
-          List<ControlledResourceIamRole> roles;
-          ApiControlledResourceIamRole apiControlledResourceIamRole =
-              commonFields.getPrivateResourceUser().getPrivateResourceIamRole();
+          var iamRole =
+              ControlledResourceIamRole.fromApiModel(
+                  commonFields.getPrivateResourceUser().getPrivateResourceIamRole());
 
-          if (apiControlledResourceIamRole != null) {
-            roles = new ArrayList<ControlledResourceIamRole>();
-            roles.add(
-                ControlledResourceIamRole.fromApiModel(
-                    commonFields.getPrivateResourceUser().getPrivateResourceIamRole()));
-          } else {
-            // PF-1218 - Once UI & CLI are no longer sending this for application managed resources,
-            // remove this block.
-            roles =
-                commonFields.getPrivateResourceUser().getPrivateResourceIamRoles().stream()
-                    .map(ControlledResourceIamRole::fromApiModel)
-                    .collect(Collectors.toList());
-            if (roles.isEmpty()) {
-              throw new ValidationException(
-                  "You must specify at least one role when you specify PrivateResourceIamRoles");
-            }
-          }
-
-          // The legal options for the assigned user of an application is READER
-          // or WRITER. EDITOR is not allowed. We take the "max" of READER and WRITER.
-          var maxRole = ControlledResourceIamRole.READER;
-          for (ControlledResourceIamRole role : roles) {
-            if (role == ControlledResourceIamRole.WRITER) {
-              if (maxRole == ControlledResourceIamRole.READER) {
-                maxRole = role;
-              }
-            } else if (role != ControlledResourceIamRole.READER) {
-              throw new ValidationException(
-                  "For application private controlled resources, only READER and WRITER roles are allowed. Found "
-                      + role.toApiModel());
-            }
+          if (iamRole != ControlledResourceIamRole.READER
+              && iamRole != ControlledResourceIamRole.WRITER) {
+            throw new ValidationException(
+                "For application private controlled resources, only READER and WRITER roles are allowed. Found "
+                    + iamRole.toApiModel());
           }
 
           return new PrivateUserRole.Builder()
               .present(true)
               .userEmail(userEmail)
-              .role(maxRole)
+              .role(iamRole)
               .build();
         }
 
