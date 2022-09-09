7e9cb89b9e3d957b15c159d9d2331124aaa29ac0
yuhuyoyo <yuhuyoyo@google.com>
2022-09-09 12:11:16 -0400

    not done yet

--- service/src/test/java/bio/terra/workspace/app/configuration/external/controller/FolderApiControllerTest.java
+++ service/src/test/java/bio/terra/workspace/app/configuration/external/controller/FolderApiControllerTest.java
@@ -1,9 +1,35 @@
 package bio.terra.workspace.app.configuration.external.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
 import bio.terra.workspace.common.BaseUnitTest;
+import bio.terra.workspace.service.iam.AuthenticatedUserRequest;
+import bio.terra.workspace.service.iam.SamService;
+import bio.terra.workspace.service.iam.model.SamConstants.SamResource;
+import bio.terra.workspace.service.iam.model.SamConstants.SamSpendProfileAction;
+import bio.terra.workspace.service.iam.model.WsmIamRole;
+import java.util.List;
+import java.util.Optional;
+import org.broadinstitute.dsde.workbench.client.sam.model.UserStatusInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 public class FolderApiControllerTest extends BaseUnitTest {
+  private static final AuthenticatedUserRequest USER_REQUEST =
+      new AuthenticatedUserRequest(
+          "fake@email.com", "subjectId123456", Optional.of("ThisIsNotARealBearerToken"));
+
   @Autowired MockMvc mockMvc;
+
+  @MockBean SamService mockSamService;
+
+  @Test
+  public void createFolder() {
+
+  }
 }
