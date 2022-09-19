5a5595c652844603aa998574549ec0ec8ca8f086
giventocode <3589801+giventocode@users.noreply.github.com>
2022-09-19 12:27:25 -0400

    spotless

--- service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/vm/CreateAzureNetworkInterfaceStepTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/vm/CreateAzureNetworkInterfaceStepTest.java
@@ -29,97 +29,97 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("unit")
 public class CreateAzureNetworkInterfaceStepTest extends BaseUnitTest {
 
-    @Mock private NetworkManager networkManager;
+  @Mock private NetworkManager networkManager;
 
-    @Mock private AzureConfiguration azureConfiguration;
-    @Mock private CrlService crlService;
-    @Mock private ControlledAzureVmResource resource;
+  @Mock private AzureConfiguration azureConfiguration;
+  @Mock private CrlService crlService;
+  @Mock private ControlledAzureVmResource resource;
 
-    @Mock private ResourceDao resourceDao;
+  @Mock private ResourceDao resourceDao;
 
-    @Mock private LandingZoneApiDispatch landingZoneApiDispatch;
+  @Mock private LandingZoneApiDispatch landingZoneApiDispatch;
 
-    @Mock private ControlledAzureNetworkResource controlledAzureNetworkResource;
+  @Mock private ControlledAzureNetworkResource controlledAzureNetworkResource;
 
-    @Mock private Networks networks;
+  @Mock private Networks networks;
 
-    @Mock private AzureCloudContext azureCloudContext;
+  @Mock private AzureCloudContext azureCloudContext;
 
-    private final String STUB_MRG = "RG";
+  private final String STUB_MRG = "RG";
 
-    private final String STUB_SUBNET = "SUBNET";
+  private final String STUB_SUBNET = "SUBNET";
 
-    private CreateAzureNetworkInterfaceStep networkInterfaceStep;
+  private CreateAzureNetworkInterfaceStep networkInterfaceStep;
 
-    @Mock private Network armNetwork;
+  @Mock private Network armNetwork;
 
-    @Mock private Subnet armSubnet;
+  @Mock private Subnet armSubnet;
 
-    @BeforeEach
-    void setUp() {
-        networkInterfaceStep =
-                new CreateAzureNetworkInterfaceStep(
-                        azureConfiguration, crlService, resource, resourceDao, landingZoneApiDispatch);
-        when(networkManager.networks()).thenReturn(networks);
-        var subnets = new HashMap<String, Subnet>();
-        subnets.put(STUB_SUBNET, armSubnet);
-        when(armNetwork.subnets()).thenReturn(subnets);
-    }
+  @BeforeEach
+  void setUp() {
+    networkInterfaceStep =
+        new CreateAzureNetworkInterfaceStep(
+            azureConfiguration, crlService, resource, resourceDao, landingZoneApiDispatch);
+    when(networkManager.networks()).thenReturn(networks);
+    var subnets = new HashMap<String, Subnet>();
+    subnets.put(STUB_SUBNET, armSubnet);
+    when(armNetwork.subnets()).thenReturn(subnets);
+  }
 
-    @Test
-    void getExistingNetworkResources_networkIdIsProvided_returnsWorkspaceNetwork() {
-        var networkId = UUID.randomUUID();
-        var workspaceId = UUID.randomUUID();
+  @Test
+  void getExistingNetworkResources_networkIdIsProvided_returnsWorkspaceNetwork() {
+    var networkId = UUID.randomUUID();
+    var workspaceId = UUID.randomUUID();
 
-        setUpNetworkInWorkspaceInteractionChain(networkId, workspaceId);
+    setUpNetworkInWorkspaceInteractionChain(networkId, workspaceId);
 
-        NetworkSubnetPair result =
-                networkInterfaceStep.getExistingNetworkResources(azureCloudContext, networkManager);
+    NetworkSubnetPair result =
+        networkInterfaceStep.getExistingNetworkResources(azureCloudContext, networkManager);
 
-        assertThat(result.network(), equalTo(armNetwork));
-        assertThat(result.subnet(), equalTo(armSubnet));
-    }
+    assertThat(result.network(), equalTo(armNetwork));
+    assertThat(result.subnet(), equalTo(armSubnet));
+  }
 
-    @Test
-    void getExistingNetworkResources_networkIdIsNotProvided_returnsLZNetwork() {
-        var networkId = UUID.randomUUID();
-        var workspaceId = UUID.randomUUID();
+  @Test
+  void getExistingNetworkResources_networkIdIsNotProvided_returnsLZNetwork() {
+    var networkId = UUID.randomUUID();
+    var workspaceId = UUID.randomUUID();
 
-        setUpNetworkInLZInteractionChain(networkId, workspaceId);
+    setUpNetworkInLZInteractionChain(networkId, workspaceId);
 
-        NetworkSubnetPair result =
-                networkInterfaceStep.getExistingNetworkResources(azureCloudContext, networkManager);
+    NetworkSubnetPair result =
+        networkInterfaceStep.getExistingNetworkResources(azureCloudContext, networkManager);
 
-        assertThat(result.network(), equalTo(armNetwork));
-        assertThat(result.subnet(), equalTo(armSubnet));
-    }
+    assertThat(result.network(), equalTo(armNetwork));
+    assertThat(result.subnet(), equalTo(armSubnet));
+  }
 
-    private void setUpNetworkInWorkspaceInteractionChain(UUID networkId, UUID workspaceId) {
-        when(azureCloudContext.getAzureResourceGroupId()).thenReturn(STUB_MRG);
-        when(resource.getNetworkId()).thenReturn(networkId);
-        when(resource.getWorkspaceId()).thenReturn(workspaceId);
-        when(resourceDao.getResource(workspaceId, networkId))
-                .thenReturn(controlledAzureNetworkResource);
-        when(controlledAzureNetworkResource.castByEnum(WsmResourceType.CONTROLLED_AZURE_NETWORK))
-                .thenReturn(controlledAzureNetworkResource);
-        when(controlledAzureNetworkResource.getNetworkName()).thenReturn(networkId.toString());
-        when(controlledAzureNetworkResource.getSubnetName()).thenReturn(STUB_SUBNET);
-        when(networks.getByResourceGroup(STUB_MRG, networkId.toString())).thenReturn(armNetwork);
-    }
+  private void setUpNetworkInWorkspaceInteractionChain(UUID networkId, UUID workspaceId) {
+    when(azureCloudContext.getAzureResourceGroupId()).thenReturn(STUB_MRG);
+    when(resource.getNetworkId()).thenReturn(networkId);
+    when(resource.getWorkspaceId()).thenReturn(workspaceId);
+    when(resourceDao.getResource(workspaceId, networkId))
+        .thenReturn(controlledAzureNetworkResource);
+    when(controlledAzureNetworkResource.castByEnum(WsmResourceType.CONTROLLED_AZURE_NETWORK))
+        .thenReturn(controlledAzureNetworkResource);
+    when(controlledAzureNetworkResource.getNetworkName()).thenReturn(networkId.toString());
+    when(controlledAzureNetworkResource.getSubnetName()).thenReturn(STUB_SUBNET);
+    when(networks.getByResourceGroup(STUB_MRG, networkId.toString())).thenReturn(armNetwork);
+  }
 
-    private void setUpNetworkInLZInteractionChain(UUID networkId, UUID workspaceId) {
-        var lzId = UUID.randomUUID().toString();
-        var resources = new ArrayList<ApiAzureLandingZoneDeployedResource>();
-        resources.add(
-                new ApiAzureLandingZoneDeployedResource()
-                        .resourceName(STUB_SUBNET)
-                        .resourceParentId(networkId.toString()));
+  private void setUpNetworkInLZInteractionChain(UUID networkId, UUID workspaceId) {
+    var lzId = UUID.randomUUID().toString();
+    var resources = new ArrayList<ApiAzureLandingZoneDeployedResource>();
+    resources.add(
+        new ApiAzureLandingZoneDeployedResource()
+            .resourceName(STUB_SUBNET)
+            .resourceParentId(networkId.toString()));
 
-        when(resource.getNetworkId()).thenReturn(null);
-        when(landingZoneApiDispatch.getLandingZoneId(azureCloudContext)).thenReturn(lzId);
-        when(landingZoneApiDispatch.listSubnetsWithParentVNetByPurpose(
-                lzId, SubnetResourcePurpose.WORKSPACE_COMPUTE_SUBNET))
-                .thenReturn(resources);
-        when(networks.getById(networkId.toString())).thenReturn(armNetwork);
-    }
+    when(resource.getNetworkId()).thenReturn(null);
+    when(landingZoneApiDispatch.getLandingZoneId(azureCloudContext)).thenReturn(lzId);
+    when(landingZoneApiDispatch.listSubnetsWithParentVNetByPurpose(
+            lzId, SubnetResourcePurpose.WORKSPACE_COMPUTE_SUBNET))
+        .thenReturn(resources);
+    when(networks.getById(networkId.toString())).thenReturn(armNetwork);
+  }
 }
