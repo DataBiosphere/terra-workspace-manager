6c50e48b82a1abac63d1e15131319faeb6cddba2
zloery <6668057+zloery@users.noreply.github.com>
2022-09-20 16:35:24 -0400

    stop writing test user access tokens to logs

--- service/src/test/java/bio/terra/workspace/app/configuration/external/controller/ResourceApiControllerConnectedTest.java
+++ service/src/test/java/bio/terra/workspace/app/configuration/external/controller/ResourceApiControllerConnectedTest.java
@@ -33,11 +33,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@AutoConfigureMockMvc
 public class ResourceApiControllerConnectedTest extends BaseConnectedTest {
 
   @Autowired MockMvc mockMvc;
