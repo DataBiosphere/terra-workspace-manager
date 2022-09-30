d82cbbf966d031e206a2d4392378bddb892a9892
yuhuyoyo <yuhuyoyo@google.com>
2022-09-30 17:26:32 -0400

    also add notebook.instances.get

--- service/src/main/java/bio/terra/workspace/service/workspace/CloudSyncRoleMapping.java
+++ service/src/main/java/bio/terra/workspace/service/workspace/CloudSyncRoleMapping.java
@@ -76,6 +76,7 @@ public class CloudSyncRoleMapping {
               "iam.serviceAccounts.list",
               "lifesciences.operations.cancel",
               "lifesciences.workflows.run",
+              "notebooks.instances.get",
               "notebooks.operations.cancel",
               "notebooks.operations.delete",
               "notebooks.operations.get",
