2b0dc1ff37fdf82c2c2e264a9b859edd172f1340
yuhuyoyo <yuhuyoyo@google.com>
2022-10-21 14:24:07 -0400

    create role if none exist

--- service/src/main/java/bio/terra/workspace/service/admin/flights/cloudcontexts/gcp/GcpIamCustomRolePatchStep.java
+++ service/src/main/java/bio/terra/workspace/service/admin/flights/cloudcontexts/gcp/GcpIamCustomRolePatchStep.java
@@ -11,12 +11,16 @@ import bio.terra.stairway.exception.RetryException;
 import bio.terra.workspace.service.resource.controlled.cloud.gcp.CustomGcpIamRole;
 import bio.terra.workspace.service.resource.controlled.cloud.gcp.CustomGcpIamRoleMapping;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.iam.v1.model.CreateRoleRequest;
 import com.google.api.services.iam.v1.model.Role;
 import java.io.IOException;
 import java.util.HashSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Step to update IAM custom project and resource roles in GCP project. */
 public class GcpIamCustomRolePatchStep implements Step {
+  private final Logger logger = LoggerFactory.getLogger(GcpIamCustomRolePatchStep.class);
 
   private final IamCow iamCow;
   private final String projectId;
@@ -34,7 +38,11 @@ public class GcpIamCustomRolePatchStep implements Step {
   public StepResult doStep(FlightContext flightContext)
       throws InterruptedException, RetryException {
     for (CustomGcpIamRole customProjectRole : customGcpIamRoles) {
-      updateCustomRole(customProjectRole, projectId);
+      if (getCustomRole(customProjectRole, projectId) == null) {
+        createCustomRole(customProjectRole, projectId);
+      } else {
+        updateCustomRole(customProjectRole, projectId);
+      }
     }
     return StepResult.getStepResultSuccess();
   }
@@ -45,19 +53,60 @@ public class GcpIamCustomRolePatchStep implements Step {
    */
   private void updateCustomRole(CustomGcpIamRole customRole, String projectId)
       throws RetryException {
+    try {
+      Role role = new Role().setIncludedPermissions(customRole.getIncludedPermissions());
+      iamCow
+          .projects()
+          .roles()
+          .patch(customRole.getFullyQualifiedRoleName(projectId), role)
+          .execute();
+      logger.debug(
+          "Updated role {} with permissions {} in project {}",
+          customRole.getRoleName(),
+          customRole.getIncludedPermissions(),
+          projectId);
+    } catch (IOException e) {
+      handleIOException(e, customRole.getFullyQualifiedRoleName(projectId));
+    }
+  }
+
+  private Role getCustomRole(CustomGcpIamRole customRole, String projectId) {
     String fullyQualifiedRoleName = customRole.getFullyQualifiedRoleName(projectId);
     try {
-      Role role = iamCow.projects().roles().get(fullyQualifiedRoleName).execute();
-      role.setIncludedPermissions(customRole.getIncludedPermissions());
-      iamCow.projects().roles().patch(fullyQualifiedRoleName, role).execute();
+      return iamCow.projects().roles().get(fullyQualifiedRoleName).execute();
     } catch (IOException e) {
-      handleIOException(e);
+      handleIOException(e, customRole.getFullyQualifiedRoleName(projectId));
+      return null;
+    }
+  }
+
+  /**
+   * Utility for creating custom roles in GCP from WSM's CustomGcpIamRole objects. These roles will
+   * be defined at the project level in the specified by projectId.
+   */
+  private void createCustomRole(CustomGcpIamRole customRole, String projectId)
+      throws RetryException {
+    try {
+      Role gcpRole =
+          new Role()
+              .setIncludedPermissions(customRole.getIncludedPermissions())
+              .setTitle(customRole.getRoleName());
+      CreateRoleRequest request =
+          new CreateRoleRequest().setRole(gcpRole).setRoleId(customRole.getRoleName());
+      iamCow.projects().roles().create("projects/" + projectId, request).execute();
+      logger.debug(
+          "Created role {} with permissions {} in project {}",
+          customRole.getRoleName(),
+          customRole.getIncludedPermissions(),
+          projectId);
+    } catch (IOException e) {
+      // Retry on IO exceptions thrown by CRL.
+      handleIOException(e, customRole.getFullyQualifiedRoleName(projectId));
     }
   }
 
   @Override
   public StepResult undoStep(FlightContext flightContext) throws InterruptedException {
-
     for (CustomGcpIamRole customGcpIamRole : customGcpIamRoles) {
       resetCustomRolesToPreviousSetOfPermissions(flightContext, customGcpIamRole);
     }
@@ -78,8 +127,13 @@ public class GcpIamCustomRolePatchStep implements Step {
                 originalRole.getName(),
                 new Role().setIncludedPermissions(originalRole.getIncludedPermissions()))
             .execute();
+        logger.debug(
+            "Updated role {} with permissions {} in project {}",
+            originalRole.getName(),
+            originalRole.getIncludedPermissions(),
+            projectId);
       } catch (IOException e) {
-        handleIOException(e);
+        handleIOException(e, customGcpIamRoles.getFullyQualifiedRoleName(projectId));
       }
     } else {
       try {
@@ -88,16 +142,22 @@ public class GcpIamCustomRolePatchStep implements Step {
             .roles()
             .delete(customGcpIamRoles.getFullyQualifiedRoleName(projectId))
             .execute();
+        logger.debug(
+            "Deleted role {} with permissions {} in project {}",
+            customGcpIamRoles.getRoleName(),
+            customGcpIamRoles.getIncludedPermissions(),
+            projectId);
       } catch (IOException e) {
-        handleIOException(e);
+        handleIOException(e, customGcpIamRoles.getFullyQualifiedRoleName(projectId));
       }
     }
   }
 
-  private void handleIOException(IOException e) {
+  private void handleIOException(IOException e, String customRole) {
     if (e instanceof GoogleJsonResponseException googleEx) {
       // If receives client error, do not retry
       if (googleEx.getStatusCode() >= 400 && googleEx.getStatusCode() < 500) {
+        logger.error("calling GCP iam/roles api receives error for custom role {}", customRole, e);
         return;
       }
     }
