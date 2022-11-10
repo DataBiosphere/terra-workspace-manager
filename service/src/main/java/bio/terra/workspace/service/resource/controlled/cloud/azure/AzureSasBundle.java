ad930ab1e5560097b4d0c52ba5a242c908c511c1
Drew Herbst <aherbst@broadinstitute.org>
2022-11-10 10:17:22 -0500

    Formatting

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/azure/AzureSasBundle.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/azure/AzureSasBundle.java
@@ -1,4 +1,3 @@
 package bio.terra.workspace.service.resource.controlled.cloud.azure;
 
-public record AzureSasBundle(String sasToken, String sasUrl) {
-}
+public record AzureSasBundle(String sasToken, String sasUrl) {}
