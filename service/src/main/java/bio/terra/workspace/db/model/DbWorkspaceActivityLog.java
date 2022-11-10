50b865460d1394cee0f78680aebe5cb4893f8648
melissachang <10929390+melissachang@users.noreply.github.com>
2022-11-10 14:39:49 -0800

    Revert "PF-2176: Log workspace update policies endpoint (#892)" (#898)
    This reverts commit ecf09ed2672014ebae948b15f28d8f185fda8a46.

--- service/src/main/java/bio/terra/workspace/db/model/DbWorkspaceActivityLog.java
+++ service/src/main/java/bio/terra/workspace/db/model/DbWorkspaceActivityLog.java
@@ -1,12 +1,53 @@
 package bio.terra.workspace.db.model;
 
+import bio.terra.workspace.service.workspace.exceptions.MissingRequiredFieldsException;
 import bio.terra.workspace.service.workspace.model.OperationType;
+import java.util.Optional;
+import java.util.function.Supplier;
 
-public record DbWorkspaceActivityLog(
-    String actorEmail, String actorSubjectId, OperationType operationType) {
+public class DbWorkspaceActivityLog {
+
+  private OperationType operationType;
+
+  private String actorEmail;
+
+  private String actorSubjectId;
+
+  private static final Supplier<RuntimeException> MISSING_REQUIRED_FIELD =
+      () -> new MissingRequiredFieldsException("Missing required field");
+
+  private DbWorkspaceActivityLog operationType(OperationType operationType) {
+    this.operationType = operationType;
+    return this;
+  }
+
+  private DbWorkspaceActivityLog actorEmail(String actorEmail) {
+    this.actorEmail = actorEmail;
+    return this;
+  }
+
+  private DbWorkspaceActivityLog actorSubjectId(String actorSubjectId) {
+    this.actorSubjectId = actorSubjectId;
+    return this;
+  }
+
+  public OperationType getOperationType() {
+    return Optional.ofNullable(operationType).orElseThrow(MISSING_REQUIRED_FIELD);
+  }
+
+  public String getActorEmail() {
+    return Optional.ofNullable(actorEmail).orElseThrow(MISSING_REQUIRED_FIELD);
+  }
+
+  public String getActorSubjectId() {
+    return Optional.ofNullable(actorSubjectId).orElseThrow(MISSING_REQUIRED_FIELD);
+  }
 
   public static DbWorkspaceActivityLog getDbWorkspaceActivityLog(
-      OperationType operationType, String actorEmail, String actorSubjectId) {
-    return new DbWorkspaceActivityLog(actorEmail, actorSubjectId, operationType);
+      OperationType operationType, String userEmail, String subjectId) {
+    return new DbWorkspaceActivityLog()
+        .operationType(operationType)
+        .actorEmail(userEmail)
+        .actorSubjectId(subjectId);
   }
 }
