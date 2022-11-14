a5cad583fbcedd6fad02c1137587dce5ac694172
Drew Herbst <aherbst@broadinstitute.org>
2022-11-14 08:45:02 -0500

    Remove unused variable

--- service/src/main/java/bio/terra/workspace/app/controller/ControlledAzureResourceApiController.java
+++ service/src/main/java/bio/terra/workspace/app/controller/ControlledAzureResourceApiController.java
@@ -39,7 +39,6 @@ import bio.terra.workspace.generated.model.ApiJobControl;
 import bio.terra.workspace.generated.model.ApiJobReport;
 import bio.terra.workspace.service.iam.AuthenticatedUserRequest;
 import bio.terra.workspace.service.iam.AuthenticatedUserRequestFactory;
-import bio.terra.workspace.service.iam.SamRethrow;
 import bio.terra.workspace.service.iam.SamService;
 import bio.terra.workspace.service.iam.model.SamConstants.SamControlledResourceActions;
 import bio.terra.workspace.service.job.JobService;
@@ -253,9 +252,6 @@ public class ControlledAzureResourceApiController extends ControlledResourceCont
                 storageContainerResource.getStorageAccountId(),
                 SamControlledResourceActions.READ_ACTION)
             .castByEnum(WsmResourceType.CONTROLLED_AZURE_STORAGE_ACCOUNT);
-    final String userEmail =
-        SamRethrow.onInterrupted(
-            () -> getSamService().getUserEmailFromSam(userRequest), "getUserEmailFromSam");
 
     OffsetDateTime startTime =
         OffsetDateTime.now().minusMinutes(azureConfiguration.getSasTokenStartTimeMinutesOffset());
