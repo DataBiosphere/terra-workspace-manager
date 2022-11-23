77f559eecc78ecb14bcb158e19c9b448c8f6919b
Sergiy Getlin <sergiygetlin@microsoft.com>
2022-11-23 11:20:31 -0800

    Randomize storage account name to avoid conflicts

--- service/src/test/java/bio/terra/workspace/common/utils/TestUtils.java
+++ service/src/test/java/bio/terra/workspace/common/utils/TestUtils.java
@@ -1,5 +1,6 @@
 package bio.terra.workspace.common.utils;
 
+import java.util.Locale;
 import java.util.UUID;
 
 public class TestUtils {
@@ -9,5 +10,23 @@ public class TestUtils {
     // in bucket name.
     String randomString = prefix + UUID.randomUUID();
     return randomString.replaceAll("[-_]", "");
+  }
+
+  /**
+   * Generates random string based on UUID value. Hyphens are removed from the result string.
+   * Maximum length of result string is 32: UUID length of 36 minus 4 hyphens.
+   *
+   * @param length maximum length of the output string. Length will be cut off down to its max value
+   *     if exceeds max value of 32.
+   * @return string value containing lowercase characters and digits
+   */
+  public static String getRandomString(int length) {
+    // removes hyphen since it's forbidden to use in Azure storage account
+    int maxLength = 32;
+    return UUID.randomUUID()
+        .toString()
+        .toLowerCase(Locale.ROOT)
+        .replace("-", "")
+        .substring(0, length <= maxLength ? length - 1 : maxLength - 1);
   }
 }
