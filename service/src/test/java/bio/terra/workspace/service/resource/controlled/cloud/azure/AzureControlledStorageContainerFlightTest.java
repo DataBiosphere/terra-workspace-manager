9f37191fa0116334415ecfba960201a6036d9870
zloery <6668057+zloery@users.noreply.github.com>
2022-11-29 17:59:23 -0500

    combine tests for shared LZ

--- service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/AzureControlledStorageContainerFlightTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/AzureControlledStorageContainerFlightTest.java
@@ -71,8 +71,16 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
     sharedWorkspace = azureTestUtils.createWorkspace(workspaceService);
     workspaceUuid = sharedWorkspace.getWorkspaceId();
     azureUtils.createCloudContext(workspaceUuid, userAccessUtils.defaultUserAuthRequest());
+  }
+
+  @AfterAll
+  public void cleanup() {
+    workspaceService.deleteWorkspace(sharedWorkspace, userAccessUtils.defaultUserAuthRequest());
+  }
+
+  private void setupLandingZone() {
     // create quasi landing zone with a single resource - shared storage account
-    storageAccountName = "lzsharedstorageaccount";
+    storageAccountName = String.format("lzsharedstacc%s", TestUtils.getRandomString(6));
     UUID landingZoneId = UUID.fromString(landingZoneTestUtils.getDefaultLandingZoneId());
     testLandingZoneManager =
         new TestLandingZoneManager(
@@ -85,11 +93,10 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
 
     testLandingZoneManager.createLandingZoneWithSharedStorageAccount(
         landingZoneId, workspaceUuid, storageAccountName, "eastus");
+
   }
 
-  @AfterAll
-  public void cleanup() {
-    workspaceService.deleteWorkspace(sharedWorkspace, userAccessUtils.defaultUserAuthRequest());
+  private void cleanupLandingZone() {
     testLandingZoneManager.deleteLandingZoneWithSharedStorageAccount(
         UUID.fromString(landingZoneTestUtils.getDefaultLandingZoneId()),
         azureTestUtils.getAzureCloudContext().getAzureResourceGroupId(),
@@ -98,6 +105,7 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
 
   @Test
   public void createAndDeleteAzureStorageResource() throws InterruptedException {
+    setupLandingZone();
     AuthenticatedUserRequest userRequest = userAccessUtils.defaultUserAuthRequest();
 
     final ApiAzureStorageCreationParameters accountCreationParameters =
@@ -139,6 +147,19 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
         containerResource,
         WsmResourceType.CONTROLLED_AZURE_STORAGE_CONTAINER);
 
+    // create SAS token for the storage container above validate
+    OffsetDateTime startTime = OffsetDateTime.now();
+    OffsetDateTime expiryTime = startTime.plusMinutes(15L);
+    var azureSasBundle =
+        azureStorageAccessService.createAzureStorageContainerSasToken(
+            workspaceUuid,
+            containerResourceId,
+            userRequest,
+            new SasTokenOptions(null, startTime, expiryTime, null, null));
+    assertNotNull(azureSasBundle);
+    assertNotNull(azureSasBundle.sasToken());
+    assertNotNull(azureSasBundle.sasUrl());
+
     // clean up resources - delete storage container resource
     azureUtils.submitControlledResourceDeletionFlight(
         workspaceUuid,
@@ -157,27 +178,17 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
         accountResource.getStorageAccountName(),
         azureTestUtils.getStorageManager().storageAccounts()::getByResourceGroup);
 
-    // Verify containers have been deleted (Can't do this in submitControlledResourceDeletionFlight
-    // because the get function takes a different number of arguments. Also no need to sleep another
-    // 5 seconds.)
-    verifyStorageAccountContainerIsDeleted(accountResource, containerName);
-  }
-
-  @Test
-  public void createAndDeleteAzureStorageContainerBasedOnLandingZoneSharedStorageAccount()
-      throws InterruptedException {
-    String storageAccountName = String.format("lzsharedstacc%s", TestUtils.getRandomString(6));
-    AuthenticatedUserRequest userRequest = userAccessUtils.defaultUserAuthRequest();
+    // Next, use the shared storage account already in the landing zone.
 
     // Submit a storage container creation flight and then verify the resource exists in the
     // workspace.
-    final UUID containerResourceId = UUID.randomUUID();
+    final UUID sharedContainerResourceId = UUID.randomUUID();
     final String storageContainerName = ControlledResourceFixtures.uniqueBucketName();
-    ControlledAzureStorageContainerResource containerResource =
+    ControlledAzureStorageContainerResource sharedContainerResource =
         ControlledResourceFixtures.getAzureStorageContainer(
             workspaceUuid,
             null,
-            containerResourceId,
+            sharedContainerResourceId,
             storageContainerName,
             getAzureName("rc"),
             getAzureName("rc-desc"));
@@ -185,19 +196,38 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
     azureUtils.createResource(
         workspaceUuid,
         userRequest,
-        containerResource,
+        sharedContainerResource,
         WsmResourceType.CONTROLLED_AZURE_STORAGE_CONTAINER);
 
     TimeUnit.MINUTES.sleep(1);
+
+    // create SAS token for the storage container above validate
+    OffsetDateTime sharedStartTime = OffsetDateTime.now();
+    OffsetDateTime sharedExpiryTime = startTime.plusMinutes(15L);
+    var sharedAzureSasBundle =
+        azureStorageAccessService.createAzureStorageContainerSasToken(
+            workspaceUuid,
+            sharedContainerResourceId,
+            userRequest,
+            new SasTokenOptions(null, sharedStartTime, sharedExpiryTime, null, null));
+    assertNotNull(sharedAzureSasBundle);
+    assertNotNull(sharedAzureSasBundle.sasToken());
+    assertNotNull(sharedAzureSasBundle.sasUrl());
 
     // clean up resources - delete storage container resource
     azureUtils.submitControlledResourceDeletionFlight(
         workspaceUuid,
         userRequest,
-        containerResource,
+        sharedContainerResource,
         azureTestUtils.getAzureCloudContext().getAzureResourceGroupId(),
-        containerResource.getStorageContainerName(),
+        sharedContainerResource.getStorageContainerName(),
         null); // Don't sleep/verify deletion yet.
+
+    // Verify containers have been deleted (Can't do this in submitControlledResourceDeletionFlight
+    // because the get function takes a different number of arguments. Also no need to sleep another
+    // 5 seconds.)
+    verifyStorageAccountContainerIsDeleted(accountResource, containerName);
+    cleanupLandingZone();
   }
 
   @Test
@@ -241,33 +271,19 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
   public void
       createAzureStorageContainerFlightFailedBecauseLandingZoneDoesntHaveSharedStorageAccount()
           throws InterruptedException {
-    // Unlike other tests, this one uses a different landing zone and so cannot share a cloud
-    // context
-    Workspace workspace = azureTestUtils.createWorkspace(workspaceService);
-    UUID workspaceId = workspace.getWorkspaceId();
     AuthenticatedUserRequest userRequest = userAccessUtils.defaultUserAuthRequest();
-    azureUtils.createCloudContext(workspaceId, userRequest);
 
     // create quasi landing zone without resources
-    UUID alternateLandingZoneId = UUID.randomUUID();
+    UUID alternateLandingZoneId = UUID.fromString(landingZoneTestUtils.getDefaultLandingZoneId());
 
-    TestLandingZoneManager testLandingZoneManager =
-        new TestLandingZoneManager(
-            azureCloudContextService,
-            landingZoneDao,
-            workspaceDao,
-            crlService,
-            azureConfig,
-            workspaceId);
-
-    testLandingZoneManager.createLandingZoneWithoutResources(alternateLandingZoneId, workspaceId);
+    testLandingZoneManager.createLandingZoneWithoutResources(alternateLandingZoneId, workspaceUuid);
 
     // Submit a storage container creation flight which should error out
     final UUID containerResourceId = UUID.randomUUID();
     final String storageContainerName = ControlledResourceFixtures.uniqueBucketName();
     ControlledAzureStorageContainerResource containerResource =
         ControlledResourceFixtures.getAzureStorageContainer(
-            workspaceId,
+            workspaceUuid,
             null,
             containerResourceId,
             storageContainerName,
@@ -279,7 +295,7 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
             jobService.getStairway(),
             CreateControlledResourceFlight.class,
             azureTestUtils.createControlledResourceInputParameters(
-                workspaceId, userRequest, containerResource, null),
+                workspaceUuid, userRequest, containerResource, null),
             STAIRWAY_FLIGHT_TIMEOUT,
             null);
 
@@ -289,126 +305,6 @@ public class AzureControlledStorageContainerFlightTest extends BaseAzureConnecte
 
     // clean up resources - delete alternate lz database record only
     testLandingZoneManager.deleteLandingZoneWithoutResources(alternateLandingZoneId);
-  }
-
-  @Test
-  public void generateAzureStorageContainerSasToken() throws InterruptedException {
-    AuthenticatedUserRequest userRequest = userAccessUtils.defaultUserAuthRequest();
-
-    final ApiAzureStorageCreationParameters accountCreationParameters =
-        ControlledResourceFixtures.getAzureStorageCreationParameters();
-
-    final UUID accountResourceId = UUID.randomUUID();
-    ControlledAzureStorageResource accountResource =
-        ControlledResourceFixtures.getAzureStorage(
-            workspaceUuid,
-            accountResourceId,
-            accountCreationParameters.getStorageAccountName(),
-            accountCreationParameters.getRegion(),
-            getAzureName("rs"),
-            getAzureName("rs-desc"));
-
-    // Submit a storage account creation flight and then verify the resource exists in the
-    // workspace.
-    azureUtils.createResource(
-        workspaceUuid,
-        userRequest,
-        accountResource,
-        WsmResourceType.CONTROLLED_AZURE_STORAGE_ACCOUNT);
-
-    // Submit a storage container creation flight and then verify the resource exists in the
-    // workspace.
-    final UUID containerResourceId = UUID.randomUUID();
-    final String containerName = ControlledResourceFixtures.uniqueBucketName();
-    ControlledAzureStorageContainerResource containerResource =
-        ControlledResourceFixtures.getAzureStorageContainer(
-            workspaceUuid,
-            accountResourceId,
-            containerResourceId,
-            containerName,
-            getAzureName("rc"),
-            getAzureName("rc-desc"));
-
-    azureUtils.createResource(
-        workspaceUuid,
-        userRequest,
-        containerResource,
-        WsmResourceType.CONTROLLED_AZURE_STORAGE_CONTAINER);
-
-    TimeUnit.SECONDS.sleep(15);
-
-    // create SAS token for the storage container above validate
-    OffsetDateTime startTime = OffsetDateTime.now();
-    OffsetDateTime expiryTime = startTime.plusMinutes(15L);
-    var azureSasBundle =
-        azureStorageAccessService.createAzureStorageContainerSasToken(
-            workspaceUuid,
-            containerResourceId,
-            userRequest,
-            new SasTokenOptions(null, startTime, expiryTime, null, null));
-    assertNotNull(azureSasBundle);
-    assertNotNull(azureSasBundle.sasToken());
-    assertNotNull(azureSasBundle.sasUrl());
-
-    // clean up resources - delete storage container resource
-    azureUtils.submitControlledResourceDeletionFlight(
-        workspaceUuid,
-        userRequest,
-        containerResource,
-        azureTestUtils.getAzureCloudContext().getAzureResourceGroupId(),
-        containerResource.getStorageContainerName(),
-        null); // Don't sleep/verify deletion yet.
-
-    // clean up resources - delete storage account resource
-    azureUtils.submitControlledResourceDeletionFlight(
-        workspaceUuid,
-        userRequest,
-        accountResource,
-        azureTestUtils.getAzureCloudContext().getAzureResourceGroupId(),
-        accountResource.getStorageAccountName(),
-        azureTestUtils.getStorageManager().storageAccounts()::getByResourceGroup);
-
-    verifyStorageAccountContainerIsDeleted(accountResource, containerName);
-  }
-
-  @Test
-  public void generateAzureStorageContainerSasTokenBasedOnLandingZoneSharedStorageAccount()
-      throws InterruptedException {
-    AuthenticatedUserRequest userRequest = userAccessUtils.defaultUserAuthRequest();
-    // Submit a storage container creation flight and then verify the resource exists in the
-    // workspace.
-    final UUID containerResourceId = UUID.randomUUID();
-    final String storageContainerName = ControlledResourceFixtures.uniqueBucketName();
-    ControlledAzureStorageContainerResource containerResource =
-        ControlledResourceFixtures.getAzureStorageContainer(
-            workspaceUuid,
-            null,
-            containerResourceId,
-            storageContainerName,
-            getAzureName("rc"),
-            getAzureName("rc-desc"));
-
-    // create azure storage container
-    azureUtils.createResource(
-        workspaceUuid,
-        userRequest,
-        containerResource,
-        WsmResourceType.CONTROLLED_AZURE_STORAGE_CONTAINER);
-
-    TimeUnit.MINUTES.sleep(1);
-
-    // create SAS token for the storage container above validate
-    OffsetDateTime startTime = OffsetDateTime.now();
-    OffsetDateTime expiryTime = startTime.plusMinutes(15L);
-    var azureSasBundle =
-        azureStorageAccessService.createAzureStorageContainerSasToken(
-            workspaceUuid,
-            containerResourceId,
-            userRequest,
-            new SasTokenOptions(null, startTime, expiryTime, null, null));
-    assertNotNull(azureSasBundle);
-    assertNotNull(azureSasBundle.sasToken());
-    assertNotNull(azureSasBundle.sasUrl());
   }
 
   private void verifyStorageAccountContainerIsDeleted(
