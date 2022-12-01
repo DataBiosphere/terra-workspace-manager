2265b898bccf72472fbc4c5b5695e83b4e98d8fa
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2022-12-01 17:45:14 -0500

    clean up folder and activity log table too

--- scripts/clean_wsm.sql
+++ scripts/clean_wsm.sql
@@ -1,3 +1,3 @@
 -- Remove all of the data; leave the schema intact.
-TRUNCATE workspace, cloud_context, application, enabled_application, resource CASCADE;
+TRUNCATE workspace, cloud_context, application, enabled_application, resource, folder, workspace_activity_log CASCADE;
 
