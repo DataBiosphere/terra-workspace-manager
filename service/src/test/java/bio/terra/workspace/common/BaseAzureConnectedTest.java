332579176ea9bacd070043a889e19523cb787a9d
Marcus Talbott <mtalbott@broadinstitute.org>
2022-12-06 16:25:54 -0500

    spotless again

--- service/src/test/java/bio/terra/workspace/common/BaseAzureConnectedTest.java
+++ service/src/test/java/bio/terra/workspace/common/BaseAzureConnectedTest.java
@@ -1,21 +1,20 @@
 package bio.terra.workspace.common;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.when;
+
 import bio.terra.workspace.app.configuration.external.AzureTestConfiguration;
 import bio.terra.workspace.service.spendprofile.SpendProfile;
 import bio.terra.workspace.service.spendprofile.SpendProfileId;
 import bio.terra.workspace.service.spendprofile.SpendProfileService;
 import bio.terra.workspace.service.workspace.model.CloudPlatform;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.Mockito.when;
 
 /** Base class for azure tests. Treat these as connected tests: connected to Azure */
 @Tag("azure")
