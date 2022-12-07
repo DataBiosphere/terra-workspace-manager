819c79d28c2cc7713046b4d4aa787ebf7fa648ee
rogerwangcs <rogerwangcs@gmail.com>
2022-12-07 15:23:41 -0500

    restart jupyter lab service in post startup script to load updated terra environmental variables in notebooks

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/gcp/ainotebook/post-startup.sh
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/gcp/ainotebook/post-startup.sh
@@ -195,6 +195,9 @@ fi
 # Generate cromwell.conf
 sudo -u "${JUPYTER_USER}" sh -c "terra cromwell generate-config"
 
+# restart jupyterlab service to load environment variables set in post startup script
+sudo systemctl restart jupyter.service
+
 # Attempt to clone all the git repo references in the workspace. If the user's ssh key does not exist or doesn't have access
 # to the git references, the corresponding git repo cloning will be skipped.
 # Keep this as last thing in script. There will be integration test for git cloning (PF-1660). If this is last thing, then
