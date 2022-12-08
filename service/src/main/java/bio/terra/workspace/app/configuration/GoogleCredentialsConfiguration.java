4c645605c8d48110170f6a3cc30dff3859fc6374
melissachang <10929390+melissachang@users.noreply.github.com>
2022-12-08 13:36:10 -0800

    revert debugging

--- service/src/main/java/bio/terra/workspace/app/configuration/GoogleCredentialsConfiguration.java
+++ service/src/main/java/bio/terra/workspace/app/configuration/GoogleCredentialsConfiguration.java
@@ -3,8 +3,6 @@ package bio.terra.workspace.app.configuration;
 import com.google.api.services.storagetransfer.v1.StoragetransferScopes;
 import com.google.auth.oauth2.GoogleCredentials;
 import java.io.IOException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,15 +10,11 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GoogleCredentialsConfiguration {
-  private static final Logger logger =
-      LoggerFactory.getLogger(GoogleCredentialsConfiguration.class);
-
   @Bean
   @Profile("!unit-test")
   public GoogleCredentials getGoogleCredentials() {
     try {
       GoogleCredentials googleCredentials = GoogleCredentials.getApplicationDefault();
-      logger.info("###### googleCredentials {}", googleCredentials);
       if (googleCredentials.createScopedRequired()) {
         googleCredentials = googleCredentials.createScoped(StoragetransferScopes.all());
       }
