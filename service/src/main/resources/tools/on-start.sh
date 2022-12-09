1ad5b96a6b4d85685914cb675c4a5c2f7d657ef5
John Czerkowicz <jczerk@verily.com>
2022-12-09 16:42:18 -0500

    More script improvements.

--- service/src/main/resources/tools/on-start.sh
+++ service/src/main/resources/tools/on-start.sh
@@ -20,7 +20,7 @@ nohup yum install -y google-cloud-cli &
 cat << EOM | sed -i '/^# User specific aliases and functions$/ r /dev/stdin' /home/ec2-user/.bashrc
 if [ ! -f '/home/ec2-user/SageMaker/.terra/notebook_metadata.json' ]; then
     # The user has not configured their environment to access Terra yet, call now.
-    /home/ec2-user/terra/terra-auth.py --configure
+    /usr/local/bin/terra-auth --configure
     retVal=\$?
 
     # If this got status 1, that means user was not authenticated.  Prompt them to authenticate
@@ -28,7 +28,7 @@ if [ ! -f '/home/ec2-user/SageMaker/.terra/notebook_metadata.json' ]; then
     if [ \${retVal} -eq 1 ]; then
       echo "Please log in with Google Application Default Credentials in order to authenticate with Terra.\n"
       /usr/bin/gcloud auth application-default login
-      /home/ec2-user/terra/terra-auth.py --configure
+      /usr/local/bin/terra-auth --configure
     fi
 fi
 EOM
@@ -36,11 +36,11 @@ EOM
 # Change the terminal shell to bash
 echo "exec -l bash" > /home/ec2-user/.profile
 
-sudo -u ec2-user -i <<'EOF'
-
 # Copy in the Terra auth helper CLI (eventually this will install Terra CLI instead)
 wget https://raw.githubusercontent.com/DataBiosphere/terra-workspace-manager/jczerk/aws_wlz_interface/service/src/main/resources/tools/terra-auth.py -O "/usr/local/bin/terra-auth"
 chmod +x "/usr/local/bin/terra-auth"
+
+sudo -u ec2-user -i <<'EOF'
 
 # Lazy create terra persistence directory and symlink
 mkdir -p /home/ec2-user/SageMaker/.terra
@@ -60,16 +60,18 @@ for env in /home/ec2-user/anaconda3/envs/*; do
     fi
 
     # We don't need to wait for these and delay startup, so nohup them in background
-    nohup -c 'source /home/ec2-user/anaconda3/bin/activate $(basename "$env") && \
-    pip install google-auth && \
-    source /home/ec2-user/anaconda3/bin/deactivate' &
+    nohup sh -c \
+        'source /home/ec2-user/anaconda3/bin/activate $(basename "$env") && \
+         pip install google-auth && \
+         source /home/ec2-user/anaconda3/bin/deactivate'\
+    &> "nohup.out.pip.$(basename $env)" &
 done
 
 # If we have ADC, attempt to re-configure at startup.
 if [ -f /home/ec2-user/.config/gcloud/application_default_credentials.json ]; then
   source /home/ec2-user/anaconda3/bin/activate base
   pip install google-auth
-  /home/ec2-user/terra/terra-auth.py --configure
+  /usr/local/bin/terra-auth --configure
   source /home/ec2-user/anaconda3/bin/deactivate
 fi
 
