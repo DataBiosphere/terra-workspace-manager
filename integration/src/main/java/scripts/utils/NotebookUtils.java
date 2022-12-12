c5dc7f9bb3d7a012be622c91bdc8d11c13f28993
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2022-12-12 10:30:33 -0500

    PF-2326: add better logging when notebookUtils fails to get proxy url and retry (#956)
    * add logging in assertInstanceHasProxyUrl
    
    * address comment

--- integration/src/main/java/scripts/utils/NotebookUtils.java
+++ integration/src/main/java/scripts/utils/NotebookUtils.java
@@ -33,8 +33,11 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NotebookUtils {
+  private static final Logger logger = LoggerFactory.getLogger(NotebookUtils.class);
 
   /**
    * Create and return a private AI Platform Notebook controlled resource with constant values. This
@@ -196,6 +199,7 @@ public class NotebookUtils {
                         .execute()
                         .getProxyUri();
                 if (p == null) {
+                  logger.info("Notebook proxy url is null, retry");
                   throw new NullPointerException();
                 }
                 return p;
@@ -203,9 +207,12 @@ public class NotebookUtils {
                 // any other IO exception.
               } catch (GoogleJsonResponseException e) {
                 if (e.getStatusCode() == HttpStatus.SC_FORBIDDEN) {
+                  logger.info("Fails to fetch notebook proxy url due to 403, retry", e);
                   throw e;
                 }
-              } catch (IOException ignored) {
+                logger.info("Fails to fetch notebook proxy url, do not retry", e);
+              } catch (Exception ignored) {
+                logger.info("Fails to fetch notebook proxy url, do not retry", ignored);
               }
               return null;
             });
