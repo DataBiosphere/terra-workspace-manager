ce52be009e79ac5402b8b11b27a9e2a4b2b5cfb7
cbookg <cbook@verily.com>
2022-12-13 13:14:06 -0500

    add comments

--- service/src/main/java/bio/terra/workspace/common/utils/RetryRules.java
+++ service/src/main/java/bio/terra/workspace/common/utils/RetryRules.java
@@ -31,6 +31,9 @@ public class RetryRules {
    * few seconds.
    */
   public static RetryRule shortExponential() {
+    // maxOperationTimeSeconds must be larger than socket timeout (20s), otherwise a socket timeout
+    // won't be retried.
+    return new RetryRuleExponentialBackoff(1, 8, /* maxOperationTimeSeconds */ 30);
     return new RetryRuleExponentialBackoff(1, 8, 30);
   }
 
