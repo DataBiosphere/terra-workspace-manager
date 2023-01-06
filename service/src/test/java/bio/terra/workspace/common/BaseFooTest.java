5839048ec055705446bd264ab111b0823fc454b4
aherbst-broad <aherbst@broadinstitute.org>
2023-01-06 16:39:46 -0500

    Fix

--- service/src/test/java/bio/terra/workspace/common/BaseFooTest.java
+++ service/src/test/java/bio/terra/workspace/common/BaseFooTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.springframework.test.context.ActiveProfiles;
 
 @Tag("foo")
-@ActiveProfiles("foo-test")
+@ActiveProfiles("connected-test")
 public class BaseFooTest extends BaseTest {
     public static final String BUFFER_SERVICE_DISABLED_ENVS_REG_EX = "dev|alpha|staging|prod";
 
