45fd1411a1e8dcf8d137dfc2b4ff1337fe1393be
aherbst-broad <aherbst@broadinstitute.org>
2023-01-06 16:34:37 -0500

    Mucking w/test configuraiton

---
+++ service/src/test/java/bio/terra/workspace/common/BaseFooTest.java
@@ -0,0 +1,11 @@
+package bio.terra.workspace.common;
+
+import org.junit.jupiter.api.Tag;
+import org.springframework.test.context.ActiveProfiles;
+
+@Tag("foo")
+@ActiveProfiles("foo-test")
+public class BaseFooTest extends BaseTest {
+    public static final String BUFFER_SERVICE_DISABLED_ENVS_REG_EX = "dev|alpha|staging|prod";
+
+}
