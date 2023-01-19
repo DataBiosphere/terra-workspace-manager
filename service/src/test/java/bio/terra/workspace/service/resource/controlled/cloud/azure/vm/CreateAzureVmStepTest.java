be9a4fbf7e9f8b28ae0d896a5e67d1f8d082ac80
Robert Title <rtitle@broadinstitute.org>
2023-01-19 10:24:12 -0500

    [TOAZ-294] Have WSM query LZ service as the WSM SA, not the calling user (#997)
    * update test to not use custom image
    
    * add vm user
    
    * spotless apply isn't working
    
    * use a specific version
    
    * disable this test for now
    
    * Lint
    
    * First cut: replace all interal calls to LZ with WSM token instead of user token
    
    * Fix tests, change some Azure connected tests to unit tests
    
    * Spotless
    
    * Fix unit tests
    
    * Compliation
    
    * Add Leo SA as well
    
    Co-authored-by: Qi Wang <qi@broadinstitute.org>
    Co-authored-by: aherbst-broad <aherbst@broadinstitute.org>
    Co-authored-by: aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>

--- service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/vm/CreateAzureVmStepTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/vm/CreateAzureVmStepTest.java
@@ -13,7 +13,7 @@ import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.StepResult;
 import bio.terra.workspace.app.configuration.external.AzureConfiguration;
-import bio.terra.workspace.common.BaseAzureConnectedTest;
+import bio.terra.workspace.common.BaseAzureUnitTest;
 import bio.terra.workspace.common.fixtures.ControlledResourceFixtures;
 import bio.terra.workspace.common.utils.AzureVmUtils;
 import bio.terra.workspace.db.ResourceDao;
@@ -60,7 +60,7 @@ import org.mockito.Mock;
 
 @Disabled(
     "Until we have a use case for using custom image and we have a custom image available for testing")
-public class CreateAzureVmStepTest extends BaseAzureConnectedTest {
+public class CreateAzureVmStepTest extends BaseAzureUnitTest {
 
   private static final String STUB_STRING_RETURN = "stubbed-return";
   private static final String STUB_DISK_NAME = "stub-disk-name";
