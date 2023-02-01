b5a2b1b83fdd90c097f0545f2b56d05533906410
Mike Neville <mikenev@verily.com>
2023-02-01 14:44:26 -0500

    Add integration test

--- integration/src/main/java/scripts/testscripts/ImportDataCollection.java
+++ integration/src/main/java/scripts/testscripts/ImportDataCollection.java
@@ -3,88 +3,216 @@ package scripts.testscripts;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static scripts.utils.BqDatasetUtils.assertDatasetsAreEqualIgnoringLastUpdatedDate;
 
 import bio.terra.testrunner.runner.config.TestUserSpecification;
 import bio.terra.workspace.api.ControlledGcpResourceApi;
 import bio.terra.workspace.api.ReferencedGcpResourceApi;
 import bio.terra.workspace.api.WorkspaceApi;
 import bio.terra.workspace.client.ApiException;
+import bio.terra.workspace.model.CloneReferencedGcpGcsBucketResourceResult;
+import bio.terra.workspace.model.CloneReferencedResourceRequestBody;
 import bio.terra.workspace.model.CloningInstructionsEnum;
 import bio.terra.workspace.model.CreateWorkspaceRequestBody;
-import bio.terra.workspace.model.CreatedAzureStorageContainerSasToken;
 import bio.terra.workspace.model.CreatedControlledGcpGcsBucket;
 import bio.terra.workspace.model.CreatedWorkspace;
-import bio.terra.workspace.model.GcpBigQueryDatasetResource;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 import bio.terra.workspace.model.GcpGcsBucketResource;
 import bio.terra.workspace.model.Properties;
 import bio.terra.workspace.model.Property;
+import bio.terra.workspace.model.WorkspaceDescription;
 import bio.terra.workspace.model.WsmPolicyInput;
 import bio.terra.workspace.model.WsmPolicyInputs;
 import bio.terra.workspace.model.WsmPolicyPair;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scripts.utils.BqDatasetUtils;
 import scripts.utils.ClientTestUtils;
 import scripts.utils.CloudContextMaker;
 import scripts.utils.GcsBucketUtils;
 import scripts.utils.WorkspaceAllocateTestScriptBase;
 
 public class ImportDataCollection extends WorkspaceAllocateTestScriptBase {
-  private static final Logger logger =
-    LoggerFactory.getLogger(ImportDataCollection.class);
+  private static final Logger logger = LoggerFactory.getLogger(ImportDataCollection.class);
 
   private static final String DATASET_RESOURCE_NAME = "wsmtest_dataset";
 
+  // TODO [PF-2407] Once we can patch workspace policies we won't have to create so many workspaces
   @Override
   protected void doUserJourney(TestUserSpecification testUser, WorkspaceApi workspaceApi)
-    throws Exception {
+      throws Exception {
     ControlledGcpResourceApi resourceApi =
-      ClientTestUtils.getControlledGcpResourceClient(testUser, server);
+        ClientTestUtils.getControlledGcpResourceClient(testUser, server);
+    ReferencedGcpResourceApi referencedGcpResourceApi =
+        ClientTestUtils.getReferencedGcpResourceClient(testUser, server);
+
+    String gcpCentralLocation = "gcp.us-central1";
+    String gcpEastLocation = "gcp.us-east1";
+    String usaLocation = "usa";
+
+    /**
+     * Create a workspace with us-central1 policy and a reference bucket. This will act as our data
+     * collection for most of the scenarios covered in this journey.
+     */
+    CreatedWorkspace centralDataCollection =
+        CreateWorkspaceWithRegionPolicy(workspaceApi, gcpCentralLocation);
 
     // Create a cloud context
-    String projectId = CloudContextMaker.createGcpCloudContext(getWorkspaceId(), workspaceApi);
+    String projectId =
+        CloudContextMaker.createGcpCloudContext(centralDataCollection.getId(), workspaceApi);
     logger.info("Created project {}", projectId);
 
     // Create a shared Bucket
     CreatedControlledGcpGcsBucket controlledBucket =
-      GcsBucketUtils.makeControlledGcsBucketUserShared(
-        resourceApi, getWorkspaceId(), DATASET_RESOURCE_NAME, CloningInstructionsEnum.REFERENCE);
+        GcsBucketUtils.makeControlledGcsBucketUserShared(
+            resourceApi,
+            centralDataCollection.getId(),
+            DATASET_RESOURCE_NAME,
+            CloningInstructionsEnum.REFERENCE);
     UUID resourceId = controlledBucket.getResourceId();
+    GcpGcsBucketResource controlledBucketResource = controlledBucket.getGcpBucket();
     logger.info("Created controlled bucket {}", resourceId);
 
-    ReferencedGcpResourceApi referenceApi = ClientTestUtils.getReferencedGcpResourceClient(testUser, server);
+    // make a reference to the shared Bucket
+    GcpGcsBucketResource dataCollectionReferenceResource =
+        GcsBucketUtils.makeGcsBucketReference(
+            controlledBucketResource.getAttributes(),
+            referencedGcpResourceApi,
+            centralDataCollection.getId(),
+            "referenceBucket",
+            CloningInstructionsEnum.REFERENCE);
 
+    /**
+     * Scenario 1: Workspace without region should gain region from data collection. Workspace
+     * (policy=empty) + Data Collection (policy=us-central1). Result: OK & Workspace
+     * (policy=us-central1)
+     */
+    // We'll use the workspace allocated by the Base class as the target workspace.
 
-    CreatedWorkspace usCentralWorkspace = CreateWorkspaceWithRegionPolicy(workspaceApi, "gcp.us-central1");
+    CloneReferencedGcpGcsBucketResourceResult bucketCloneResult =
+        referencedGcpResourceApi.cloneGcpGcsBucketReference(
+            new CloneReferencedResourceRequestBody().destinationWorkspaceId(getWorkspaceId()),
+            dataCollectionReferenceResource.getMetadata().getWorkspaceId(),
+            dataCollectionReferenceResource.getMetadata().getResourceId());
 
+    validateWorkspaceContainsRegionPolicy(workspaceApi, getWorkspaceId(), gcpCentralLocation);
 
+    /**
+     * Scenario 2: Workspace region should be reduced to data collection region. Workspace
+     * (policy=usa) + Data Collection (policy=us-central1). Result: OK & Workspace
+     * (policy=us-central1)
+     */
+    CreatedWorkspace scenario2Workspace =
+        CreateWorkspaceWithRegionPolicy(workspaceApi, usaLocation);
 
+    referencedGcpResourceApi.cloneGcpGcsBucketReference(
+        new CloneReferencedResourceRequestBody().destinationWorkspaceId(scenario2Workspace.getId()),
+        dataCollectionReferenceResource.getMetadata().getWorkspaceId(),
+        dataCollectionReferenceResource.getMetadata().getResourceId());
 
-    workspaceApi.deleteWorkspace(usCentralWorkspace.getId());
+    validateWorkspaceContainsRegionPolicy(
+        workspaceApi, scenario2Workspace.getId(), gcpCentralLocation);
+    workspaceApi.deleteWorkspace(scenario2Workspace.getId());
 
-    assertNotEquals(usCentralWorkspace.getId(), getWorkspaceId());
+    /**
+     * Scenario 3: Workspace region isn't updated if data collection doesn't specify a region.
+     * Workspace (policy=east) + Data Collection (policy=empty). Result: OK & Workspace
+     * (policy=east)
+     */
+    CreatedWorkspace eastWorkspace = CreateWorkspaceWithRegionPolicy(workspaceApi, gcpEastLocation);
+
+    CreatedWorkspace noPolicyDataCollection =
+        createWorkspace(UUID.randomUUID(), getSpendProfileId(), workspaceApi);
+
+    GcpGcsBucketResource noPolicyReferenceResource =
+        GcsBucketUtils.makeGcsBucketReference(
+            controlledBucketResource.getAttributes(),
+            referencedGcpResourceApi,
+            noPolicyDataCollection.getId(),
+            "referenceBucket",
+            CloningInstructionsEnum.REFERENCE);
+
+    referencedGcpResourceApi.cloneGcpGcsBucketReference(
+        new CloneReferencedResourceRequestBody().destinationWorkspaceId(eastWorkspace.getId()),
+        noPolicyReferenceResource.getMetadata().getWorkspaceId(),
+        noPolicyReferenceResource.getMetadata().getResourceId());
+
+    validateWorkspaceContainsRegionPolicy(workspaceApi, eastWorkspace.getId(), gcpEastLocation);
+    // don't delete this workspace because we'll reuse it in the next scenario.
+
+    /**
+     * Scenario 4: Workspace and data collection have incompatible region policies. Workspace
+     * (policy=east) + Data Collection (policy=central). Result: Policy Exception
+     */
+    assertThrows(
+        ApiException.class,
+        () ->
+            referencedGcpResourceApi.cloneGcpGcsBucketReference(
+                new CloneReferencedResourceRequestBody()
+                    .destinationWorkspaceId(eastWorkspace.getId()),
+                dataCollectionReferenceResource.getMetadata().getWorkspaceId(),
+                dataCollectionReferenceResource.getMetadata().getResourceId()),
+        "Policy merge has conflicts");
+    workspaceApi.deleteWorkspace(eastWorkspace.getId());
+
+    /**
+     * Scenario 5: Workspace has compatible policy but an incompatible resource. Workspace
+     * (policy=usa,resource=east) + Data Collection (policy=central). Result: Policy Exception
+     */
+    CreatedWorkspace scenario5Workspace =
+        CreateWorkspaceWithRegionPolicy(workspaceApi, usaLocation);
+
+    List<Property> properties = new ArrayList<>();
+    properties.add(new Property().key("terra-default-location").value("us-east1"));
+    workspaceApi.updateWorkspaceProperties(properties, scenario5Workspace.getId());
+
+    // Create a cloud context
+    projectId = CloudContextMaker.createGcpCloudContext(scenario5Workspace.getId(), workspaceApi);
+    logger.info("Created project {}", projectId);
+
+    // Create a shared Bucket
+    controlledBucket =
+        GcsBucketUtils.makeControlledGcsBucketUserShared(
+            resourceApi,
+            scenario5Workspace.getId(),
+            DATASET_RESOURCE_NAME,
+            CloningInstructionsEnum.REFERENCE);
+    resourceId = controlledBucket.getResourceId();
+
+    logger.info("Created controlled bucket {}", resourceId);
+
+    assertThrows(
+        ApiException.class,
+        () ->
+            referencedGcpResourceApi.cloneGcpGcsBucketReference(
+                new CloneReferencedResourceRequestBody()
+                    .destinationWorkspaceId(scenario5Workspace.getId()),
+                dataCollectionReferenceResource.getMetadata().getWorkspaceId(),
+                dataCollectionReferenceResource.getMetadata().getResourceId()),
+        "Policy merge has conflicts");
+
+    workspaceApi.deleteWorkspace(scenario5Workspace.getId());
+    workspaceApi.deleteWorkspace(centralDataCollection.getId());
   }
 
-  private CreatedWorkspace CreateWorkspaceWithRegionPolicy(WorkspaceApi workspaceApi, String location) throws Exception {
+  private CreatedWorkspace CreateWorkspaceWithRegionPolicy(
+      WorkspaceApi workspaceApi, String location) throws Exception {
     UUID workspaceId = UUID.randomUUID();
 
-    WsmPolicyInputs policies = new WsmPolicyInputs()
-      .addInputsItem(
-        new WsmPolicyInput()
-          .name("region-constraint")
-          .namespace("terra")
-          .additionalData(
-            new ArrayList<WsmPolicyPair>() {{ add(new WsmPolicyPair().key("region-name").value(location)); }}
-          ));
+    WsmPolicyInputs policies =
+        new WsmPolicyInputs()
+            .addInputsItem(
+                new WsmPolicyInput()
+                    .name("region-constraint")
+                    .namespace("terra")
+                    .additionalData(
+                        new ArrayList<WsmPolicyPair>() {
+                          {
+                            add(new WsmPolicyPair().key("region-name").value(location));
+                          }
+                        }));
 
     Properties properties = new Properties();
     Property property1 = new Property().key("foo").value("bar");
@@ -93,19 +221,32 @@ public class ImportDataCollection extends WorkspaceAllocateTestScriptBase {
     properties.add(property2);
 
     final var requestBody =
-      new CreateWorkspaceRequestBody()
-        .id(workspaceId)
-        .stage(getStageModel())
-        .policies(policies)
-        .properties(properties);
+        new CreateWorkspaceRequestBody()
+            .id(workspaceId)
+            .spendProfile(getSpendProfileId())
+            .stage(getStageModel())
+            .policies(policies)
+            .properties(properties);
     final CreatedWorkspace workspace = workspaceApi.createWorkspace(requestBody);
     assertThat(workspace.getId(), equalTo(workspaceId));
     return workspace;
   }
 
+  private void validateWorkspaceContainsRegionPolicy(
+      WorkspaceApi workspaceApi, UUID workspaceId, String region) throws Exception {
+    WorkspaceDescription updatedWorkspace = workspaceApi.getWorkspace(workspaceId, null);
+    List<WsmPolicyInput> updatedPolicies = updatedWorkspace.getPolicies();
+
+    assertEquals(1, updatedPolicies.size());
+    assertEquals("region-constraint", updatedPolicies.get(0).getName());
+    WsmPolicyPair regionPolicy = updatedPolicies.get(0).getAdditionalData().get(0);
+    assertEquals("region-name", regionPolicy.getKey());
+    assertEquals(region, regionPolicy.getValue());
+  }
+
   @Override
   public void doCleanup(List<TestUserSpecification> testUsers, WorkspaceApi workspaceApi)
-    throws Exception {
+      throws Exception {
     try {
       workspaceApi.deleteWorkspace(getWorkspaceId());
     } catch (ApiException e) {
