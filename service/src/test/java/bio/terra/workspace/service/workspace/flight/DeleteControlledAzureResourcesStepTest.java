65c389b214174da5beffe4eeccbc9cfba0397578
Marcus Talbott <mtalbott@broadinstitute.org>
2023-02-09 16:34:36 -0500

    happy path

--- service/src/test/java/bio/terra/workspace/service/workspace/flight/DeleteControlledAzureResourcesStepTest.java
+++ service/src/test/java/bio/terra/workspace/service/workspace/flight/DeleteControlledAzureResourcesStepTest.java
@@ -30,14 +30,14 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-public class DeleteControlledAzureResourcesStepTest extends BaseAzureUnitTest {
+class DeleteControlledAzureResourcesStepTest extends BaseAzureUnitTest {
   @Mock private ControlledResourceService mockControlledResourceService;
   @Mock private ResourceDao mockResourceDao;
   @Mock private SamService mockSamService;
   @Mock private FlightContext mockFlightContext;
 
   @Test
-  public void testRequiresDeleteAction() throws InterruptedException, RetryException {
+  void testRequiresDeleteAction() throws InterruptedException, RetryException {
     UUID workspaceId = UUID.randomUUID();
     AuthenticatedUserRequest userRequest =
         new AuthenticatedUserRequest("user@example.com", "subjectId", Optional.empty());
@@ -105,5 +105,75 @@ public class DeleteControlledAzureResourcesStepTest extends BaseAzureUnitTest {
         .deleteControlledResourceSync(any(), any(), any());
     verify(mockControlledResourceService, never())
         .deleteControlledResourceAsync(any(), any(), any(), any(), any());
+  }
+
+  @Test
+  void testDeletesAllResources() throws InterruptedException, RetryException {
+    UUID workspaceId = UUID.randomUUID();
+    AuthenticatedUserRequest userRequest =
+        new AuthenticatedUserRequest("user@example.com", "subjectId", Optional.empty());
+    ControlledResource deleteMe =
+        ControlledAzureStorageContainerResource.builder()
+            .common(
+                ControlledResourceFields.builder()
+                    .workspaceUuid(workspaceId)
+                    .resourceId(UUID.randomUUID())
+                    .name("deleteMe")
+                    .cloningInstructions(CloningInstructions.COPY_NOTHING)
+                    .createdByEmail(userRequest.getEmail())
+                    .managedBy(ManagedByType.MANAGED_BY_USER)
+                    .accessScope(AccessScopeType.ACCESS_SCOPE_SHARED)
+                    .build())
+            .storageAccountId(UUID.randomUUID())
+            .storageContainerName("user-storage-container")
+            .build();
+    ControlledResource deleteMeToo =
+        ControlledAzureStorageContainerResource.builder()
+            .common(
+                ControlledResourceFields.builder()
+                    .workspaceUuid(workspaceId)
+                    .resourceId(UUID.randomUUID())
+                    .name("deleteMeToo")
+                    .cloningInstructions(CloningInstructions.COPY_NOTHING)
+                    .createdByEmail("application@example.com")
+                    .managedBy(ManagedByType.MANAGED_BY_APPLICATION)
+                    .accessScope(AccessScopeType.ACCESS_SCOPE_SHARED)
+                    .build())
+            .storageAccountId(UUID.randomUUID())
+            .storageContainerName("application-storage-container")
+            .build();
+    doReturn(List.of(deleteMe, deleteMeToo))
+        .when(mockResourceDao)
+        .listControlledResources(workspaceId, CloudPlatform.AZURE);
+    doReturn(true)
+        .when(mockSamService)
+        .isAuthorized(
+            userRequest,
+            deleteMe.getCategory().getSamResourceName(),
+            deleteMe.getResourceId().toString(),
+            SamConstants.SamControlledResourceActions.DELETE_ACTION);
+    doReturn(true)
+        .when(mockSamService)
+        .isAuthorized(
+            userRequest,
+            deleteMeToo.getCategory().getSamResourceName(),
+            deleteMeToo.getResourceId().toString(),
+            SamConstants.SamControlledResourceActions.DELETE_ACTION);
+
+    DeleteControlledAzureResourcesStep deleteControlledAzureResourcesStep =
+        new DeleteControlledAzureResourcesStep(
+            mockResourceDao,
+            mockControlledResourceService,
+            mockSamService,
+            workspaceId,
+            userRequest);
+
+    final StepResult result = deleteControlledAzureResourcesStep.doStep(mockFlightContext);
+
+    assertThat(result, equalTo(StepResult.getStepResultSuccess()));
+    verify(mockControlledResourceService)
+        .deleteControlledResourceSync(workspaceId, deleteMe.getResourceId(), userRequest);
+    verify(mockControlledResourceService)
+        .deleteControlledResourceSync(workspaceId, deleteMeToo.getResourceId(), userRequest);
   }
 }
