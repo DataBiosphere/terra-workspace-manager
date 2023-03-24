c1f320ade7d1bfcf691176600fa5b0fa749b99af
John Czerkowicz <67699527+jmczerk@users.noreply.github.com>
2023-03-24 18:51:25 -0400

    Consume latest terra-aws-resource-discovery. (#1173)

--- service/src/test/java/bio/terra/workspace/common/utils/AwsUtilsTest.java
+++ service/src/test/java/bio/terra/workspace/common/utils/AwsUtilsTest.java
@@ -2,6 +2,7 @@ package bio.terra.workspace.common.utils;
 
 import bio.terra.aws.resource.discovery.Environment;
 import bio.terra.aws.resource.discovery.LandingZone;
+import bio.terra.aws.resource.discovery.Metadata;
 import bio.terra.workspace.app.configuration.external.AwsConfiguration;
 import bio.terra.workspace.app.configuration.external.FeatureConfiguration;
 import bio.terra.workspace.common.BaseAwsConnectedTest;
@@ -10,6 +11,8 @@ import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -18,16 +21,25 @@ import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 
 @Tag("awsConnected")
 public class AwsUtilsTest extends BaseAwsConnectedTest {
-
+  private static final Logger logger = LoggerFactory.getLogger(AwsUtilsTest.class);
   @Autowired private AwsConfiguration awsConfiguration;
   @Autowired private FeatureConfiguration featureConfiguration;
 
+  private void logEnvironmentMetadata(Metadata metadata) {
+    logger.info("AWS Environment Infrastructure Details:");
+    logger.info("       Terra Tenant Alias: {}", metadata.getTenantAlias());
+    logger.info("      AWS Organization ID: {}", metadata.getOrganizationId());
+    logger.info("  Terra Environment Alias: {}", metadata.getEnvironmentAlias());
+    logger.info("           AWS Account ID: {}", metadata.getAccountId());
+    logger.info("     Module Major Version: {}", metadata.getMajorVersion());
+  }
+
   @Test
   void hello_bucket() throws IOException {
-    Assertions.assertDoesNotThrow(
-        () -> {
-          featureConfiguration.awsEnabledCheck();
-        });
+    Assertions.assertDoesNotThrow(() -> featureConfiguration.awsEnabledCheck());
+
+    // Log the AWS config
+    logger.info("AWS Configuration: {}", awsConfiguration.toString());
 
     // Create the EnvironmentDiscovery instance.  This should be created at service instantiation
     // and live for the lifetime of the service process.
@@ -37,14 +49,17 @@ public class AwsUtilsTest extends BaseAwsConnectedTest {
     // caching will happen under the hood to prevent too many S3 calls to the discovery bucket.
     Environment environment = environmentDiscovery.discoverEnvironment();
 
-    // This lifetime should track that of the discovered environment where possible (stale creds
-    // will get refreshed under the hood).
+    // This lifetime should track that of the discovered environment where possible (stale
+    // credentials will get refreshed under the hood).
     AwsCredentialsProvider awsCredentialsProvider =
         AwsUtils.createWsmCredentialProvider(awsConfiguration, environment);
 
+    // Log details about the discovered environment.
+    logEnvironmentMetadata(environment.getMetadata());
+
     // Iterate over supported regions in Environment.
 
-    for (Region region : environmentDiscovery.discoverEnvironment().getSupportedRegions()) {
+    for (Region region : environment.getSupportedRegions()) {
       Optional<LandingZone> landingZoneOptional = environment.getLandingZone(region);
       Assertions.assertTrue(landingZoneOptional.isPresent());
       LandingZone landingZone = landingZoneOptional.get();
@@ -53,16 +68,19 @@ public class AwsUtilsTest extends BaseAwsConnectedTest {
       // permissions on the bucket, which the WSM role has.  This is a basic "Hello, world!" test
       // that we've discovered the environment and assumed the TerraWorkspaceManager role.
 
-      S3Client s3Client =
-          S3Client.builder().region(region).credentialsProvider(awsCredentialsProvider).build();
+      String bucketName = landingZone.getStorageBucket().name();
+      HeadBucketRequest request = HeadBucketRequest.builder().bucket(bucketName).build();
 
-      HeadBucketRequest request =
-          HeadBucketRequest.builder().bucket(landingZone.getStorageBucket().name()).build();
+      // SdkClient classes should generally be used in try-with-resource blocks.
+      try (S3Client s3Client =
+          S3Client.builder().region(region).credentialsProvider(awsCredentialsProvider).build()) {
+        s3Client.headBucket(request);
+      } catch (Exception exception) {
+        Assertions.fail(String.format("HeadBucket request failed: %s", exception), exception);
+      }
 
-      Assertions.assertDoesNotThrow(
-          () -> {
-            s3Client.headBucket(request);
-          });
+      logger.info(
+          "Confirmed access to bucket '{}' in AWS Region {}", bucketName, region.toString());
     }
   }
 }
