1013bead5bfbfac98ca348859fb57c435b5d2240
Aaron Wang <66176554+aaron-wang@users.noreply.github.com>
2023-03-27 17:28:55 +0000

    [PF-2452] Handle exceeded quota limit in create notebook step. (#1157)
    Update the logic of pollAndRetry in GcpUtils.
    * Handle case of 403 to catch quota limit exceeded error for AI notebooks
    * Throw more specific errors for certain 4XX codes

--- service/src/test/java/bio/terra/workspace/common/utils/GcpUtilsTest.java
+++ service/src/test/java/bio/terra/workspace/common/utils/GcpUtilsTest.java
@@ -1,11 +1,23 @@
 package bio.terra.workspace.common.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+import bio.terra.cloudres.google.api.services.common.OperationCow;
+import bio.terra.common.exception.ForbiddenException;
 import bio.terra.workspace.common.BaseUnitTest;
+import com.google.api.services.notebooks.v1.model.Operation;
+import com.google.rpc.Code;
+import java.time.Duration;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 public class GcpUtilsTest extends BaseUnitTest {
+
+  @Mock private OperationCow<Operation> mockOperationCow;
+  @Mock private OperationCow.OperationAdapter<Operation> mockOperationAdapter;
+  @Mock private OperationCow.OperationAdapter.StatusAdapter mockOperationStatusAdapter;
 
   @Test
   public void parseRegionWithRegions() {
@@ -26,5 +38,22 @@ public class GcpUtilsTest extends BaseUnitTest {
     assertEquals("us-east1", GcpUtils.parseRegion("us-east1-a"));
     assertEquals("asia-northeast3", GcpUtils.parseRegion("asia-northeast3-c"));
     assertEquals("australia-southeast2", GcpUtils.parseRegion("australia-southeast2-b"));
+  }
+
+  // The permission denied code includes cases of the "quota exceeded limit" error for AI notebook
+  // creation.
+  @Test
+  public void pollAndRetry_catchesPermissionDeniedError_forOperation() {
+    // Mock the OperationCow.
+    when(mockOperationCow.getOperationAdapter()).thenReturn(mockOperationAdapter);
+    when(mockOperationAdapter.getDone()).thenReturn(true);
+
+    // Mock the permission error thrown when polling the error (e.g., quota exceeded limit)
+    when(mockOperationAdapter.getError()).thenReturn(mockOperationStatusAdapter);
+    when(mockOperationStatusAdapter.getCode()).thenReturn(Code.PERMISSION_DENIED_VALUE);
+    assertThrows(
+        ForbiddenException.class,
+        () ->
+            GcpUtils.pollAndRetry(mockOperationCow, Duration.ofSeconds(5), Duration.ofMinutes(1)));
   }
 }
