ba31d7fe4c0362cc97cc987835e367738ac6d406
Aaron Wang <66176554+aaron-wang@users.noreply.github.com>
2023-03-31 14:03:53 +0000

    Retrieve machineType and acceleratorConfig if respective attributes are null.

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/gcp/ainotebook/ControlledAiNotebookHandler.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/gcp/ainotebook/ControlledAiNotebookHandler.java
@@ -1,13 +1,27 @@
 package bio.terra.workspace.service.resource.controlled.cloud.gcp.ainotebook;
 
+import bio.terra.cloudres.google.notebooks.AIPlatformNotebooksCow;
+import bio.terra.cloudres.google.notebooks.InstanceName;
+import bio.terra.workspace.common.utils.RetryUtils;
 import bio.terra.workspace.db.DbSerDes;
 import bio.terra.workspace.db.model.DbResource;
+import bio.terra.workspace.service.crl.CrlService;
 import bio.terra.workspace.service.resource.controlled.model.ControlledResourceFields;
 import bio.terra.workspace.service.resource.model.WsmResource;
 import bio.terra.workspace.service.resource.model.WsmResourceHandler;
 import bio.terra.workspace.service.workspace.GcpCloudContextService;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.gax.rpc.ApiException;
+import com.google.cloud.notebooks.v1.GetInstanceRequest;
+import com.google.cloud.notebooks.v1.Instance;
+import com.google.cloud.notebooks.v1.NotebookServiceClient;
 import com.google.common.base.CharMatcher;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import javax.annotation.PostConstruct;
@@ -15,6 +29,10 @@ import javax.ws.rs.BadRequestException;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static bio.terra.workspace.common.utils.RetryUtils.DEFAULT_RETRY_FACTOR_INCREASE;
+import static bio.terra.workspace.common.utils.RetryUtils.DEFAULT_RETRY_SLEEP_DURATION;
+import static bio.terra.workspace.common.utils.RetryUtils.DEFAULT_RETRY_SLEEP_DURATION_MAX;
 
 @SuppressFBWarnings(
     value = "ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD",
@@ -50,13 +68,52 @@ public class ControlledAiNotebookHandler implements WsmResourceHandler {
         Optional.ofNullable(attributes.getProjectId())
             .orElse(gcpCloudContextService.getRequiredGcpProject(dbResource.getWorkspaceId()));
 
+    // Notebook attributes created prior to PF-2323 do not have machineType or acceleratorConfig
+    // stored.
+    // Thus, retrieve these two values from the cloud.
+    String retrievedMachineType = attributes.getMachineType();
+    AcceleratorConfig retrievedAcceleratorConfig = attributes.getAcceleratorConfig();
+
+    if (retrievedMachineType == null || retrievedAcceleratorConfig == null) {
+      try (NotebookServiceClient notebookServiceClient = NotebookServiceClient.create()) {
+        InstanceName instanceName =
+            InstanceName.builder()
+                .instanceId(attributes.getInstanceId())
+                .projectId(attributes.getProjectId())
+                .location(attributes.getLocation())
+                .build();
+
+        List<Class<? extends Exception>> retryableErrors = new ArrayList<>();
+        retryableErrors.add(ApiException.class);
+        Instance cloudInstance =
+            RetryUtils.getWithRetryOnException(
+                () ->
+                    notebookServiceClient.getInstance(
+                        GetInstanceRequest.newBuilder().setName(instanceName.formatName()).build()),
+                Duration.ofMinutes(5),
+                DEFAULT_RETRY_SLEEP_DURATION,
+                DEFAULT_RETRY_FACTOR_INCREASE,
+                DEFAULT_RETRY_SLEEP_DURATION_MAX,
+                retryableErrors);
+
+        retrievedMachineType = cloudInstance.getMachineType();
+        retrievedAcceleratorConfig =
+            new AcceleratorConfig(
+                cloudInstance.getMachineType(),
+                cloudInstance.getAcceleratorConfig().getCoreCount());
+
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+
     return ControlledAiNotebookInstanceResource.builder()
         .common(new ControlledResourceFields(dbResource))
         .instanceId(attributes.getInstanceId())
         .location(attributes.getLocation())
         .projectId(projectId)
-        .machineType(attributes.getMachineType())
-        .acceleratorConfig(attributes.getAcceleratorConfig())
+        .machineType(retrievedMachineType)
+        .acceleratorConfig(retrievedAcceleratorConfig)
         .build();
   }
 
