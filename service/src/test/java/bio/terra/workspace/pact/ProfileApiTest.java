1fb8fc45bf731579699a52418e01f42a60650bf5
Blake Geno <bgeno@broadinstitute.org>
2023-04-27 11:06:49 -0400

    added second test, and usage of pathFromProviderState

--- service/src/test/java/bio/terra/workspace/pact/ProfileApiTest.java
+++ service/src/test/java/bio/terra/workspace/pact/ProfileApiTest.java
@@ -1,9 +1,11 @@
 package bio.terra.workspace.pact;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
@@ -12,43 +14,66 @@ import bio.terra.workspace.service.iam.AuthenticatedUserRequest;
 import bio.terra.workspace.service.iam.SamService;
 import bio.terra.workspace.service.spendprofile.SpendProfileId;
 import bio.terra.workspace.service.spendprofile.SpendProfileService;
+import bio.terra.workspace.service.spendprofile.exceptions.SpendUnauthorizedException;
+import bio.terra.workspace.service.workspace.model.CloudPlatform;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import org.mockito.Mockito;
-
-import java.util.Optional;
 
 @Tag("pact-test")
 @ExtendWith(PactConsumerTestExt.class)
 public class ProfileApiTest {
 
+  // This will need to be set up with BPM
   static final String billingProfileId = "4a5afeaa-b3b2-fa51-8e4e-9dbf294b7837";
 
   @Pact(consumer = "wsm-consumer", provider = "bpm-provider")
-  public RequestResponsePact retrievingABillingProfile(PactDslWithProvider builder) {
-    var cloudPlatformStrings =  "AZURE|GCP";
-        //Arrays.stream(CloudPlatform.values()).map(Enum::name).collect(Collectors.joining("|"));
-    var billingProfileResponseShape = new PactDslJsonBody()
-        .stringMatcher("cloudPlatform", cloudPlatformStrings)
-        .uuid("tenantId")
-        .uuid("subscriptionId")
-        .stringType("managedResourceGroupId")
-        .stringType("billingAccountId")
-        .uuid("id");
+  public RequestResponsePact billingProfileAvailable(PactDslWithProvider builder) {
+    // Using the WSM version of this, instead of the BPM version,
+    // because what we care about is not what BPM sends, but that WSM can handle the value
+    var cloudPlatformStrings =
+        Stream.of(CloudPlatform.AZURE, CloudPlatform.GCP)
+            .map(CloudPlatform::toSql)
+            .collect(Collectors.joining("|"));
+    var billingProfileResponseShape =
+        new PactDslJsonBody()
+            .stringMatcher("cloudPlatform", cloudPlatformStrings)
+            .uuid("tenantId")
+            .uuid("subscriptionId")
+            .stringType("managedResourceGroupId")
+            .stringType("billingAccountId")
+            .uuid("id");
     return builder
+        .given("an existing billing profile")
         .uponReceiving("A request to retrieve a billing profile")
         .method("GET")
-        .path(String.format("/api/profiles/v1/%s", billingProfileId))
-        .willRespondWith().status(200)
+        .pathFromProviderState(
+            "/api/profiles/v1/${profileId}", String.format("/api/profiles/v1/%s", billingProfileId))
+        .willRespondWith()
+        .status(200)
         .body(billingProfileResponseShape)
         .toPact();
   }
 
+  @Pact(consumer = "wsm-consumer", provider = "bpm-provider")
+  public RequestResponsePact billingProfileUnAvailable(PactDslWithProvider builder) {
+    return builder
+        .uponReceiving("A request to retrieve a billing profile")
+        .method("GET")
+        .pathFromProviderState(
+            "/api/profiles/v1/${profileId}", String.format("/api/profiles/v1/%s", billingProfileId))
+        .willRespondWith()
+        .status(403)
+        .toPact();
+  }
+
   @Test
-  @PactTestFor(pactMethod = "retrievingABillingProfile")
-  public void testAuthorizingLinking(MockServer mockServer) throws Exception {
+  @PactTestFor(pactMethod = "billingProfileAvailable")
+  public void testAuthorizingLinkingOfAnExistingProfile(MockServer mockServer) {
     var config = new SpendProfileConfiguration();
 
     config.setBasePath(mockServer.getUrl());
@@ -61,8 +86,23 @@ public class ProfileApiTest {
     var service = new SpendProfileService(samService, config);
 
     service.authorizeLinking(spendProfileId, true, userRequest);
-
   }
 
+  @Test
+  @PactTestFor(pactMethod = "billingProfileUnAvailable")
+  public void testAuthorizingLinkingOfAnNonexistantProfile(MockServer mockServer) {
+    var config = new SpendProfileConfiguration();
 
+    config.setBasePath(mockServer.getUrl());
+
+    var samService = Mockito.mock(SamService.class);
+
+    var userRequest = new AuthenticatedUserRequest();
+    userRequest.token(Optional.of("dummyValue"));
+    var spendProfileId = new SpendProfileId(billingProfileId);
+    var service = new SpendProfileService(samService, config);
+    assertThrows(
+        SpendUnauthorizedException.class,
+        () -> service.authorizeLinking(spendProfileId, true, userRequest));
+  }
 }
