0530ae9e472e091af00fd9f9d4e0fe700a230ef1
Dex Amundsen <dexamundsen@verily.com>
2023-05-03 12:10:57 -0700

    comment update

--- service/src/test/java/bio/terra/workspace/common/BaseAwsUnitTest.java
+++ service/src/test/java/bio/terra/workspace/common/BaseAwsUnitTest.java
@@ -3,6 +3,7 @@ package bio.terra.workspace.common;
 import org.junit.jupiter.api.Tag;
 import org.springframework.test.context.ActiveProfiles;
 
+/** Base class for AWS unit tests: not connected to AWS */
 @Tag("aws-unit")
 @ActiveProfiles({"aws-unit-test", "unit-test"})
 public class BaseAwsUnitTest extends BaseUnitTestMocks {}
--- service/src/test/java/bio/terra/workspace/common/BaseUnitTest.java
+++ service/src/test/java/bio/terra/workspace/common/BaseUnitTest.java
@@ -3,6 +3,7 @@ package bio.terra.workspace.common;
 import org.junit.jupiter.api.Tag;
 import org.springframework.test.context.ActiveProfiles;
 
+/** Base class for (GCP) unit tests: not connected (to GCP) */
 @Tag("unit")
 @ActiveProfiles("unit-test")
 public class BaseUnitTest extends BaseUnitTestMocks {}
