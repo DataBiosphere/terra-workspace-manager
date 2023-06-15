ab3d30a6df66fa895f78054165a48a66e32f083b
Iliad Shaghaghi <ishaghag@google.com>
2023-06-15 18:25:18 +0000

    Update default zone in test

--- service/src/test/java/bio/terra/workspace/service/resource/controlled/ControlledResourceServiceGceInstanceConnectedTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/ControlledResourceServiceGceInstanceConnectedTest.java
@@ -87,7 +87,7 @@ public class ControlledResourceServiceGceInstanceConnectedTest extends BaseConne
   /** The default roles to use when creating user private GCE instance resources */
   private static final ControlledResourceIamRole DEFAULT_ROLE = ControlledResourceIamRole.WRITER;
   /** The default GCP location to create instancess for this test. */
-  private static final String DEFAULT_INSTANCE_ZONE = "us-central1-a";
+  private static final String DEFAULT_INSTANCE_ZONE = "us-east1-b";
 
   // Store workspaceId instead of workspace so that for local development, one can easily use a
   // previously created workspace.
