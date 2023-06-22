d7f33b31e46fa7c6ef8983df10305bc80d06d84e
Dex Amundsen <dexamundsen@verily.com>
2023-06-21 19:00:25 -0700

    S3 Storage Folder Unit tests

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/aws/s3StorageFolder/DeleteAwsS3StorageFolderStep.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/aws/s3StorageFolder/DeleteAwsS3StorageFolderStep.java
@@ -1,15 +1,22 @@
 package bio.terra.workspace.service.resource.controlled.cloud.aws.s3StorageFolder;
 
+import bio.terra.common.exception.ApiException;
+import bio.terra.common.exception.NotFoundException;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
+import bio.terra.stairway.StepStatus;
 import bio.terra.stairway.exception.RetryException;
 import bio.terra.workspace.common.exception.InternalLogicException;
 import bio.terra.workspace.common.utils.AwsUtils;
 import bio.terra.workspace.service.workspace.AwsCloudContextService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 
 public class DeleteAwsS3StorageFolderStep implements Step {
+
+  private static final Logger logger = LoggerFactory.getLogger(DeleteAwsS3StorageFolderStep.class);
   private final ControlledAwsS3StorageFolderResource resource;
   private final AwsCloudContextService awsCloudContextService;
 
@@ -23,13 +30,7 @@ public class DeleteAwsS3StorageFolderStep implements Step {
   @Override
   public StepResult doStep(FlightContext flightContext)
       throws InterruptedException, RetryException {
-    AwsCredentialsProvider credentialsProvider =
-        AwsUtils.createWsmCredentialProvider(
-            awsCloudContextService.getRequiredAuthentication(),
-            awsCloudContextService.discoverEnvironment());
-
-    AwsUtils.deleteStorageFolder(credentialsProvider, resource);
-    return StepResult.getStepResultSuccess();
+    return executeDeleteAwsS3StorageFolder(awsCloudContextService, resource);
   }
 
   @Override
@@ -40,5 +41,24 @@ public class DeleteAwsS3StorageFolderStep implements Step {
             String.format(
                 "Cannot undo delete of AWS S3 Storage Folder resource %s in workspace %s.",
                 resource.getResourceId(), resource.getWorkspaceId())));
+  }
+
+  static StepResult executeDeleteAwsS3StorageFolder(
+      AwsCloudContextService awsCloudContextService,
+      ControlledAwsS3StorageFolderResource resource) {
+    AwsCredentialsProvider credentialsProvider =
+        AwsUtils.createWsmCredentialProvider(
+            awsCloudContextService.getRequiredAuthentication(),
+            awsCloudContextService.discoverEnvironment());
+
+    try {
+      AwsUtils.deleteStorageFolder(credentialsProvider, resource);
+    } catch (ApiException e) {
+      return new StepResult(StepStatus.STEP_RESULT_FAILURE_FATAL, e);
+    } catch (NotFoundException e) {
+      logger.debug("No storage folder {} to delete.", resource.getName());
+    }
+
+    return StepResult.getStepResultSuccess();
   }
 }
--- service/src/test/java/bio/terra/workspace/common/fixtures/ControlledAwsResourceFixtures.java
+++ service/src/test/java/bio/terra/workspace/common/fixtures/ControlledAwsResourceFixtures.java
@@ -1,26 +1,73 @@
 package bio.terra.workspace.common.fixtures;
 
+import static bio.terra.workspace.common.fixtures.ControlledResourceFixtures.WORKSPACE_ID;
+import static bio.terra.workspace.common.fixtures.WorkspaceFixtures.DEFAULT_SPEND_PROFILE_ID;
 import static software.amazon.awssdk.services.sagemaker.model.InstanceType.ML_T2_MEDIUM;
 
 import bio.terra.workspace.service.resource.controlled.cloud.aws.s3StorageFolder.ControlledAwsS3StorageFolderResource;
 import bio.terra.workspace.service.resource.controlled.cloud.aws.sageMakerNotebook.ControlledAwsSageMakerNotebookResource;
 import bio.terra.workspace.service.resource.controlled.model.AccessScopeType;
+import bio.terra.workspace.service.resource.model.WsmResourceState;
+import bio.terra.workspace.service.workspace.model.AwsCloudContext;
+import bio.terra.workspace.service.workspace.model.AwsCloudContextFields;
+import bio.terra.workspace.service.workspace.model.AwsCloudContextFields.AwsCloudContextV1;
+import bio.terra.workspace.service.workspace.model.CloudContextCommonFields;
 import java.util.UUID;
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.http.SdkHttpResponse;
 
 public class ControlledAwsResourceFixtures {
+  public static final long V1_VERSION = AwsCloudContextV1.getVersion();
+  public static final String MAJOR_VERSION = "v0.5.8";
+  public static final String ORGANIZATION_ID = "o-organization";
+  public static final String ACCOUNT_ID = "1245893245";
+  public static final String TENANT_ALIAS = "tenant-saas";
+  public static final String ENVIRONMENT_ALIAS = "unit-test-env";
+  public static final String AWS_REGION = "us-east-1";
+
+  public static final SdkHttpResponse SDK_HTTP_RESPONSE_200 =
+      SdkHttpResponse.builder().statusCode(200).build();
+  public static final SdkHttpResponse SDK_HTTP_RESPONSE_400 =
+      SdkHttpResponse.builder().statusCode(400).build();
+  public static final AwsCredentialsProvider AWS_CREDENTIALS_PROVIDER =
+      AnonymousCredentialsProvider.create();
+
+  // Cloud context
+
+  public static AwsCloudContext makeAwsCloudContext() {
+    return new AwsCloudContext(
+        new AwsCloudContextFields(
+            MAJOR_VERSION, ORGANIZATION_ID, ACCOUNT_ID, TENANT_ALIAS, ENVIRONMENT_ALIAS),
+        new CloudContextCommonFields(
+            DEFAULT_SPEND_PROFILE_ID, WsmResourceState.READY, /*flightId=*/ null, /*error=*/ null));
+  }
 
   // S3 Folder
 
   public static ControlledAwsS3StorageFolderResource makeDefaultAwsS3StorageFolderResource(
       UUID workspaceUuid) {
+    return makeAwsS3StorageFolderResourceBuilder(
+            workspaceUuid, "s3-resource", "foo-bucket", "bar-prefix")
+        .build();
+  }
+
+  public static ControlledAwsS3StorageFolderResource.Builder makeAwsS3StorageFolderResourceBuilder(
+      String bucket, String prefix) {
+    return makeAwsS3StorageFolderResourceBuilder(WORKSPACE_ID, "foo", bucket, prefix);
+  }
+
+  public static ControlledAwsS3StorageFolderResource.Builder makeAwsS3StorageFolderResourceBuilder(
+      UUID workspaceUuid, String resourceName, String bucket, String prefix) {
     return ControlledAwsS3StorageFolderResource.builder()
         .common(
             ControlledResourceFixtures.makeDefaultControlledResourceFieldsBuilder()
                 .workspaceUuid(workspaceUuid)
+                .name(resourceName)
+                .region(AWS_REGION)
                 .build())
-        .bucketName("foo")
-        .prefix("bar")
-        .build();
+        .bucketName(bucket)
+        .prefix(prefix);
   }
 
   // Sagemaker Notebook
