0070a18abcc1b850905566c3e7f2decd581f36e4
cahrens <roberts@broadinstitute.org>
2023-07-13 13:41:22 -0400

    Anyone with read access to a workspace should be able to read all resources in the workspace (WOR-1102).

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/ControlledResourceMetadataManager.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/ControlledResourceMetadataManager.java
@@ -79,8 +79,10 @@ public class ControlledResourceMetadataManager {
     ControlledResource controlledResource = resource.castToControlledResource();
     String samName = controlledResource.getCategory().getSamResourceName();
 
+    // Everyone who is a reader (or above) on the workspace can see all of the resources in a workspace.
+    // Thus the authorization check is againt the workspace and not the resource itself.
     if (StringUtils.equals(action, SamControlledResourceActions.READ_ACTION)) {
-      checkResourceAuthz(userRequest, samName, resourceId, action);
+      checkResourceAuthz(userRequest, SamConstants.SamResource.WORKSPACE, workspaceUuid, action);
       return controlledResource;
     }
 
