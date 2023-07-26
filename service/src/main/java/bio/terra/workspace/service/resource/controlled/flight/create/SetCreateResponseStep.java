7c2018df3a0879da3055322d419dfd9081224bc1
zloery <6668057+zloery@users.noreply.github.com>
2023-07-26 17:34:05 -0400

    dismal failure after resource create flight releases lock

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/flight/create/SetCreateResponseStep.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/flight/create/SetCreateResponseStep.java
@@ -1,8 +1,12 @@
 package bio.terra.workspace.service.resource.controlled.flight.create;
 
+import static java.lang.Boolean.TRUE;
+
 import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
+import bio.terra.stairway.StepStatus;
 import bio.terra.stairway.exception.RetryException;
 import bio.terra.workspace.db.ResourceDao;
 import bio.terra.workspace.service.iam.AuthenticatedUserRequest;
@@ -11,6 +15,7 @@ import bio.terra.workspace.service.logging.WorkspaceActivityLogService;
 import bio.terra.workspace.service.resource.controlled.model.ControlledResource;
 import bio.terra.workspace.service.resource.model.WsmResource;
 import bio.terra.workspace.service.resource.model.WsmResourceStateRule;
+import bio.terra.workspace.service.workspace.flight.WorkspaceFlightMapKeys.ResourceKeys;
 import bio.terra.workspace.service.workspace.model.OperationType;
 
 public class SetCreateResponseStep implements Step {
@@ -42,9 +47,12 @@ public class SetCreateResponseStep implements Step {
   @Override
   public StepResult doStep(FlightContext flightContext)
       throws InterruptedException, RetryException {
+    FlightMap workingMap = flightContext.getWorkingMap();
+    workingMap.put(ResourceKeys.UPDATE_COMPLETE, Boolean.FALSE);
     WsmResource responseResource =
         resourceDao.createResourceSuccess(resource, flightContext.getFlightId());
     flightContext.getWorkingMap().put(JobMapKeys.RESPONSE.getKeyName(), responseResource);
+    workingMap.put(ResourceKeys.UPDATE_COMPLETE, TRUE);
     workspaceActivityLogService.writeActivity(
         userRequest,
         resource.getWorkspaceId(),
@@ -56,6 +64,16 @@ public class SetCreateResponseStep implements Step {
 
   @Override
   public StepResult undoStep(FlightContext flightContext) throws InterruptedException {
+    Boolean didUpdate =
+        flightContext.getWorkingMap().get(ResourceKeys.UPDATE_COMPLETE, Boolean.class);
+    if (TRUE.equals(didUpdate)) {
+      // If the update is complete, then we cannot undo it. This is a teeny tiny window
+      // where the error occurs after the update, but before the success return. However,
+      // the DebugInfo.lastStepFailure will always hit it.
+      return new StepResult(
+          StepStatus.STEP_RESULT_FAILURE_FATAL, new RuntimeException("dismal failure"));
+    }
+    // Failed before update - perform undo
     return StepResult.getStepResultSuccess();
   }
 }
