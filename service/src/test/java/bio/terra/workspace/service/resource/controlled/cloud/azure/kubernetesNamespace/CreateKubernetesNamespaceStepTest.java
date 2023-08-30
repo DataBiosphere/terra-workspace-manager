19ae3d44bdef968feb68024b07a9acf95f391dc6
Douglas Voet <dvoet@users.noreply.github.com>
2023-08-30 17:03:34 -0400

    more tests

--- service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/kubernetesNamespace/CreateKubernetesNamespaceStepTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/kubernetesNamespace/CreateKubernetesNamespaceStepTest.java
@@ -3,7 +3,6 @@ package bio.terra.workspace.service.resource.controlled.cloud.azure.kubernetesNa
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -86,8 +85,7 @@ public class CreateKubernetesNamespaceStepTest extends BaseMockitoStrictStubbing
             null,
             null))
         .thenThrow(new ApiException(HttpStatus.CONFLICT.value(), "message"));
-    when(mockKubernetesClientProvider.stepResultFromException(any(), eq(HttpStatus.CONFLICT)))
-        .thenReturn(StepResult.getStepResultSuccess());
+    when(mockKubernetesClientProvider.stepResultFromException(any(), any())).thenCallRealMethod();
 
     var result =
         new CreateKubernetesNamespaceStep(workspaceId, mockKubernetesClientProvider, resource)
@@ -138,7 +136,8 @@ public class CreateKubernetesNamespaceStepTest extends BaseMockitoStrictStubbing
     when(mockCoreV1Api.deleteNamespace(
             resource.getKubernetesNamespace(), null, null, null, null, null, null))
         .thenThrow(new ApiException(HttpStatus.NOT_FOUND.value(), "message"));
-    when(mockKubernetesClientProvider.stepResultFromException(any(), eq(HttpStatus.NOT_FOUND)))
+    when(mockKubernetesClientProvider.stepResultFromException(any(), any()))
+        .thenCallRealMethod()
         .thenReturn(StepResult.getStepResultSuccess());
 
     var result =
---
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/kubernetesNamespace/KubernetesNamespaceGuardStepTest.java
@@ -0,0 +1,90 @@
+package bio.terra.workspace.service.resource.controlled.cloud.azure.kubernetesNamespace;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.FlightMap;
+import bio.terra.stairway.StepStatus;
+import bio.terra.workspace.common.fixtures.ControlledAzureResourceFixtures;
+import bio.terra.workspace.common.utils.BaseMockitoStrictStubbingTest;
+import bio.terra.workspace.service.resource.controlled.cloud.azure.KubernetesClientProvider;
+import bio.terra.workspace.service.workspace.flight.WorkspaceFlightMapKeys.ControlledResourceKeys;
+import bio.terra.workspace.service.workspace.model.AzureCloudContext;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Namespace;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+
+@Tag("azure-unit")
+public class KubernetesNamespaceGuardStepTest extends BaseMockitoStrictStubbingTest {
+  @Mock private AzureCloudContext mockAzureCloudContext;
+  @Mock private FlightMap mockWorkingMap;
+  @Mock private FlightContext mockFlightContext;
+  @Mock private KubernetesClientProvider mockKubernetesClientProvider;
+  @Mock private CoreV1Api mockCoreV1Api;
+
+  @Test
+  void testDoStepSuccess() throws InterruptedException, ApiException {
+    var workspaceId = UUID.randomUUID();
+    var creationParameters =
+        ControlledAzureResourceFixtures.getAzureKubernetesNamespaceCreationParameters(
+            null, List.of());
+
+    var resource =
+        ControlledAzureResourceFixtures.makeSharedControlledAzureKubernetesNamespaceResourceBuilder(
+                creationParameters, workspaceId)
+            .build();
+
+    when(mockKubernetesClientProvider.createCoreApiClient(mockAzureCloudContext, workspaceId))
+        .thenReturn(mockCoreV1Api);
+    when(mockCoreV1Api.readNamespace(resource.getKubernetesNamespace(), null))
+        .thenThrow(new ApiException(HttpStatus.NOT_FOUND.value(), "not found"));
+    when(mockKubernetesClientProvider.stepResultFromException(any(), any())).thenCallRealMethod();
+
+    var result =
+        new KubernetesNamespaceGuardStep(workspaceId, mockKubernetesClientProvider, resource)
+            .doStep(createMockFlightContext());
+
+    assertThat(result.getStepStatus(), equalTo(StepStatus.STEP_RESULT_SUCCESS));
+  }
+
+  @Test
+  void testDoStepExists() throws InterruptedException, ApiException {
+    var workspaceId = UUID.randomUUID();
+    var creationParameters =
+        ControlledAzureResourceFixtures.getAzureKubernetesNamespaceCreationParameters(
+            null, List.of());
+
+    var resource =
+        ControlledAzureResourceFixtures.makeSharedControlledAzureKubernetesNamespaceResourceBuilder(
+                creationParameters, workspaceId)
+            .build();
+
+    when(mockKubernetesClientProvider.createCoreApiClient(mockAzureCloudContext, workspaceId))
+        .thenReturn(mockCoreV1Api);
+    when(mockCoreV1Api.readNamespace(resource.getKubernetesNamespace(), null))
+        .thenReturn(new V1Namespace());
+
+    var result =
+        new KubernetesNamespaceGuardStep(workspaceId, mockKubernetesClientProvider, resource)
+            .doStep(createMockFlightContext());
+
+    assertThat(result.getStepStatus(), equalTo(StepStatus.STEP_RESULT_FAILURE_FATAL));
+  }
+
+  private FlightContext createMockFlightContext() {
+    when(mockFlightContext.getWorkingMap()).thenReturn(mockWorkingMap);
+    when(mockWorkingMap.get(ControlledResourceKeys.AZURE_CLOUD_CONTEXT, AzureCloudContext.class))
+        .thenReturn(mockAzureCloudContext);
+
+    return mockFlightContext;
+  }
+}
