6e3c928cfe716c995037051cd35febe84b40b032
Douglas Voet <dvoet@users.noreply.github.com>
2023-09-18 09:48:14 -0400

    docs

--- service/src/main/java/bio/terra/workspace/common/utils/MakeFlightIdsStep.java
+++ service/src/main/java/bio/terra/workspace/common/utils/MakeFlightIdsStep.java
@@ -9,13 +9,20 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Generate flight ids and store them in the working map. The ids need to be stable across runs of
- * the RunDeleteCloudContextFlightStep.
+ * Generate flight ids and store them in the working map. This is useful for launching subflights
+ * because the flight id needs to be stable across runs of the subflight so that steps launching
+ * subflights can be idempotent.
  */
 public class MakeFlightIdsStep implements Step {
   private final List<String> keys;
   private final String flightMapKey;
 
+  /**
+   * Create a step that generates flight ids and stores them as a map in the working map.
+   *
+   * @param keys create a flight id for each of these keys
+   * @param flightMapKey store the flight ids in the working map under this key
+   */
   public MakeFlightIdsStep(List<String> keys, String flightMapKey) {
     this.keys = keys;
     this.flightMapKey = flightMapKey;
