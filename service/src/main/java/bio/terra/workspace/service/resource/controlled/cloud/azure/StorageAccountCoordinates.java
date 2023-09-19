e84115f9a6a6e0205af5541cb6201e10db22d5d9
aherbst-broad <aherbst@broadinstitute.org>
2023-09-19 17:11:46 -0400

    Lint

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/azure/StorageAccountCoordinates.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/azure/StorageAccountCoordinates.java
@@ -2,5 +2,4 @@ package bio.terra.workspace.service.resource.controlled.cloud.azure;
 
 import java.util.UUID;
 
-public record StorageAccountCoordinates(UUID workspaceUuid, UUID storageContainerUuid) {
-}
+public record StorageAccountCoordinates(UUID workspaceUuid, UUID storageContainerUuid) {}
