1d56f58ab22959c85da17a4299134997ae938323
bumptagbot <robot@terra.team>
2023-09-29 16:07:44 -0400

    No retries on more errors creating GCE instance

--- service/src/main/java/bio/terra/workspace/service/resource/GcpFlightExceptionUtils.java
+++ service/src/main/java/bio/terra/workspace/service/resource/GcpFlightExceptionUtils.java
@@ -1,6 +1,8 @@
 package bio.terra.workspace.service.resource;
 
 import bio.terra.common.exception.BadRequestException;
+import bio.terra.common.exception.ForbiddenException;
+import bio.terra.common.exception.UnauthorizedException;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import org.springframework.http.HttpStatus;
 
@@ -17,6 +19,20 @@ public class GcpFlightExceptionUtils {
         || HttpStatus.NOT_FOUND.value() == e.getStatusCode()) {
       // Don't retry bad requests, which won't change. Instead, fail faster.
       throw new BadRequestException(e.getDetails().getMessage());
+    }
+  }
+
+  /**
+   * Map GCP status codes to WSM exceptions. These are errors that we DO NOT want to retry in the
+   * flight.
+   */
+  public static void handleGcpNonRetryableException(GoogleJsonResponseException e) {
+    int statusCode = e.getStatusCode();
+    switch (HttpStatus.valueOf(statusCode)) {
+      case BAD_REQUEST, NOT_FOUND, TOO_MANY_REQUESTS -> throw new BadRequestException(
+          e.getDetails().getMessage(), e);
+      case UNAUTHORIZED -> throw new UnauthorizedException(e.getDetails().getMessage(), e);
+      case FORBIDDEN -> throw new ForbiddenException(e.getDetails().getMessage(), e);
     }
   }
 }
