00b19175e72cd12b65a041a5f178819963fbe7f4
Josh Ladieu <ladieu@broadinstitute.org>
2023-10-24 15:25:44 -0400

    Mock one layer lower.
    This will allow the `@State` to better track stateful behavior, and
    hopefully prevent future collision/disagreement between stateful setups
    that might happen when mocking at the service level.

--- service/src/test/java/bio/terra/workspace/pact/WdsContractVerificationTest.java
+++ service/src/test/java/bio/terra/workspace/pact/WdsContractVerificationTest.java
@@ -11,20 +11,22 @@ import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
-import bio.terra.landingzone.db.LandingZoneDao;
-import bio.terra.landingzone.job.LandingZoneJobService;
 import bio.terra.policy.model.TpsPaoConflict;
-import bio.terra.policy.model.TpsPaoDescription;
 import bio.terra.policy.model.TpsPaoUpdateResult;
 import bio.terra.policy.model.TpsUpdateMode;
-import bio.terra.workspace.common.BaseAzureUnitTest;
+import bio.terra.workspace.common.BaseUnitTestMocks;
+import bio.terra.workspace.db.WorkspaceDao;
+import bio.terra.workspace.service.danglingresource.DanglingResourceCleanupService;
 import bio.terra.workspace.service.iam.AuthenticatedUserRequest;
-import bio.terra.workspace.service.job.JobService;
+import bio.terra.workspace.service.iam.AuthenticatedUserRequestFactory;
+import bio.terra.workspace.service.resource.model.WsmResourceState;
+import bio.terra.workspace.service.resource.referenced.ReferencedResourceService;
 import bio.terra.workspace.service.resource.referenced.model.ReferencedResource;
 import bio.terra.workspace.service.workspace.model.Workspace;
 import bio.terra.workspace.service.workspace.model.WorkspaceStage;
 import java.util.Map;
 import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
 import org.broadinstitute.dsde.workbench.client.sam.model.UserStatusInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -32,27 +34,25 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.TransactionManager;
 
+// set ActiveProfile to "unit-test" to disable some unnecessary dependencies (like
+// GoogleCredentials) that would otherwise require mocking
+@ActiveProfiles("unit-test")
 @Tag("pact-verification")
 @Provider("workspacemanager") // should match the terra chart name
 @PactBroker()
-public class WdsContractVerificationTest extends BaseAzureUnitTest {
+public class WdsContractVerificationTest extends BaseUnitTestMocks {
   @Autowired private MockMvc mockMvc;
 
-  // required for dependency chain via azureConnectedTestUtils
-  @MockBean private LandingZoneDao mockLandingZoneDao;
+  // Mocked out to prevent an error with missing service credentials
+  @MockBean private DanglingResourceCleanupService unusedDanglingResourceCleanupService;
 
-  // datasource not configured in this test runtime, so mock out the transaction manager
-  @MockBean(name = "tlzTransactionManager")
-  private TransactionManager mockTlzTransactionManager;
-
-  // for JobService.initialize call on startup
-  @MockBean private JobService mockJobService;
-
-  // for LandingZone.main call on startup
-  @MockBean private LandingZoneJobService mockLandingZoneJobService;
+  // needed to mock behavior in this test
+  @MockBean private WorkspaceDao mockWorkspaceDao;
+  @MockBean private AuthenticatedUserRequestFactory mockAuthenticatedUserRequestFactory;
+  @MockBean private ReferencedResourceService mockReferencedResourceService;
 
   @TestTemplate
   @ExtendWith(PactVerificationSpringProvider.class)
@@ -68,29 +68,37 @@ public class WdsContractVerificationTest extends BaseAzureUnitTest {
   @State({"authenticated with the given email"})
   public void authenticatedAsAccount(Map<?, ?> parameters) throws InterruptedException {
     var authenticatedEmail = parameters.get("email").toString();
+    var stubbedAuthenticatedRequest = new AuthenticatedUserRequest().email(authenticatedEmail);
+    when(mockAuthenticatedUserRequestFactory.from(any(HttpServletRequest.class)))
+        .thenReturn(stubbedAuthenticatedRequest);
+    when(mockSamService()
+            .isAuthorized(
+                eq(stubbedAuthenticatedRequest),
+                /* iamResourceType= */ anyString(),
+                /* resourceId= */ anyString(),
+                /* action= */ anyString()))
+        .thenReturn(true);
 
     // for recording the creator of the snapshot reference...
-    when(mockSamService()
-            .getUserEmailFromSamAndRethrowOnInterrupt(any(AuthenticatedUserRequest.class)))
+    when(mockSamService().getUserEmailFromSamAndRethrowOnInterrupt(eq(stubbedAuthenticatedRequest)))
         .thenReturn(authenticatedEmail);
 
     // for activity logging...
-    when(mockSamService().getUserStatusInfo(any(AuthenticatedUserRequest.class)))
+    when(mockSamService().getUserStatusInfo(eq(stubbedAuthenticatedRequest)))
         .thenReturn(new UserStatusInfo().userEmail(authenticatedEmail).userSubjectId("subjectid"));
   }
 
   @State({"a workspace with the given id exists"})
   public void workspaceIdExists(Map<?, ?> parameters) {
     var workspaceUuid = UUID.fromString(parameters.get("id").toString());
-    when(mockWorkspaceService()
-            .validateWorkspaceAndAction(
-                any(AuthenticatedUserRequest.class), eq(workspaceUuid), /* action= */ anyString()))
+    when(mockWorkspaceDao.getWorkspace(eq(workspaceUuid)))
         .thenReturn(
             Workspace.builder()
                 .workspaceId(workspaceUuid)
                 .workspaceStage(WorkspaceStage.RAWLS_WORKSPACE)
                 .userFacingId("workspaceid")
                 .createdByEmail("workspace.creator@e.mail")
+                .state(WsmResourceState.READY)
                 .build());
   }
 
@@ -98,17 +106,15 @@ public class WdsContractVerificationTest extends BaseAzureUnitTest {
   public void allowSnapshotReferenceCreation() throws InterruptedException {
     when(mockFeatureConfiguration().isTpsEnabled()).thenReturn(true);
     var policyUpdateSuccessful = new TpsPaoUpdateResult().updateApplied(true);
-    when(mockWorkspaceService()
-            .linkPolicies(
+    when(mockTpsApiDispatch()
+            .linkPao(
                 /* workspaceUuid= */ any(UUID.class),
-                any(TpsPaoDescription.class),
-                any(TpsUpdateMode.class),
-                any(AuthenticatedUserRequest.class)))
+                /* sourceObjectId= */ any(UUID.class),
+                any(TpsUpdateMode.class)))
         .thenReturn(policyUpdateSuccessful);
 
-    when(mockReferencedResourceService()
-            .createReferenceResource(
-                any(ReferencedResource.class), any(AuthenticatedUserRequest.class)))
+    when(mockReferencedResourceService.createReferenceResource(
+            any(ReferencedResource.class), any(AuthenticatedUserRequest.class)))
         // just echo back the resource that the controller attempted to create to simulate success
         .thenAnswer(invocation -> invocation.getArgument(0));
   }
@@ -120,12 +126,18 @@ public class WdsContractVerificationTest extends BaseAzureUnitTest {
         new TpsPaoUpdateResult()
             .updateApplied(false)
             .addConflictsItem(new TpsPaoConflict().name("some conflict"));
-    when(mockWorkspaceService()
-            .linkPolicies(
+    when(mockTpsApiDispatch()
+            .linkPao(
                 /* workspaceUuid= */ any(UUID.class),
-                any(TpsPaoDescription.class),
-                any(TpsUpdateMode.class),
-                any(AuthenticatedUserRequest.class)))
+                /* sourceObjectId= */ any(UUID.class),
+                any(TpsUpdateMode.class)))
         .thenReturn(policiesConflicting);
+
+    when(mockReferencedResourceService.createReferenceResource(
+            any(ReferencedResource.class), any(AuthenticatedUserRequest.class)))
+        // just throw if an unexpected attempt is made to create a referenced resource
+        .thenThrow(
+            new RuntimeException(
+                "mockTpsApiDispatch() should have prevented #createReferenceResource from being reached."));
   }
 }
