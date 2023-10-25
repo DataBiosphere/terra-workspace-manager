b9777229d5e0f43baa42e717e8965ee4242c41b0
Michael Spector <michael.l.spector@gmail.com>
2023-10-25 12:49:01 -0400

    code format

--- azureDatabaseUtils/src/main/java/bio/terra/workspace/azureDatabaseUtils/storage/AzureBlobStorage.java
+++ azureDatabaseUtils/src/main/java/bio/terra/workspace/azureDatabaseUtils/storage/AzureBlobStorage.java
@@ -18,90 +18,93 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AzureBlobStorage implements BlobStorage {
-    public AzureBlobStorage() {}
+  public AzureBlobStorage() {}
 
-    private static final Logger logger = LoggerFactory.getLogger(AzureBlobStorage.class);
+  private static final Logger logger = LoggerFactory.getLogger(AzureBlobStorage.class);
 
-    /*
-    6/28/2023 - Terra's storage inside a billing project is organized as follows:
-    by default a billing project gets a single blob storage azure resource
-    each workspace that gets created inside of that billing project will get its own container inside the blob storage
-    the container will follow the naming of "sc-<workspaceId>"
-    all files that are uploaded into the workspace will be inside the appropriate workspace container
-    containers are restricted to have the same access as the users who have access to the workspace
+  /*
+  6/28/2023 - Terra's storage inside a billing project is organized as follows:
+  by default a billing project gets a single blob storage azure resource
+  each workspace that gets created inside of that billing project will get its own container inside the blob storage
+  the container will follow the naming of "sc-<workspaceId>"
+  all files that are uploaded into the workspace will be inside the appropriate workspace container
+  containers are restricted to have the same access as the users who have access to the workspace
 
-    backups will be stored inside the workspace container in a path determined by function GenerateBackupFilename
-    which exists within BackupService.java.
-     */
-    @Override
-    public void streamOutputToBlobStorage(
-            InputStream fromStream, String blobName, String workspaceId, String blobstorageDetails) {
-        logger.info("Creating blob storage client. ");
-        BlobContainerClient blobContainerClient = constructBlockBlobClient(workspaceId, blobstorageDetails);
-        logger.info("About to write to blob storage. ");
-        // https://learn.microsoft.com/en-us/java/api/overview/azure/storage-blob-readme?view=azure-java-stable#upload-a-blob-via-an-outputstream
-        try (BufferedOutputStream blobOS =
-                     new BufferedOutputStream(
-                             blobContainerClient
-                                     .getBlobClient(blobName)
-                                     .getBlockBlobClient()
-                                     .getBlobOutputStream())) {
-            try (BufferedReader bufferedReader =
-                         new BufferedReader(new InputStreamReader(fromStream, StandardCharsets.UTF_8))) {
-                int line;
-                while ((line = bufferedReader.read()) != -1) {
-                    blobOS.write(line);
-                }
-            }
-        } catch (IOException ioEx) {
-            throw new LaunchProcessException("Error streaming output of child process", ioEx);
+  backups will be stored inside the workspace container in a path determined by function GenerateBackupFilename
+  which exists within BackupService.java.
+   */
+  @Override
+  public void streamOutputToBlobStorage(
+      InputStream fromStream, String blobName, String workspaceId, String blobstorageDetails) {
+    logger.info("Creating blob storage client. ");
+    BlobContainerClient blobContainerClient =
+        constructBlockBlobClient(workspaceId, blobstorageDetails);
+    logger.info("About to write to blob storage. ");
+    // https://learn.microsoft.com/en-us/java/api/overview/azure/storage-blob-readme?view=azure-java-stable#upload-a-blob-via-an-outputstream
+    try (BufferedOutputStream blobOS =
+        new BufferedOutputStream(
+            blobContainerClient
+                .getBlobClient(blobName)
+                .getBlockBlobClient()
+                .getBlobOutputStream())) {
+      try (BufferedReader bufferedReader =
+          new BufferedReader(new InputStreamReader(fromStream, StandardCharsets.UTF_8))) {
+        int line;
+        while ((line = bufferedReader.read()) != -1) {
+          blobOS.write(line);
         }
+      }
+    } catch (IOException ioEx) {
+      throw new LaunchProcessException("Error streaming output of child process", ioEx);
     }
+  }
 
-    @Override
-    public void streamInputFromBlobStorage(
-            OutputStream toStream, String blobName, String workspaceId, String blobstorageDetails) {
-        BlobContainerClient blobContainerClient = constructBlockBlobClient(workspaceId, blobstorageDetails);
-        try (toStream) {
-            blobContainerClient.getBlobClient(blobName).downloadStream(toStream);
-        } catch (IOException ioEx) {
-            throw new LaunchProcessException("Error streaming input to child process", ioEx);
-        }
+  @Override
+  public void streamInputFromBlobStorage(
+      OutputStream toStream, String blobName, String workspaceId, String blobstorageDetails) {
+    BlobContainerClient blobContainerClient =
+        constructBlockBlobClient(workspaceId, blobstorageDetails);
+    try (toStream) {
+      blobContainerClient.getBlobClient(blobName).downloadStream(toStream);
+    } catch (IOException ioEx) {
+      throw new LaunchProcessException("Error streaming input to child process", ioEx);
     }
+  }
 
-    private BlobContainerClient constructBlockBlobClient(String workspaceId, String blobstorageDetails) {
-        // get workspace blob storage endpoint and token
-        // (the line below was copied directly from WDS)
-        // var blobstorageDetails = workspaceManagerDao.getBlobStorageUrl(workspaceId, authToken);
+  private BlobContainerClient constructBlockBlobClient(
+      String workspaceId, String blobstorageDetails) {
+    // get workspace blob storage endpoint and token
+    // (the line below was copied directly from WDS)
+    // var blobstorageDetails = workspaceManagerDao.getBlobStorageUrl(workspaceId, authToken);
 
-        // the url we get from WSM already contains the token in it, so no need to specify sasToken
-        // separately
+    // the url we get from WSM already contains the token in it, so no need to specify sasToken
+    // separately
 
-        logger.info("Constructing block blob client with blobstorageDetails: {}", blobstorageDetails);
-        BlobServiceClient blobServiceClient =
-                new BlobServiceClientBuilder().endpoint(blobstorageDetails).buildClient();
-        logger.info("Constructed BlobServiceClient", blobstorageDetails);
-        try {
-            // the way storage containers are set up in a workspace are as follows:
-            // billing project gets a single azure storage account
-            // each workspace gets a container inside of that storage account to keep its data
-            return blobServiceClient.getBlobContainerClient("sc-" + workspaceId);
-        } catch (BlobStorageException e) {
-            // if the default workspace container doesn't exist, something went horribly wrong
-            logger.error("Default storage container missing for workspace id {}", workspaceId);
-            throw (e);
-        }
+    logger.info("Constructing block blob client with blobstorageDetails: {}", blobstorageDetails);
+    BlobServiceClient blobServiceClient =
+        new BlobServiceClientBuilder().endpoint(blobstorageDetails).buildClient();
+    logger.info("Constructed BlobServiceClient", blobstorageDetails);
+    try {
+      // the way storage containers are set up in a workspace are as follows:
+      // billing project gets a single azure storage account
+      // each workspace gets a container inside of that storage account to keep its data
+      return blobServiceClient.getBlobContainerClient("sc-" + workspaceId);
+    } catch (BlobStorageException e) {
+      // if the default workspace container doesn't exist, something went horribly wrong
+      logger.error("Default storage container missing for workspace id {}", workspaceId);
+      throw (e);
     }
+  }
 
-    @Override
-    public void deleteBlob(String blobFile, String workspaceId, String authToken) {
-        BlobContainerClient blobContainerClient = constructBlockBlobClient(workspaceId, authToken);
-        try {
-            var blobClient = blobContainerClient.getBlobClient(blobFile);
-            blobClient.delete();
-        } catch (BlobStorageException e) {
-            logger.error("Failed to delete file with name {}. ", blobFile);
-            throw (e);
-        }
+  @Override
+  public void deleteBlob(String blobFile, String workspaceId, String authToken) {
+    BlobContainerClient blobContainerClient = constructBlockBlobClient(workspaceId, authToken);
+    try {
+      var blobClient = blobContainerClient.getBlobClient(blobFile);
+      blobClient.delete();
+    } catch (BlobStorageException e) {
+      logger.error("Failed to delete file with name {}. ", blobFile);
+      throw (e);
     }
+  }
 }
