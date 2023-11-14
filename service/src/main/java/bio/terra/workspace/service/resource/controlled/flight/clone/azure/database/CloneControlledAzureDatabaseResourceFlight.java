49f0c9e6ec5f075ab4dece779c4a35536f5ed31f
Saloni Shah <salonipshah11@gmail.com>
2023-11-14 17:06:50 -0500

    address code smells

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/flight/clone/azure/database/CloneControlledAzureDatabaseResourceFlight.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/flight/clone/azure/database/CloneControlledAzureDatabaseResourceFlight.java
@@ -8,8 +8,5 @@ public class CloneControlledAzureDatabaseResourceFlight extends Flight {
   public CloneControlledAzureDatabaseResourceFlight(
       FlightMap inputParameters, Object applicationContext) {
     super(inputParameters, applicationContext);
-
-    // TODO: This is only for testing purposes. Remove before merge
-    System.out.println("**** FIND ME - inside CloneControlledAzureDatabaseResourceFlight ****");
   }
 }
