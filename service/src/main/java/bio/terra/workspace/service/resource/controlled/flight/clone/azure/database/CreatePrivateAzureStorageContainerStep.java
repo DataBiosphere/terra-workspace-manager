329146ed1ae8f421290c538e65868c68f04105f3
Katrina Pierre <kpierre@broadinstitute.org>
2023-11-17 14:01:36 -0500

    undo step

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/flight/clone/azure/database/CreatePrivateAzureStorageContainerStep.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/flight/clone/azure/database/CreatePrivateAzureStorageContainerStep.java
@@ -10,6 +10,7 @@ import bio.terra.stairway.StepStatus;
 import bio.terra.stairway.exception.RetryException;
 import bio.terra.workspace.amalgam.landingzone.azure.LandingZoneApiDispatch;
 import bio.terra.workspace.app.configuration.external.AzureConfiguration;
+import bio.terra.workspace.common.exception.AzureManagementExceptionUtils;
 import bio.terra.workspace.generated.model.ApiAzureLandingZoneDeployedResource;
 import bio.terra.workspace.service.crl.CrlService;
 import bio.terra.workspace.service.iam.SamService;
@@ -110,6 +111,88 @@ public class CreatePrivateAzureStorageContainerStep implements Step {
 
     @Override
     public StepResult undoStep(FlightContext context) throws InterruptedException {
-        return null;
+        final AzureCloudContext azureCloudContext =
+                context
+                        .getWorkingMap()
+                        .get(WorkspaceFlightMapKeys.ControlledResourceKeys.AZURE_CLOUD_CONTEXT, AzureCloudContext.class);
+        final StorageManager storageManager =
+                crlService.getStorageManager(azureCloudContext, azureConfig);
+        final String storageAccountName =
+                context.getWorkingMap().get(WorkspaceFlightMapKeys.ControlledResourceKeys.STORAGE_ACCOUNT_NAME, String.class);
+        if (storageAccountName == null) {
+            logger.warn(
+                    "Deletion of the storage container is not required. "
+                            + "Parent storage account was not found in the create.");
+            return StepResult.getStepResultSuccess();
+        }
+
+        String containerName = context.getWorkingMap().get(WorkspaceFlightMapKeys.ControlledResourceKeys.AZURE_DB_DUMP_STORAGE_CONTAINER_NAME, String.class);
+
+        try {
+            storageManager
+                    .storageAccounts()
+                    .getByResourceGroup(azureCloudContext.getAzureResourceGroupId(), storageAccountName);
+        } catch (ManagementException ex) {
+            if (AzureManagementExceptionUtils.isExceptionCode(
+                    ex, AzureManagementExceptionUtils.RESOURCE_NOT_FOUND)) {
+                logger.warn(
+                        "Deletion of the storage container is not required. Parent storage account does not exist. {}",
+                        storageAccountName);
+                return StepResult.getStepResultSuccess();
+            }
+            logger.error(
+                    "Attempt to retrieve parent Azure storage account before deleting container failed on this try: '{}'. Error Code: {}.",
+                    containerName,
+                    ex.getValue().getCode(),
+                    ex);
+            return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY, ex);
+        }
+        try {
+            storageManager
+                    .blobContainers()
+                    .get(
+                            azureCloudContext.getAzureResourceGroupId(),
+                            storageAccountName,
+                            containerName);
+        } catch (ManagementException ex) {
+            if (AzureManagementExceptionUtils.isExceptionCode(
+                    ex, AzureManagementExceptionUtils.CONTAINER_NOT_FOUND)) {
+                logger.warn(
+                        "Deletion of the storage container is not required. Storage container does not exist. {}",
+                        containerName);
+                return StepResult.getStepResultSuccess();
+            }
+            logger.error(
+                    "Attempt to retrieve Azure storage container before deleting it failed on this try: '{}'. Error Code: {}.",
+                    containerName,
+                    ex.getValue().getCode(),
+                    ex);
+            return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY, ex);
+        }
+
+        try {
+            logger.warn(
+                    "Attempting to delete storage container '{}' in account '{}'",
+                    containerName,
+                    storageAccountName);
+            storageManager
+                    .blobContainers()
+                    .delete(
+                            azureCloudContext.getAzureResourceGroupId(),
+                            storageAccountName,
+                            containerName);
+            logger.warn(
+                    "Successfully deleted storage container '{}' in account '{}'",
+                    containerName,
+                    storageAccountName);
+            return StepResult.getStepResultSuccess();
+        } catch (ManagementException ex) {
+            logger.error(
+                    "Attempt to delete Azure storage container failed: '{}'. Error Code: {}.",
+                    containerName,
+                    ex.getValue().getCode(),
+                    ex);
+            return new StepResult(StepStatus.STEP_RESULT_FAILURE_FATAL, ex);
+        }
     }
 }
