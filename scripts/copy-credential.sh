0fcbec72b7364403eb04699c1b6f8860d95b8469
aherbst-broad <aherbst@broadinstitute.org>
2024-01-18 12:44:57 -0500

    Testing

---
+++ scripts/copy-credential.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
+  echo "GOOGLE_APPLICATION_CREDENTIALS is not set"
+  exit 1
+fi
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null  && pwd )"
+outputdir="${script_dir}/config"
+
+echo "Copying creds from ${GOOGLE_APPLICATION_CREDENTIALS} to ${outputdir}/user-delegated-sa.json"
+cp "${GOOGLE_APPLICATION_CREDENTIALS}" ${outputdir}/user-delegated-sa.json
+echo "Done"
