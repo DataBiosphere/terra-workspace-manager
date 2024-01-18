cf6ddf7a4b3e52ddb9fce24f93e9a2ab559caa99
aherbst-broad <aherbst@broadinstitute.org>
2024-01-18 13:01:53 -0500

    Testing

--- scripts/copy-credential.sh
+++ scripts/copy-credential.sh
@@ -9,5 +9,10 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null  && pwd )"
 outputdir="${script_dir}/config"
 
 echo "Copying creds from ${GOOGLE_APPLICATION_CREDENTIALS} to ${outputdir}/user-delegated-sa.json"
-cp "${GOOGLE_APPLICATION_CREDENTIALS}" ${outputdir}/user-delegated-sa.json
+cp "${GOOGLE_APPLICATION_CREDENTIALS}" "${outputdir}/user-delegated-sa.json"
+
+result=$?
+if [ $result -ne 0 ]; then
+  exit $result
+fi
 echo "Done"
