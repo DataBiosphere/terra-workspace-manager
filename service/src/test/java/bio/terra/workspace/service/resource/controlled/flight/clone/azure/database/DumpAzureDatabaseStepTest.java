dd95d8d073c0f51714e13c6035827824a9d81737
Saloni Shah <salonipshah11@gmail.com>
2024-01-29 15:48:37 -0500

    [WM-2459] DB dump & restore pod should be named based on destination resource during cloning (#1622)

--- service/src/test/java/bio/terra/workspace/service/resource/controlled/flight/clone/azure/database/DumpAzureDatabaseStepTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/flight/clone/azure/database/DumpAzureDatabaseStepTest.java
@@ -24,6 +24,7 @@ import bio.terra.workspace.service.resource.controlled.cloud.azure.AzureStorageA
 import bio.terra.workspace.service.resource.controlled.cloud.azure.database.AzureDatabaseUtilsRunner;
 import bio.terra.workspace.service.resource.controlled.cloud.azure.database.ControlledAzureDatabaseResource;
 import bio.terra.workspace.service.resource.controlled.cloud.azure.storageContainer.ControlledAzureStorageContainerResource;
+import bio.terra.workspace.service.resource.controlled.model.ControlledResourceFields;
 import bio.terra.workspace.service.workspace.AzureCloudContextService;
 import bio.terra.workspace.service.workspace.WorkspaceService;
 import bio.terra.workspace.service.workspace.flight.WorkspaceFlightMapKeys;
@@ -31,6 +32,7 @@ import bio.terra.workspace.service.workspace.flight.WorkspaceFlightMapKeys.Contr
 import bio.terra.workspace.service.workspace.model.AzureCloudContext;
 import java.util.Optional;
 import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -70,6 +72,8 @@ class DumpAzureDatabaseStepTest extends BaseMockitoStrictStubbingTest {
               creationParameters, workspaceId, null)
           .build();
   private final String mockEncryptionKey = "mock-encryption-key-123";
+  private final ControlledResourceFields mockDestinationResource =
+      ControlledResourceFixtures.makeDefaultControlledResourceFields(mockDestinationWorkspaceId);
 
   @Test
   void testSuccess() throws InterruptedException {
@@ -91,7 +95,13 @@ class DumpAzureDatabaseStepTest extends BaseMockitoStrictStubbingTest {
         .pgDumpDatabase(
             eq(mockAzureCloudContext),
             eq(databaseResource.getWorkspaceId()),
-            eq("dump-db-%s".formatted(databaseResource.getResourceId())),
+            eq(
+                StringUtils.truncate(
+                    "dump-db-%s-%s"
+                        .formatted(
+                            databaseResource.getDatabaseName(),
+                            mockDestinationResource.getResourceId()),
+                    63)),
             eq(databaseResource.getDatabaseName()),
             eq(databaseServerName),
             eq(databaseUserName),
@@ -128,9 +138,7 @@ class DumpAzureDatabaseStepTest extends BaseMockitoStrictStubbingTest {
             ControlledAzureStorageContainerResource.class))
         .thenReturn(
             ControlledAzureStorageContainerResource.builder()
-                .common(
-                    ControlledResourceFixtures.makeDefaultControlledResourceFields(
-                        mockDestinationWorkspaceId))
+                .common(mockDestinationResource)
                 .storageContainerName("sc-%s".formatted(mockDestinationWorkspaceId.toString()))
                 .build());
     when(mockWorkingMap.get(
