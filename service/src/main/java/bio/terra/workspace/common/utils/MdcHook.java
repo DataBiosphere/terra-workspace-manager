7d1bb2cda171d343e04495d0accff28dc9737931
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2024-02-05 11:41:32 -0500

    [WOR-1454] Populate MDC for Stairway flight and its steps (#1626)
    * Fix latent Stairway MDC bug when starting step
    
    If we want MDC changes to show up in a log, we must make those changes before emitting the log.
    
    * Propagate MDC to Stairway thread on flight start, clear on flight end
    
    This preserves context during exceptional conditions in flight processing, such as dismal failures
    
    * Always propagate flight context across a flight, and step context across a step
    
    This supplements the context propagated across a flight's Stairway thread(s) from the calling thread.
    Expanded unit test coverage.
    
    * Spotless
    
    * Reuse step MDC specification when setting and unsetting
    
    Driveby: correct a mistaken JUnit 4 import in MdcHookTest
    
    * Spotless

--- service/src/main/java/bio/terra/workspace/common/utils/MdcHook.java
+++ service/src/main/java/bio/terra/workspace/common/utils/MdcHook.java
@@ -18,11 +18,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * A {@link StairwayHook} for propagating MDC context across steps using the input {@link
- * FlightMap}.
+ * A {@link StairwayHook} which does the following:
  *
- * <p>This allows steps to have the same MDC context as when their flight was created. Note that any
- * modifications to the MDC context within a step are not propagated to other steps.
+ * <ol>
+ *   <li>Propagates flight-specific mapped diagnostic context (MDC) across a Stairway flight's
+ *       thread(s) for the duration of the flight. This includes context provided by the caller in
+ *       the input {@link FlightMap}, intended to store the MDC from the calling thread to preserve
+ *       context across the duration of a request.
+ *   <li>Propagates step-specific MDC across a Stairway flight's thread(s) for the duration of each
+ *       step.
+ *   <li>Supplements logging at notable flight state transitions.
+ * </ol>
+ *
+ * <p><b>Note for developers:</b> Any modifications to the MDC directly within flight or step code
+ * may not have their intended effect and are not recommended (e.g. a flight may restart on a
+ * different thread due to failover-recovery).
  */
 @Component
 public class MdcHook implements StairwayHook {
@@ -31,8 +41,23 @@ public class MdcHook implements StairwayHook {
   private static final String STEP_LOG_FORMAT =
       "Operation: {}, flightClass: {}, flightId: {}, stepClass: {}, stepIndex: {}, direction: {}";
 
-  /** The key to use in {@link FlightMap} for storing the MDC context. */
+  /** The key to use in the input {@link FlightMap} for storing the MDC context. */
   public static final String MDC_FLIGHT_MAP_KEY = "mdcKey";
+
+  /** ID of the flight */
+  public static final String FLIGHT_ID_KEY = "flightId";
+
+  /** Class of the flight */
+  public static final String FLIGHT_CLASS_KEY = "flightClass";
+
+  /** Class of the flight step */
+  public static final String FLIGHT_STEP_CLASS_KEY = "flightStepClass";
+
+  /** Direction of the step (START, DO, SWITCH, or UNDO) */
+  public static final String FLIGHT_STEP_DIRECTION_KEY = "flightStepDirection";
+
+  /** The step's execution order */
+  public static final String FLIGHT_STEP_NUMBER_KEY = "flightStepNumber";
 
   private static final TypeReference<Map<String, String>> mapType = new TypeReference<>() {};
 
@@ -55,6 +80,7 @@ public class MdcHook implements StairwayHook {
 
   @Override
   public HookAction startStep(FlightContext flightContext) {
+    addStepContextToMdc(flightContext);
     logger.info(
         STEP_LOG_FORMAT,
         "startStep",
@@ -63,9 +89,6 @@ public class MdcHook implements StairwayHook {
         flightContext.getStepClassName(),
         flightContext.getStepIndex(),
         flightContext.getDirection().name());
-    String serializedMdc = flightContext.getInputParameters().get(MDC_FLIGHT_MAP_KEY, String.class);
-    // Note that this destroys any previous context on this thread.
-    MDC.setContextMap(deserializeMdc(serializedMdc));
     return HookAction.CONTINUE;
   }
 
@@ -79,12 +102,16 @@ public class MdcHook implements StairwayHook {
         flightContext.getStepClassName(),
         flightContext.getStepIndex(),
         flightContext.getDirection().name());
-    MDC.clear();
+    removeStepContextFromMdc(flightContext);
     return HookAction.CONTINUE;
   }
 
   @Override
   public HookAction startFlight(FlightContext flightContext) {
+    String serializedMdc = flightContext.getInputParameters().get(MDC_FLIGHT_MAP_KEY, String.class);
+    // Note that this destroys any previous context on this thread.
+    MDC.setContextMap(deserializeMdc(serializedMdc));
+    addFlightContextToMdc(flightContext);
     logger.info(
         FLIGHT_LOG_FORMAT,
         "startFlight",
@@ -100,6 +127,7 @@ public class MdcHook implements StairwayHook {
         "endFlight",
         flightContext.getFlightClassName(),
         flightContext.getFlightId());
+    MDC.clear();
     return HookAction.CONTINUE;
   }
 
@@ -130,9 +158,29 @@ public class MdcHook implements StairwayHook {
   }
 
   @Override
-  public HookAction stateTransition(FlightContext context) throws InterruptedException {
+  public HookAction stateTransition(FlightContext context) {
     logger.info(
         "Flight ID {} changed status to {}.", context.getFlightId(), context.getFlightStatus());
     return HookAction.CONTINUE;
+  }
+
+  private void addFlightContextToMdc(FlightContext context) {
+    MDC.put(FLIGHT_ID_KEY, context.getFlightId());
+    MDC.put(FLIGHT_CLASS_KEY, context.getFlightClassName());
+  }
+
+  private Map<String, String> stepContext(FlightContext context) {
+    return Map.of(
+        FLIGHT_STEP_CLASS_KEY, context.getStepClassName(),
+        FLIGHT_STEP_DIRECTION_KEY, context.getDirection().toString(),
+        FLIGHT_STEP_NUMBER_KEY, Integer.toString(context.getStepIndex()));
+  }
+
+  private void addStepContextToMdc(FlightContext context) {
+    stepContext(context).forEach(MDC::put);
+  }
+
+  private void removeStepContextFromMdc(FlightContext context) {
+    stepContext(context).keySet().forEach(MDC::remove);
   }
 }
