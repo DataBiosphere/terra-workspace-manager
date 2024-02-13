72621903afab9e2c2905c1045cb6951e98246e8e
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2024-02-12 23:05:23 -0500

    Refactor Stairway initialization out of JobService and into new StairwayInitializerService
    This code was previously hard to unit test within the Spring Boot JobServiceTest.
    We can now unit test that Stairway is initialized with a StairwayOptionsBuilder that is set up as we expect, including with all of our Stairway hooks (MdcHook and more).
    We can also unit test StartupInitializer.initialize to make sure that it's performing all of the initialization we expect.

---
+++ service/src/test/java/bio/terra/workspace/app/StartupInitializerTest.java
@@ -0,0 +1,100 @@
+package bio.terra.workspace.app;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import bio.terra.common.db.DataSourceManager;
+import bio.terra.common.migrate.LiquibaseMigrator;
+import bio.terra.landingzone.job.LandingZoneJobService;
+import bio.terra.landingzone.library.configuration.LandingZoneDatabaseConfiguration;
+import bio.terra.workspace.app.configuration.external.FeatureConfiguration;
+import bio.terra.workspace.app.configuration.external.WorkspaceDatabaseConfiguration;
+import bio.terra.workspace.common.annotations.Unit;
+import bio.terra.workspace.common.utils.BaseMockitoStrictStubbingTest;
+import bio.terra.workspace.service.job.StairwayInitializerService;
+import bio.terra.workspace.service.workspace.WsmApplicationService;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationContext;
+
+@Unit
+@ExtendWith(MockitoExtension.class)
+public class StartupInitializerTest extends BaseMockitoStrictStubbingTest {
+
+  @Mock private ApplicationContext context;
+  @Mock private DataSourceManager dataSourceManager;
+  @Mock private WorkspaceDatabaseConfiguration workspaceDatabaseConfiguration;
+  @Mock private LiquibaseMigrator liquibaseMigrator;
+  @Mock private StairwayInitializerService stairwayInitializerService;
+  @Mock private WsmApplicationService wsmApplicationService;
+  @Mock private FeatureConfiguration featureConfiguration;
+  @Mock private LandingZoneDatabaseConfiguration landingZoneDatabaseConfiguration;
+  @Mock private LandingZoneJobService landingZoneJobService;
+  @Mock private DataSource wsmDataSource;
+  @Mock private DataSource lzsDataSource;
+
+  private enum DatabaseInitializationInstruction {
+    INITIALIZE,
+    UPGRADE,
+    CONTINUE
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = DatabaseInitializationInstruction.class)
+  void initialize(DatabaseInitializationInstruction dbInstruction) {
+    when(context.getBean(DataSourceManager.class)).thenReturn(dataSourceManager);
+    when(context.getBean(WorkspaceDatabaseConfiguration.class))
+        .thenReturn(workspaceDatabaseConfiguration);
+    when(context.getBean(LiquibaseMigrator.class)).thenReturn(liquibaseMigrator);
+    when(context.getBean(StairwayInitializerService.class)).thenReturn(stairwayInitializerService);
+    when(context.getBean(WsmApplicationService.class)).thenReturn(wsmApplicationService);
+    when(context.getBean(FeatureConfiguration.class)).thenReturn(featureConfiguration);
+    when(context.getBean(LandingZoneDatabaseConfiguration.class))
+        .thenReturn(landingZoneDatabaseConfiguration);
+    when(context.getBean("landingZoneJobService", LandingZoneJobService.class))
+        .thenReturn(landingZoneJobService);
+
+    when(dataSourceManager.initializeDataSource(workspaceDatabaseConfiguration))
+        .thenReturn(wsmDataSource);
+    switch (dbInstruction) {
+      case INITIALIZE -> {
+        when(workspaceDatabaseConfiguration.isInitializeOnStart()).thenReturn(true);
+        when(landingZoneDatabaseConfiguration.isInitializeOnStart()).thenReturn(true);
+        when(landingZoneDatabaseConfiguration.getDataSource()).thenReturn(lzsDataSource);
+      }
+      case UPGRADE -> {
+        when(workspaceDatabaseConfiguration.isUpgradeOnStart()).thenReturn(true);
+        when(landingZoneDatabaseConfiguration.isUpgradeOnStart()).thenReturn(true);
+        when(landingZoneDatabaseConfiguration.getDataSource()).thenReturn(lzsDataSource);
+      }
+      case CONTINUE -> {
+        // No additional mocking.
+      }
+    }
+
+    StartupInitializer.initialize(context);
+
+    verify(featureConfiguration).logFeatures();
+    switch (dbInstruction) {
+      case INITIALIZE -> {
+        verify(liquibaseMigrator).initialize(anyString(), eq(wsmDataSource));
+        verify(liquibaseMigrator).initialize(anyString(), eq(lzsDataSource));
+      }
+      case UPGRADE -> {
+        verify(liquibaseMigrator).upgrade(anyString(), eq(wsmDataSource));
+        verify(liquibaseMigrator).upgrade(anyString(), eq(lzsDataSource));
+      }
+      case CONTINUE -> verifyNoInteractions(liquibaseMigrator);
+    }
+    verify(stairwayInitializerService).initialize();
+    verify(wsmApplicationService).configure();
+    verify(landingZoneJobService).initialize();
+  }
+}
---
+++ service/src/test/java/bio/terra/workspace/service/job/StairwayInitializerServiceTest.java
@@ -0,0 +1,83 @@
+package bio.terra.workspace.service.job;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import bio.terra.common.db.DataSourceManager;
+import bio.terra.common.stairway.MonitoringHook;
+import bio.terra.common.stairway.StairwayComponent;
+import bio.terra.workspace.app.configuration.external.StairwayDatabaseConfiguration;
+import bio.terra.workspace.common.annotations.Unit;
+import bio.terra.workspace.common.logging.WorkspaceActivityLogHook;
+import bio.terra.workspace.common.utils.FlightBeanBag;
+import bio.terra.workspace.common.utils.MdcHook;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.opentelemetry.api.OpenTelemetry;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Unit
+@ExtendWith(MockitoExtension.class)
+public class StairwayInitializerServiceTest {
+
+  @Mock private DataSourceManager dataSourceManager;
+  @Mock private StairwayDatabaseConfiguration stairwayDatabaseConfiguration;
+  @Mock private MdcHook mdcHook;
+  @Mock private WorkspaceActivityLogHook workspaceActivityLogHook;
+  @Mock private StairwayComponent stairwayComponent;
+  @Mock private FlightBeanBag flightBeanBag;
+  private StairwayInitializerService stairwayInitializerService;
+
+  @BeforeEach
+  void beforeEach() {
+    stairwayInitializerService =
+        new StairwayInitializerService(
+            dataSourceManager,
+            stairwayDatabaseConfiguration,
+            mdcHook,
+            workspaceActivityLogHook,
+            stairwayComponent,
+            flightBeanBag,
+            mock(ObjectMapper.class),
+            OpenTelemetry.noop());
+  }
+
+  @Test
+  void initialize() {
+    var stairwayOptionsBuilder = new StairwayComponent.StairwayOptionsBuilder();
+    when(stairwayComponent.newStairwayOptionsBuilder()).thenReturn(stairwayOptionsBuilder);
+    var dataSource = mock(DataSource.class);
+    when(dataSourceManager.initializeDataSource(stairwayDatabaseConfiguration))
+        .thenReturn(dataSource);
+
+    stairwayInitializerService.initialize();
+
+    // Stairway is initialized with our Stairway options builder
+    verify(stairwayComponent).initialize(stairwayOptionsBuilder);
+    assertThat(
+        "Stairway is initialized with data source",
+        stairwayOptionsBuilder.getDataSource(),
+        is(dataSource));
+    assertThat(
+        "Stairway is initialized with flight bean bag context",
+        stairwayOptionsBuilder.getContext(),
+        is(flightBeanBag));
+    assertThat(
+        "Stairway is initialized with MDC, monitoring, and activity log hooks",
+        stairwayOptionsBuilder.getHooks(),
+        contains(is(mdcHook), instanceOf(MonitoringHook.class), is(workspaceActivityLogHook)));
+    assertThat(
+        "Stairway is initialized with exception serializer",
+        stairwayOptionsBuilder.getExceptionSerializer(),
+        instanceOf(StairwayExceptionSerializer.class));
+  }
+}
