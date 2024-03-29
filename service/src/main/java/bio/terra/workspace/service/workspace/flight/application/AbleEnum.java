7ffe6d0e850ccde84027b0d87233299e2b10f303
Douglas Voet <dvoet@users.noreply.github.com>
2024-03-29 11:17:26 -0400

    not needed

--- service/src/main/java/bio/terra/workspace/service/workspace/flight/application/AbleEnum.java
+++ service/src/main/java/bio/terra/workspace/service/workspace/flight/application/AbleEnum.java
@@ -2,9 +2,5 @@ package bio.terra.workspace.service.workspace.flight.application;
 
 public enum AbleEnum {
   ENABLE,
-  DISABLE;
-
-  public AbleEnum toggle() {
-    return this == ENABLE ? DISABLE : ENABLE;
-  }
+  DISABLE
 }
