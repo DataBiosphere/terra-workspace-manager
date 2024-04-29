4079a92f2fd27747e6f51f797b7ed8e9b76bc78b
aherbst-broad <aherbst@broadinstitute.org>
2024-04-29 14:16:33 -0400

    Fix

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/azure/vm/LzsRetry.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/azure/vm/LzsRetry.java
@@ -110,7 +110,6 @@ public class LzsRetry {
 
   private boolean isRetryable(ApiException apiException) {
     return isTimeoutException(apiException)
-        //|| apiException.getCode() == HttpStatus.SC_FORBIDDEN
         || apiException.getCode() == HttpStatus.SC_INTERNAL_SERVER_ERROR
         || apiException.getCode() == HttpStatus.SC_BAD_GATEWAY
         || apiException.getCode() == HttpStatus.SC_SERVICE_UNAVAILABLE
