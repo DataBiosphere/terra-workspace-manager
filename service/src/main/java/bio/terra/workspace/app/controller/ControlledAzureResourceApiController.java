6181ee44ba5decf506582e85f7a4b2e06bbfea21
Tom Wiseman <twiseman@broadinstitute.org>
2024-05-06 16:42:09 -0400

    email

--- service/src/main/java/bio/terra/workspace/app/controller/ControlledAzureResourceApiController.java
+++ service/src/main/java/bio/terra/workspace/app/controller/ControlledAzureResourceApiController.java
@@ -382,12 +382,14 @@ public class ControlledAzureResourceApiController extends ControlledResourceCont
 
     // validate the workspace and user access
     AuthenticatedUserRequest userRequest = getAuthenticatedInfo();
+    logger.info("Request: " + userRequest.toString());
+
     Workspace workspace = validateWorkspaceResourceCreationPermissions(userRequest, workspaceUuid, body.getCommon());
     AzureCloudContext cloudContext = workspaceService.validateWorkspaceAndContextState(workspaceUuid, CloudPlatform.AZURE).castByEnum(CloudPlatform.AZURE);
 
       String userManagedIdentity = null;
       try {
-        logger.info("Email: " + body.getCommon().getPrivateResourceUser().getUserName());
+        logger.info("Email: " + userRequest.getEmail().substring(0,5));
         logger.info("SubscriptionID: " + cloudContext.getAzureSubscriptionId());
         logger.info("Tenant ID: " + cloudContext.getAzureTenantId());
         logger.info("Resource Group ID: " + cloudContext.getAzureResourceGroupId());
