2b6c0a9e5299a488cca345009cc362f5831ad9e3
Marc Talbott <mtalbott@broadinstitute.org>
2024-05-07 10:52:38 -0400

    Revert "[WOR-1335] Cache Sam permissions checks (#1723)" (#1727)
    This reverts commit 7a5db590ba9cbfb7ae14aa0366a5023efe0a148e.

--- service/src/main/java/bio/terra/workspace/service/iam/model/SamPermissionsCacheKey.java
+++
@@ -1,4 +0,0 @@
-package bio.terra.workspace.service.iam.model;
-
-public record SamPermissionsCacheKey(
-    String samResourceType, String samResourceName, String samResourceAction, String userEmail) {}
