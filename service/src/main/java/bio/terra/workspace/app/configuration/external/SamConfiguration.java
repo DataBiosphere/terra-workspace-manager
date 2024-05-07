b93cf999bbcdcfba044385cd0069a306f42cb418
Marcus Talbott <mtalbott@broadinstitute.org>
2024-05-07 10:14:04 -0400

    Revert "[WOR-1335] Cache Sam permissions checks (#1723)"
    This reverts commit 7a5db590ba9cbfb7ae14aa0366a5023efe0a148e.

--- service/src/main/java/bio/terra/workspace/app/configuration/external/SamConfiguration.java
+++ service/src/main/java/bio/terra/workspace/app/configuration/external/SamConfiguration.java
@@ -1,6 +1,5 @@
 package bio.terra.workspace.app.configuration.external;
 
-import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -12,21 +11,11 @@ public class SamConfiguration {
   /** URL of the SAM instance */
   private String basePath;
 
-  private Duration permissionsCacheLifetime;
-
   public String getBasePath() {
     return basePath;
   }
 
   public void setBasePath(String basePath) {
     this.basePath = basePath;
-  }
-
-  public Duration getPermissionsCacheLifetime() {
-    return permissionsCacheLifetime;
-  }
-
-  public void setPermissionsCacheLifetime(Duration permissionsCacheLifetime) {
-    this.permissionsCacheLifetime = permissionsCacheLifetime;
   }
 }
