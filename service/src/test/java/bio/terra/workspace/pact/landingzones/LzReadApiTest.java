3d3210134725087b080c208616ffc239b18b8ac7
aherbst-broad <aherbst@broadinstitute.org>
2024-05-28 13:42:51 -0400

    Testing

--- service/src/test/java/bio/terra/workspace/pact/landingzones/LzReadApiTest.java
+++ service/src/test/java/bio/terra/workspace/pact/landingzones/LzReadApiTest.java
@@ -47,7 +47,7 @@ class LzReadApiTest {
 
   HttpLandingZoneService landingZoneService;
 
-  static final String DT_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS";
+  static final String DT_FORMAT = "yy-MM-dd'T'HH:mm:ss.SSS";
 
   static final UUID azureResourceId = UUID.fromString("e3327645-1e8f-454d-bdae-fe06b4762542");
 
