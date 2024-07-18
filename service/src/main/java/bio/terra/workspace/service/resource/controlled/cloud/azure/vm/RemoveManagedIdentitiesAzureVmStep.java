51213b42f4b74dac9481cb6b28b55f4a85d299c9
Blake Geno <bgeno@broadinstitute.org>
2024-07-18 13:12:04 -0400

    allow management exceptions from VmHelper to be handled by DeleteAzureControlledResourceStep

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/azure/vm/RemoveManagedIdentitiesAzureVmStep.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/cloud/azure/vm/RemoveManagedIdentitiesAzureVmStep.java
@@ -7,6 +7,7 @@ import bio.terra.workspace.service.crl.CrlService;
 import bio.terra.workspace.service.resource.controlled.cloud.azure.DeleteAzureControlledResourceStep;
 import bio.terra.workspace.service.workspace.flight.WorkspaceFlightMapKeys.ControlledResourceKeys;
 import bio.terra.workspace.service.workspace.model.AzureCloudContext;
+import com.azure.core.management.exception.ManagementException;
 import com.azure.resourcemanager.compute.ComputeManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,8 +34,17 @@ public class RemoveManagedIdentitiesAzureVmStep extends DeleteAzureControlledRes
             .get(ControlledResourceKeys.AZURE_CLOUD_CONTEXT, AzureCloudContext.class);
 
     ComputeManager computeManager = crlService.getComputeManager(azureCloudContext, azureConfig);
-    return AzureVmHelper.removeAllUserAssignedManagedIdentitiesFromVm(
-        azureCloudContext, computeManager, resource.getVmName());
+    var helperResult =
+        AzureVmHelper.removeAllUserAssignedManagedIdentitiesFromVm(
+            azureCloudContext, computeManager, resource.getVmName());
+    if (helperResult.isSuccess()) {
+      return helperResult;
+    } else if (helperResult.getException().isPresent()
+        && helperResult.getException().get() instanceof ManagementException) {
+      throw (ManagementException) helperResult.getException().get();
+    } else {
+      return helperResult;
+    }
   }
 
   @Override
