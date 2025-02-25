f3865358e7a0cfe3e323a0522f7455ed8b3f9cbf
David An <davidan@broadinstitute.org>
2025-02-25 16:39:18 -0500

    does this work?

--- service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/database/AzureDatabaseUtilsRunnerTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/controlled/cloud/azure/database/AzureDatabaseUtilsRunnerTest.java
@@ -49,7 +49,7 @@ public class AzureDatabaseUtilsRunnerTest extends BaseMockitoStrictStubbingTest 
   @Mock private ApiAzureLandingZoneDeployedResource mockDatabase;
   @Mock private ApiAzureLandingZoneDeployedResource mockAdminIdentity;
 
-  private final String AZURE_ENV = "AzureCloud";
+  private final String AZURE_ENV = "AZURE";
 
   @BeforeEach
   public void createAzureDatabaseUtilsRunner() {
